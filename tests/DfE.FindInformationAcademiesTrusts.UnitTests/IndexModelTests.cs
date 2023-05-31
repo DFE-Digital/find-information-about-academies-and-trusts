3219d1ff83185d511012ddd6a9ee630d693a96f2
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2023-05-31 15:01:26 +0100

    Rename local mock variables

--- tests/DfE.FindInformationAcademiesTrusts.UnitTests/IndexModelTests.cs
+++ tests/DfE.FindInformationAcademiesTrusts.UnitTests/IndexModelTests.cs
@@ -7,16 +7,16 @@ public class IndexModelTests
     [Fact]
     public async Task OnGetAsync_should_log_trusts_from_api()
     {
-        var loggerMock = new MockLogger<IndexModel>();
-        var academiesApiMock = new Mock<IAcademiesApi>();
+        var mockLogger = new MockLogger<IndexModel>();
+        var mockAcademiesApi = new Mock<IAcademiesApi>();
 
         const string data = "this is data about trusts in the api";
-        academiesApiMock.Setup(a => a.GetTrusts().Result).Returns(data);
+        mockAcademiesApi.Setup(a => a.GetTrusts().Result).Returns(data);
 
-        var sut = new IndexModel(loggerMock.Object, academiesApiMock.Object);
+        var sut = new IndexModel(mockLogger.Object, mockAcademiesApi.Object);
 
         await sut.OnGetAsync();
 
-        loggerMock.VerifyLogInformation(data);
+        mockLogger.VerifyLogInformation(data);
     }
 }
