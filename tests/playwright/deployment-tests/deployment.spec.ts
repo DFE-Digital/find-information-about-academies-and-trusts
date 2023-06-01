2f5a5db64e93866511eef1722359c5471839ee4b
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2023-06-01 15:39:37 +0100

    Add deployment test
    Tests that the app is running, does not test the connection to the API and the database as we don't yet have a connection in the app

---
+++ tests/playwright/deployment-tests/deployment.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('is deployed', async ({ page }) => {
+  await page.goto('######');
+
+  await expect(page).toHaveTitle('Home page - DfE.FindInformationAcademiesTrusts');
+});
