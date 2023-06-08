2c0b8edd46a8e5dd005ab7dc9d81ce67b2dbb419
Liz Daly <liz7daly@gmail.com>
2023-06-08 10:20:33 +0100

    Add TrustSearch class

---
+++ DfE.FindInformationAcademiesTrusts/TrustSearch.cs
@@ -0,0 +1,27 @@
+namespace DfE.FindInformationAcademiesTrusts;
+
+public interface ITrustSearch
+{
+    public Task<IEnumerable<string>> SearchAsync(string searchTerm);
+}
+
+public class TrustSearch : ITrustSearch
+{
+    private readonly ITrustProvider _trustProvider;
+
+    public TrustSearch(ITrustProvider trustProvider)
+    {
+        _trustProvider = trustProvider;
+    }
+
+    public async Task<IEnumerable<string>> SearchAsync(string searchTerm)
+    {
+        if (string.IsNullOrWhiteSpace(searchTerm))
+        {
+            throw new ArgumentException("No search term provided");
+        }
+
+        var trusts = await _trustProvider.GetTrustsAsync();
+        return trusts.Where(s => s.Contains(searchTerm, StringComparison.OrdinalIgnoreCase));
+    }
+}
