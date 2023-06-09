f427ec6f6a1cb52e14604913d08b612efd3f5f5d
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2023-06-09 13:32:58 +0100

    Add Academies API response objects

---
+++ DfE.FindInformationAcademiesTrusts/AcademiesApiResponseModels/ErrorResponse.cs
@@ -0,0 +1,7 @@
+namespace DfE.FindInformationAcademiesTrusts.AcademiesApiResponseModels;
+
+public class ErrorResponse
+{
+    public int? StatusCode { get; set; }
+    public string? Message { get; set; }
+}
