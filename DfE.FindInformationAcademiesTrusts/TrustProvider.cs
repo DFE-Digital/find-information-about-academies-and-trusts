b3281477e5b00c5807ba61c118646390e7896837
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2023-06-09 16:04:42 +0100

    Change from `v2/trusts/bulk` endpoint to `v2/trusts`

--- DfE.FindInformationAcademiesTrusts/TrustProvider.cs
+++ DfE.FindInformationAcademiesTrusts/TrustProvider.cs
@@ -21,15 +21,16 @@ public class TrustProvider : ITrustProvider
     {
         var httpClient = _httpClientFactory.CreateClient("AcademiesApi");
 
-        var httpResponseMessage = await httpClient.GetAsync("v2/trusts/bulk");
+        var httpResponseMessage = await httpClient.GetAsync("v2/trusts");
         if (httpResponseMessage.IsSuccessStatusCode)
         {
             await using var contentStream = await httpResponseMessage.Content.ReadAsStreamAsync();
-            var json = await JsonSerializer.DeserializeAsync<ApiResponseV2<TrustResponse>>(contentStream);
+            var json = await JsonSerializer.DeserializeAsync<ApiResponseV2<TrustSummaryResponse>>(contentStream,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
             if (json?.Data != null)
             {
-                var transformedData = json.Data.Where(t => t.GiasData?.GroupName != null)
-                    .Select(t => new Trust(t.GiasData!.GroupName!));
+                var transformedData = json.Data.Where(t => t.GroupName != null)
+                    .Select(t => new Trust(t.GroupName!));
                 return transformedData;
             }
 
