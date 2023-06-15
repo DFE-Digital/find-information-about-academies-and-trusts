7b9487b1ad013a6772e8cfe9864f540ad44f0462
Liz Daly <liz7daly@gmail.com>
2023-06-15 10:59:57 +0100

    Add search behaviour to search route
    Navigating to search will perform search using query parameter and display results

---
+++ tests/DfE.FindInformationAcademiesTrusts.UnitTests/Pages/SearchModelTests.cs
@@ -0,0 +1,30 @@
+using DfE.FindInformationAcademiesTrusts.Pages;
+
+namespace DfE.FindInformationAcademiesTrusts.UnitTests.Pages;
+
+public class SearchModelTests
+{
+    [Fact]
+    public async Task OnGetAsync_should_search_if_query_parameter()
+    {
+        var mockTrustSearch = new Mock<ITrustSearch>();
+        mockTrustSearch.Setup(s => s.SearchAsync(
+            It.IsAny<string>()
+        ).Result).Returns(
+            new[]
+            {
+                new Trust("trust 1"),
+                new Trust("trust 2"),
+                new Trust("trust 3")
+            }
+        );
+
+        var sut = new SearchModel(mockTrustSearch.Object);
+
+        sut.Query = "trust";
+
+        await sut.OnGetAsync();
+
+        sut.Trusts.Should().HaveCount(3).And.OnlyHaveUniqueItems();
+    }
+}
