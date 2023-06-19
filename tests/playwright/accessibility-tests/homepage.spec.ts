94739f71eafb291af5933c3b30411b06a96229bb
Liz Daly <liz7daly@gmail.com>
2023-06-19 11:28:12 +0100

    Update test to include criteria tags for WCAG 2.1 A and AA

--- tests/playwright/accessibility-tests/homepage.spec.ts
+++ tests/playwright/accessibility-tests/homepage.spec.ts
@@ -5,7 +5,9 @@ test.describe('homepage', () => {
   test('should not have any automatically detectable accessibility issues', async ({ page}) => {
     await page.goto('/');
 
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze()
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+      .analyze()
 
     expect(accessibilityScanResults.violations).toEqual([])
   })
