a945eb90216b30c0f974218051bb8226cada2dfb
Chris Sherlock <chris.sherlock@nimbleapproach.com>
2023-06-29 12:23:29 +0100

    Use direct api calls instead of module

--- tests/playwright/global.teardown.ts
+++ tests/playwright/global.teardown.ts
@@ -1,31 +1,45 @@
 import { test as teardown } from '@playwright/test'
-import { ZapClient } from 'zaproxy'
-import fs from 'fs'
 
-teardown('Generate ZAP report', async () => {
-    if(process.env.zapReport) {
-        console.log('Generating ZAP report')
+teardown('Generate ZAP report', async ({ request }) => {
+    if(process.env.ZAP) {
+        console.log('\nGenerating ZAP report')
 
-        const zapOptions = {
-            apiKey: process.env.zapApiKey || '',
-            proxy: process.env.zapUrl || 'http://localhost:8080'
-        }
-        const zaproxy = new ZapClient(zapOptions)
-        try {
-            await zaproxy.core.htmlreport()
-            .then(
-                resp => {
-                    if(!fs.existsSync('./reports')){
-                        fs.mkdirSync('./reports')
-                    }
-                    fs.writeFileSync('./reports/ZAP-Report.html', resp)
-                },
-                err => {
-                    console.log(`Error during report generation: ${err}`)
+        // Wait for passive scanner to finish scanning before generating report
+        let recordsRemaining = 100;
+        while(recordsRemaining != 0) {
+            const pscanRecords = await request.get(`${process.env.ZAP_URL}/JSON/pscan/view/recordsToScan/`, {
+                headers: {
+                    'X-ZAP-API-Key': process.env.ZAP_API_KEY ? process.env.ZAP_API_KEY : ''
                 }
-            )
-        } catch (err) {
-            console.log(`Error contacting the ZAP API: ${err}`)
+            })
+            await pscanRecords.json()
+            .then(resp => {
+                recordsRemaining = parseInt(resp.recordsToScan, 10)
+            })
+            .catch(err => {
+                console.log(`Error contacting the ZAP API: ${err}`)
+                // avoid infinite loop on error state
+                recordsRemaining = 0
+            })
         }
+
+        const report = await request.post(`${process.env.ZAP_URL}/JSON/reports/action/generate/`, {
+            headers: {
+                'X-ZAP-API-Key': process.env.ZAP_API_KEY ? process.env.ZAP_API_KEY : ''
+            },
+            form: {
+                title: 'Report',
+                template: 'traditional-html',
+                reportFileName: 'ZAP-report'
+            }
+        })
+
+        await report.json()
+        .then(resp => {
+            console.log(`Generated report at ${ resp.generate }`)
+        })
+        .catch(err => {
+            console.log(`Error generating report from ZAP: ${err}`)
+        })
     }
 });
