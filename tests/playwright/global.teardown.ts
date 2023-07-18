1b6e015ac0e4cb3116fb12c8d1c57a38f01481d0
Chris Sherlock <chris.sherlock@nimbleapproach.com>
2023-07-18 16:49:37 +0100

    Fix linting errors in teardown

--- tests/playwright/global.teardown.ts
+++ tests/playwright/global.teardown.ts
@@ -1,47 +1,53 @@
 import { test as teardown } from '@playwright/test'
 import zaproxy from 'zaproxy'
 
-teardown('Generate ZAP report', async ({ request }) => {
-    if(process.env.ZAP) {
-        console.log('\nGenerating ZAP report')
+teardown('Generate ZAP report', async () => {
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  if (process.env.ZAP) {
+    console.log('\nGenerating ZAP report')
 
-        const zapOptions = {
-            apiKey: process.env.ZAP_API_KEY,
-            proxy: {
-                host: process.env.ZAP_ADDRESS,
-                port: process.env.ZAP_PORT
-            }
-        }
-        
-        const zap = new zaproxy(zapOptions)
-        // Wait for passive scanner to finish scanning before generating report
-        let recordsRemaining = 100;
-        while(recordsRemaining != 0) {
-            await zap.pscan.recordsToScan()
-            .then((resp) => {
-                try {
-                    recordsRemaining = parseInt(resp.recordsToScan, 10)
-                } catch (err) {
-                    console.log(`Error converting result: ${err}`)
-                    recordsRemaining = 0
-                }
-            })
-            .catch((err) => {
-                console.log(`Error from the ZAP API: ${err}`)
-                recordsRemaining = 0
-            })
-        }
-    
-        await zap.reports.generate({
-            title: 'Report',
-            template: 'traditional-html',
-            reportfilename: 'ZAP-Report.html'
-        })
+    const zapOptions = {
+      apiKey: process.env.ZAP_API_KEY,
+      proxy: {
+        host: process.env.ZAP_ADDRESS,
+        port: process.env.ZAP_PORT
+      }
+    }
+
+    // eslint-disable-next-line new-cap
+    const zap = new zaproxy(zapOptions)
+    // Wait for passive scanner to finish scanning before generating report
+    let recordsRemaining = 100
+    while (recordsRemaining !== 0) {
+      await zap.pscan.recordsToScan()
         .then((resp) => {
-            console.log(`${JSON.stringify(resp)}`)
+          try {
+            recordsRemaining = parseInt(resp.recordsToScan, 10)
+          } catch (err) {
+            if (err instanceof Error) {
+              console.log(`Error converting result: ${err.message}`)
+            } else {
+              console.log('Unknown error during results conversion')
+            }
+            recordsRemaining = 0
+          }
         })
-        .catch((err) => {
-            console.log(`Error from ZAP API: ${err}`)
+        .catch((err: string) => {
+          console.log(`Error from the ZAP API: ${err}`)
+          recordsRemaining = 0
         })
     }
-});
+
+    await zap.reports.generate({
+      title: 'Report',
+      template: 'traditional-html',
+      reportfilename: 'ZAP-Report.html'
+    })
+      .then((resp) => {
+        console.log(`${JSON.stringify(resp)}`)
+      })
+      .catch((err: string) => {
+        console.log(`Error from ZAP API: ${err}`)
+      })
+  }
+})
