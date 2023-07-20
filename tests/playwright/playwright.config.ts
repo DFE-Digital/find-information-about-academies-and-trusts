37872ddc279597d0785d78a29f44c379917d6b37
Chris Sherlock <chris.sherlock@nimbleapproach.com>
2023-07-20 11:17:42 +0100

    Create individual project and browser config per test type

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -30,30 +30,81 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-      testIgnore: './deployment-tests'
+      use: { ...devices['Desktop Chrome'] }
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-      testIgnore: './deployment-tests'
+      use: { ...devices['Desktop Firefox'] }
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-      testIgnore: './deployment-tests'
+      use: { ...devices['Desktop Safari'] }
     },
     {
       name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
-      testIgnore: './deployment-tests'
+      use: { ...devices['Desktop Edge'], channel: 'msedge' }
+    },
+
+    {
+      name: 'mock-ui',
+      testIgnore: '**/*.spec.ts'
     },
     {
-      name: 'cross-browser',
-      dependencies: ['chromium', 'firefox', 'webkit', 'Microsoft Edge']
+      name: 'chromium-mock',
+      use: { ...devices['Desktop Chrome'] },
+      testDir: './ui-tests',
+      dependencies: ['mock-ui']
     },
+    {
+      name: 'firefox-mock',
+      use: { ...devices['Desktop Firefox'] },
+      testDir: './ui-tests',
+      dependencies: ['mock-ui']
+    },
+    {
+      name: 'webkit-mock',
+      use: { ...devices['Desktop Safari'] },
+      testDir: './ui-tests',
+      dependencies: ['mock-ui']
+    },
+    {
+      name: 'edge-mock',
+      use: { ...devices['Desktop Edge'], channel: 'msedge' },
+      testDir: './ui-tests',
+      dependencies: ['mock-ui']
+    },
+
+    {
+      name: 'accessibility',
+      testIgnore: '**/*.spec.ts'
+    },
+    {
+      name: 'chromium-mock',
+      use: { ...devices['Desktop Chrome'] },
+      testDir: './accessibility-tests',
+      dependencies: ['accessibility']
+    },
+    {
+      name: 'firefox-a11y',
+      use: { ...devices['Desktop Firefox'] },
+      testDir: './accessibility-tests',
+      dependencies: ['accessibility']
+    },
+    {
+      name: 'webkit-a11y',
+      use: { ...devices['Desktop Safari'] },
+      testDir: './accessibility-tests',
+      dependencies: ['mock-ui']
+    },
+    {
+      name: 'edge-a11y',
+      use: { ...devices['Desktop Edge'], channel: 'msedge' },
+      testDir: './accessibility-tests',
+      dependencies: ['accessibility']
+    },
+
     {
       name: 'deployment-tests',
       use: {
@@ -62,6 +113,7 @@ export default defineConfig({
       },
       testDir: './deployment-tests'
     },
+
     {
       name: 'zap-tests',
       use: {
