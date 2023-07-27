20f5e8865fb269ae2cac0cf0208f70ff6918d8c4
Liz Daly <liz7daly@gmail.com>
2023-07-27 11:37:12 +0100

    Update deployment test for search functionality

--- tests/playwright/deployment-tests/deployment.spec.ts
+++ tests/playwright/deployment-tests/deployment.spec.ts
@@ -1,8 +1,13 @@
 import { test } from '@playwright/test'
 import { HomePage } from '../page-object-model/home-page'
+import { SearchResultsPage } from '../page-object-model/search-results-page'
 
 test('is deployed', async ({ page }) => {
   const homePage = new HomePage(page)
+  const searchResultsPage = new SearchResultsPage(page)
   await homePage.goTo()
   await homePage.expect.toBeOnTheRightPage()
+
+  await homePage.searchFor('education')
+  await searchResultsPage.expect.toShowResults()
 })
--- tests/playwright/page-object-model/search-results-page.ts
+++ tests/playwright/page-object-model/search-results-page.ts
@@ -3,9 +3,12 @@ import { Locator, Page, expect } from '@playwright/test'
 export class SearchResultsPage {
   readonly expect: SearchResultsPageAssertions
   readonly _headerLocator: Locator
+  readonly _searchResultsListLocator: Locator
+
   constructor (readonly page: Page) {
     this.expect = new SearchResultsPageAssertions(this)
     this._headerLocator = this.page.locator('h1')
+    this._searchResultsListLocator = this.page.locator('ul')
   }
 }
 
@@ -14,5 +17,9 @@ class SearchResultsPageAssertions {
 
   async toBeOnPageWithResultsFor (searchTerm: string): Promise<void> {
     await expect(this.searchResultsPage._headerLocator).toHaveText(`Search results for "${searchTerm}"`)
+  }
+
+  async toShowResults (): Promise<void> {
+    await expect(this.searchResultsPage._searchResultsListLocator).not.toHaveCount(0)
   }
 }
