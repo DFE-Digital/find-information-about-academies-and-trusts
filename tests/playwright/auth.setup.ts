9eac49f1f0621d847565c04c2773ca5557a4be28
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2023-08-02 16:46:21 +0100

    Add authentication for the Test user

--- tests/playwright/auth.setup.ts
+++ tests/playwright/auth.setup.ts
@@ -1,21 +1,55 @@
 import { test as setup } from '@playwright/test'
+import { HomePage } from './page-object-model/home-page'
+
+if (process.env.TEST_USER_ACCOUNT_NAME === null || process.env.TEST_USER_ACCOUNT_NAME === undefined) {
+  throw new Error('Test user name must be defined')
+}
+if (process.env.TEST_USER_ACCOUNT_PASSWORD === null || process.env.TEST_USER_ACCOUNT_PASSWORD === undefined) {
+  throw new Error('Test user password must be defined')
+}
 
 const authFile = '.auth/user.json'
-const tenantId = process.env.AuthTenantId ?? ''
-const baseUrl = process.env.PLAYWRIGHT_BASEURL ?? 'http://localhost:5163/'
+const unauthenticatedFile = '.auth/unauthenticated-user.json'
+const username: string = process.env.TEST_USER_ACCOUNT_NAME
+const password: string = process.env.TEST_USER_ACCOUNT_PASSWORD
 
-setup('authenticate', async ({ request }) => {
-  await request.post(`https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize`, {
-    form: {
-      response_type: 'code',
-      client_id: process.env.AuthClientID ?? '',
-      scope: 'openid profile email',
-      username: process.env.AuthTestUserName ?? '',
-      password: process.env.AuthTestUserPassword ?? '',
-      redirect_uri: baseUrl + 'signin-oidc',
-      state: '12345',
-      response_mode: 'query'
+setup('authenticate', async ({ page }) => {
+  const homePage = new HomePage(page)
+
+  await page.goto('/')
+
+  await page.locator('[name=loginfmt]').fill((username))
+  await page.getByRole('button', { name: 'Next' }).click()
+
+  await page.locator('[name=passwd]').fill((password))
+  await page.getByRole('button', { name: 'Sign in' }).click()
+
+  // Wait until we are on home page, we might (or might not) have to press a "Stay signed in" button
+  const timeout = 3000
+  const maxtime = Date.now() + timeout
+  const step = 500
+  const staySignedInLocator = page.getByText('Stay signed in')
+  while (Date.now() < maxtime) {
+    try {
+      if (await staySignedInLocator.isVisible()) {
+        await page.getByRole('button', { name: 'No' }).click()
+        break
+      } else if ((await page.title()) === 'Home page - Find information about academies and trusts') {
+        break
+      } else {
+        await page.waitForTimeout(step)
+      }
+    } catch (error) {
+      // if we are mid navigation then an error will be thrown when we attempt to access the page
+      // we should wait and try again
+      await page.waitForTimeout(step)
     }
-  })
-  await request.storageState({ path: authFile })
+  }
+  await homePage.expect.toBeOnTheRightPage()
+
+  await page.context().storageState({ path: authFile })
+})
+
+setup('save unauthenticated state', async ({ page }) => {
+  await page.context().storageState({ path: unauthenticatedFile })
 })
