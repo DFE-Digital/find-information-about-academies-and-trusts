cc9a12aab3ed1145fc58bcee88455fd668563960
Liz Daly <liz7daly@gmail.com>
2023-08-08 16:21:50 +0100

    Update page headings for search page
    Includes updates to UI tests, as current UI tests will fail with the heading update
    
    - delete search results page object model
    - move new methods to search page model

--- tests/playwright/page-object-model/search-page.ts
+++ tests/playwright/page-object-model/search-page.ts
@@ -3,11 +3,15 @@ import { Locator, Page, expect } from '@playwright/test'
 export class SearchPage {
   readonly expect: SearchPageAssertions
   readonly _headerLocator: Locator
+  readonly _searchResultsListHeaderLocator: Locator
   readonly _searchResultsListItemLocator: Locator
 
   constructor (readonly page: Page) {
     this.expect = new SearchPageAssertions(this)
     this._headerLocator = this.page.locator('h1')
+    this._searchResultsListHeaderLocator = this.page.getByRole('heading', {
+      name: 'results for'
+    })
     this._searchResultsListItemLocator = this.page.locator('main>>li')
   }
 
@@ -20,7 +24,11 @@ class SearchPageAssertions {
   constructor (readonly searchPage: SearchPage) {}
 
   async toBeOnPageWithResultsFor (searchTerm: string): Promise<void> {
-    await expect(this.searchPage._headerLocator).toHaveText(`Search results for "${searchTerm}"`)
+    await expect(this.searchPage._searchResultsListHeaderLocator).toContainText(`results for "${searchTerm}"`)
+  }
+
+  async toBeOnTheRightPage (): Promise<void> {
+    await expect(this.searchPage._headerLocator).toHaveText('Search')
   }
 
   async toShowResults (): Promise<void> {
