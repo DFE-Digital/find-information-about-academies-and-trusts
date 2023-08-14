d731fbac470083c4bf114074f0d36c37802c1f4f
Liz Daly <liz7daly@gmail.com>
2023-08-14 17:17:08 +0100

    Move fakeTrusts test data to get property
    This data was previously accessed via a static method, but a get only property is more appropriate.

--- tests/DfE.FindInformationAcademiesTrusts.UnitTests/Pages/SearchModelTests.cs
+++ tests/DfE.FindInformationAcademiesTrusts.UnitTests/Pages/SearchModelTests.cs
@@ -4,6 +4,13 @@ namespace DfE.FindInformationAcademiesTrusts.UnitTests.Pages;
 
 public class SearchModelTests
 {
+    private readonly Trust[] _fakeTrusts =
+    {
+        new("trust 1", "Dorthy Inlet, Kingston upon Hull, City of, JY36 9VC", "2044", 0),
+        new("trust 2", "Grant Course, North East Lincolnshire, QH96 9WV", "2044", 3),
+        new("trust 3", "Abbott Turnpike, East Riding of Yorkshire, BI86 4LZ", "2044", 4)
+    };
+
     [Fact]
     public async Task OnGetAsync_should_search_if_query_parameter()
     {
@@ -11,7 +18,7 @@ public class SearchModelTests
 
         var mockTrustProvider = new Mock<ITrustProvider>();
         mockTrustProvider.Setup(s => s.GetTrustsByNameAsync(query).Result)
-            .Returns(FakeTrusts());
+            .Returns(_fakeTrusts);
         var sut = new SearchModel(mockTrustProvider.Object)
         {
             KeyWords = query
@@ -19,7 +26,7 @@ public class SearchModelTests
 
         await sut.OnGetAsync();
 
-        sut.Trusts.Should().BeEquivalentTo(FakeTrusts());
+        sut.Trusts.Should().BeEquivalentTo(_fakeTrusts);
     }
 
     [Fact]
@@ -31,15 +38,5 @@ public class SearchModelTests
         await sut.OnGetAsync();
 
         sut.Trusts.Should().BeEmpty();
-    }
-
-    private static Trust[] FakeTrusts()
-    {
-        return new[]
-        {
-            new Trust("trust 1", "Dorthy Inlet, Kingston upon Hull, City of, JY36 9VC", "2044", 0),
-            new Trust("trust 2", "Grant Course, North East Lincolnshire, QH96 9WV", "2044", 3),
-            new Trust("trust 3", "Abbott Turnpike, East Riding of Yorkshire, BI86 4LZ", "2044", 4)
-        };
     }
 }
--- tests/DfE.FindInformationAcademiesTrusts.UnitTests/TrustSearchTests.cs
+++ tests/DfE.FindInformationAcademiesTrusts.UnitTests/TrustSearchTests.cs
@@ -6,10 +6,17 @@ public class TrustSearchTests
 {
     private readonly ITrustSearch _sut;
 
+    private readonly Trust[] _fakeTrusts =
+    {
+        new("trust 1", "Dorthy Inlet, Kingston upon Hull, City of, JY36 9VC", "2044", 0),
+        new("trust 2", "Grant Course, North East Lincolnshire, QH96 9WV", "2044", 3),
+        new("trust 3", "Abbott Turnpike, East Riding of Yorkshire, BI86 4LZ", "2044", 24)
+    };
+
     public TrustSearchTests()
     {
         var mockTrustsProvider = new Mock<ITrustProvider>();
-        mockTrustsProvider.Setup(t => t.GetTrustsAsync().Result).Returns(FakeTrusts());
+        mockTrustsProvider.Setup(t => t.GetTrustsAsync().Result).Returns(_fakeTrusts);
         _sut = new TrustSearch(mockTrustsProvider.Object);
     }
 
@@ -52,15 +59,5 @@ public class TrustSearchTests
     {
         await Invoking(() => _sut.SearchAsync(term)).Should().ThrowAsync<ArgumentException>()
             .WithMessage("No search term provided");
-    }
-
-    private static Trust[] FakeTrusts()
-    {
-        return new[]
-        {
-            new Trust("trust 1", "Dorthy Inlet, Kingston upon Hull, City of, JY36 9VC", "2044", 0),
-            new Trust("trust 2", "Grant Course, North East Lincolnshire, QH96 9WV", "2044", 3),
-            new Trust("trust 3", "Abbott Turnpike, East Riding of Yorkshire, BI86 4LZ", "2044", 24)
-        };
     }
 }
