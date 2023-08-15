8ca5bcbf55421cd291890a7a938e978a24ca0e4b
Liz Daly <liz7daly@gmail.com>
2023-08-15 13:45:10 +0100

    Add mock trust provider to homepage UI tests
    This also solves the problem that the same endpoint was being registered twice with different responses - causing in flaky test failures.

--- tests/playwright/ui-tests/homepage.spec.ts
+++ tests/playwright/ui-tests/homepage.spec.ts
@@ -1,33 +1,15 @@
 import { test } from '@playwright/test'
-import { EndpointFeature, IWireMockFeatures, IWireMockRequest, IWireMockResponse, WireMock } from 'wiremock-captain'
 import { HomePage } from '../page-object-model/home-page'
 import { SearchPage } from '../page-object-model/search-page'
+import { MockTrustsProvider } from '../mocks/mock-trusts-provider'
 
 test.describe('homepage', () => {
-  const mock = new WireMock(process.env.WIREMOCK_BASEURL ?? 'http://localhost:8080')
   let homePage: HomePage
   let searchPage: SearchPage
 
   test.beforeAll(async () => {
-    const request: IWireMockRequest = {
-      method: 'GET',
-      endpoint: '/v3/trusts'
-    }
-
-    const mockedResponse: IWireMockResponse = {
-      status: 200,
-      body: {
-        Data: [
-          { GroupName: 'trust 1', TrustAddress: { Street: null, Locality: null, AdditionalLine: null, Town: null, County: null, Postcode: null }, Ukprn: '123', Establishments: [] },
-          { GroupName: 'trust 2', TrustAddress: { Street: null, Locality: null, AdditionalLine: null, Town: null, County: null, Postcode: null }, Ukprn: '124', Establishments: [] },
-          { GroupName: 'trust 3', TrustAddress: { Street: null, Locality: null, AdditionalLine: null, Town: null, County: null, Postcode: null }, Ukprn: '125', Establishments: [] }
-        ]
-      }
-    }
-
-    const features: IWireMockFeatures = { requestEndpointFeature: EndpointFeature.UrlPath }
-
-    await mock.register(request, mockedResponse, features)
+    const mockTrustsProvider = new MockTrustsProvider()
+    await mockTrustsProvider.registerGetTrusts()
   })
 
   test.beforeEach(async ({ page }) => {
