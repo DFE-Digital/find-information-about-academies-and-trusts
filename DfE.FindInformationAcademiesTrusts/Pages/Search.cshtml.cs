23de467f6380de7d499f21cd724dbb994954f3ed
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2023-08-31 11:29:22 +0100

    Autocompleted result now goes direct to details page

--- DfE.FindInformationAcademiesTrusts/Pages/Search.cshtml.cs
+++ DfE.FindInformationAcademiesTrusts/Pages/Search.cshtml.cs
@@ -1,5 +1,6 @@
 using DfE.FindInformationAcademiesTrusts.Pages.Shared;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DfE.FindInformationAcademiesTrusts.Pages;
 
@@ -14,11 +15,18 @@ public class SearchModel : LayoutModel, ISearchFormModel
 
     public string InputId => "search";
     [BindProperty(SupportsGet = true)] public string KeyWords { get; set; } = string.Empty;
+    [BindProperty(SupportsGet = true)] public string TrustId { get; set; } = string.Empty;
     public IEnumerable<Trust> Trusts { get; set; } = Array.Empty<Trust>();
 
-    public async Task OnGetAsync()
+    public async Task<IActionResult> OnGetAsync()
     {
+        if (!string.IsNullOrWhiteSpace(TrustId))
+        {
+            return RedirectToPage("/Trusts/Details", new { Ukprn = TrustId });
+        }
+
         Trusts = await GetTrustsForKeywords();
+        return new PageResult();
     }
 
     private async Task<IEnumerable<Trust>> GetTrustsForKeywords()
