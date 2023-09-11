c62b72a60fe5d9e5decf39649e6cb4c9a44ba675
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2023-09-11 11:41:48 +0100

    Refactor js and no js UI tests
    Rather than copying tests to rerun with and without JavaScript, creating a loop for tests that should run in both contexts, with an extra describe block which should only pass with JavaScript enabled.

--- tests/playwright/ui-tests/searchpage.spec.ts
+++ tests/playwright/ui-tests/searchpage.spec.ts
@@ -2,6 +2,7 @@ import { test } from '@playwright/test'
 import { SearchPage } from '../page-object-model/search-page'
 import { DetailsPage } from '../page-object-model/trust/details-page'
 import { MockTrustsProvider } from '../mocks/mock-trusts-provider'
+import { javaScriptContexts } from '../helpers'
 
 test.describe('Search page', () => {
   let searchPage: SearchPage
@@ -13,34 +14,68 @@ test.describe('Search page', () => {
     detailsPage = new DetailsPage(page)
   })
 
-  test.describe('With JavaScript enabled', () => {
-    test.describe('Given a user goes to straight to the search page', () => {
-      test('then they see an empty search input and can search by a new term', async () => {
-        await searchPage.goTo()
-        await searchPage.searchForm.expect.inputToContainNoSearchTerm()
-        await searchPage.expect.toSeeNoResultsMessage()
-        await searchPage.searchForm.searchFor(searchTerm)
-        await searchPage.expect.toSeeInformationForEachResult()
+  for (const javaScriptContext of javaScriptContexts) {
+    test.describe(`With JavaScript ${javaScriptContext.name}`, () => {
+      test.use({ javaScriptEnabled: javaScriptContext.isEnabled })
+
+      test.describe('Given a user goes to straight to the search page', () => {
+        test('then they see an empty search input and can search by a new term', async () => {
+          await searchPage.goTo()
+          await searchPage.searchForm.expect.inputToContainNoSearchTerm()
+          await searchPage.expect.toSeeNoResultsMessage()
+          await searchPage.searchForm.searchFor(searchTerm)
+          await searchPage.expect.toSeeInformationForEachResult()
+        })
+      })
+
+      test.describe('Given a user searches for a term that returns results', () => {
+        test.beforeEach(async () => {
+          await searchPage.goToSearchFor(searchTerm)
+          await searchPage.expect.toBeOnPageWithResultsFor(searchTerm)
+        })
+
+        test('then it displays a list of results with information about each trust', async () => {
+          await searchPage.expect.toDisplayNumberOfResultsFound()
+          await searchPage.expect.toSeeInformationForEachResult()
+        })
+
+        test('the user can edit their search and search again', async () => {
+          await searchPage.searchForm.expect.inputToContainSearchTerm()
+          await searchPage.searchForm.searchFor('education')
+          await searchPage.expect.toBeOnPageWithResultsFor('education')
+          await searchPage.searchForm.expect.inputToContainSearchTerm()
+          await searchPage.expect.toSeeInformationForEachResult()
+        })
+
+        for (const trustResponse of MockTrustsProvider.fakeTrustsResponseData[searchTerm]) {
+          const trustName = trustResponse.GroupName
+
+          test(`when the user clicks on "${trustName}" then it navigates to the details page`, async () => {
+            await searchPage.clickOnSearchResultLinkWithText(trustName)
+            await detailsPage.expect.toBeOnTheRightPageFor(trustName)
+          })
+        }
+      })
+
+      test.describe('Given a user searches for a term that returns no results', () => {
+        test('then they can see an input containing the search term so they can edit it', async () => {
+          await searchPage.goToSearchFor('non')
+          await searchPage.searchForm.expect.inputToContainSearchTerm()
+        })
+
+        test('they see a helpful message to help them change their search', async () => {
+          await searchPage.goToSearchFor('non')
+          await searchPage.expect.toSeeNoResultsMessage()
+        })
       })
     })
+  }
 
+  test.describe('Only with JavaScript enabled', () => {
     test.describe('Given a user searches for a term that returns results', () => {
       test.beforeEach(async () => {
         await searchPage.goToSearchFor(searchTerm)
         await searchPage.expect.toBeOnPageWithResultsFor(searchTerm)
-      })
-
-      test('then it displays a list of results with information about each trust', async () => {
-        await searchPage.expect.toDisplayNumberOfResultsFound()
-        await searchPage.expect.toSeeInformationForEachResult()
-      })
-
-      test('the user can edit their search and search again', async () => {
-        await searchPage.searchForm.expect.inputToContainSearchTerm()
-        await searchPage.searchForm.searchFor('education')
-        await searchPage.expect.toBeOnPageWithResultsFor('education')
-        await searchPage.searchForm.expect.inputToContainSearchTerm()
-        await searchPage.expect.toSeeInformationForEachResult()
       })
 
       test('the user can edit their search and select an item using autocomplete', async () => {
@@ -50,15 +85,6 @@ test.describe('Search page', () => {
         await searchPage.searchForm.submitSearch()
         await detailsPage.expect.toBeOnTheRightPageFor('Abbey Education')
       })
-
-      for (const trustResponse of MockTrustsProvider.fakeTrustsResponseData[searchTerm]) {
-        const trustName = trustResponse.GroupName
-
-        test(`when the user clicks on "${trustName}" then it navigates to the details page`, async () => {
-          await searchPage.clickOnSearchResultLinkWithText(trustName)
-          await detailsPage.expect.toBeOnTheRightPageFor(trustName)
-        })
-      }
     })
 
     test.describe('Given a user is typing a search term', () => {
@@ -95,73 +121,6 @@ test.describe('Search page', () => {
       test('then they should see no results message if there are no matching trusts', async () => {
         await searchPage.searchForm.typeSearchTerm('non')
         await searchPage.searchForm.expect.toshowNoResultsFoundInAutocomplete()
-      })
-    })
-
-    test.describe('Given a user searches for a term that returns no results', () => {
-      test('then they can see an input containing the search term so they can edit it', async () => {
-        await searchPage.goToSearchFor('non')
-        await searchPage.searchForm.expect.inputToContainSearchTerm()
-      })
-
-      test('they see a helpful message to help them change their search', async () => {
-        await searchPage.goToSearchFor('non')
-        await searchPage.expect.toSeeNoResultsMessage()
-      })
-    })
-  })
-
-  test.describe('With JavaScript disabled', () => {
-    test.use({ javaScriptEnabled: false })
-
-    test.describe('Given a user goes to straight to the search page', () => {
-      test('then they see an empty search input and can search by a new term', async () => {
-        await searchPage.goTo()
-        await searchPage.searchForm.expect.inputToContainNoSearchTerm()
-        await searchPage.expect.toSeeNoResultsMessage()
-        await searchPage.searchForm.searchFor(searchTerm)
-        await searchPage.expect.toSeeInformationForEachResult()
-      })
-    })
-
-    test.describe('Given a user searches for a term that returns results', () => {
-      test.beforeEach(async () => {
-        await searchPage.goToSearchFor(searchTerm)
-        await searchPage.expect.toBeOnPageWithResultsFor(searchTerm)
-      })
-
-      test('then it displays a list of results with information about each trust', async () => {
-        await searchPage.expect.toDisplayNumberOfResultsFound()
-        await searchPage.expect.toSeeInformationForEachResult()
-      })
-
-      test('the user can edit their search and search again', async () => {
-        await searchPage.searchForm.expect.inputToContainSearchTerm()
-        await searchPage.searchForm.searchFor('education')
-        await searchPage.expect.toBeOnPageWithResultsFor('education')
-        await searchPage.searchForm.expect.inputToContainSearchTerm()
-        await searchPage.expect.toSeeInformationForEachResult()
-      })
-
-      for (const trustResponse of MockTrustsProvider.fakeTrustsResponseData[searchTerm]) {
-        const trustName = trustResponse.GroupName
-
-        test(`when the user clicks on "${trustName}" then it navigates to the details page`, async () => {
-          await searchPage.clickOnSearchResultLinkWithText(trustName)
-          await detailsPage.expect.toBeOnTheRightPageFor(trustName)
-        })
-      }
-    })
-
-    test.describe('Given a user searches for a term that returns no results', () => {
-      test('then they can see an input containing the search term so they can edit it', async () => {
-        await searchPage.goToSearchFor('non')
-        await searchPage.searchForm.expect.inputToContainSearchTerm()
-      })
-
-      test('they see a helpful message to help them change their search', async () => {
-        await searchPage.goToSearchFor('non')
-        await searchPage.expect.toSeeNoResultsMessage()
       })
     })
   })
