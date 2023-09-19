2102add5d5e5b87b94a0f1bad2ebf66dff625921
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2023-09-19 15:32:04 +0100

    Add UI tests for correct headings on details page
    Details page should show a banner containing trust name and type, and a h1 with the page title 'Details'.

--- tests/playwright/page-object-model/trust/details-page.ts
+++ tests/playwright/page-object-model/trust/details-page.ts
@@ -1,18 +1,15 @@
 import { Locator, Page, expect } from '@playwright/test'
 import { TrustHeaderComponent } from '../shared/trust-header-component'
-import { TrustNavigationComponent } from '../shared/trust-navigation-component'
 import { MockTrustsProvider } from '../../mocks/mock-trusts-provider'
 
 export class DetailsPage {
   readonly expect: DetailsPageAssertions
   readonly trustHeading: TrustHeaderComponent
-  readonly trustNavigation: TrustNavigationComponent
   readonly pageHeadingLocator: Locator
 
   constructor (readonly page: Page) {
     this.expect = new DetailsPageAssertions(this)
     this.trustHeading = new TrustHeaderComponent(page)
-    this.trustNavigation = new TrustNavigationComponent(page)
     this.pageHeadingLocator = page.locator('h1')
   }
 
@@ -29,11 +26,6 @@ class DetailsPageAssertions {
   async toBeOnTheRightPageFor (trust: string): Promise<void> {
     await this.detailsPage.trustHeading.expect.toContain(trust)
     await expect(this.detailsPage.pageHeadingLocator).toHaveText('Details')
-  }
-
-  async toBeOnTheRightPage (): Promise<void> {
-    const { name } = MockTrustsProvider.expectedFormattedTrustResult
-    await this.toBeOnTheRightPageFor(name)
   }
 
   async toSeeCorrectTrustNameAndTypeInHeader (): Promise<void> {
