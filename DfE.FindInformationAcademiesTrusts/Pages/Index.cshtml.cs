61538cccc8960e3775f8ae49c4c2e4656395bf83
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2023-09-20 17:25:13 +0100

    Split layouts for flexibility
    The main content wrapper used in `_Layout.cshtml` was causing issues with Trust layout - clicking skip to main content will not skip past the side nav.
    
    For more flexibility have split out the body content in the primary `_Layout.cshtml` into two sub layouts:
    1. `_ContentLayout` for normal content pages (no side nav)
    2. `_Trustlayout` has been updated to include the wrapper elements and classes required for each page.
    
    As the wrapper classes are unique for the home page, have modev these to the `index.cshtml`

--- DfE.FindInformationAcademiesTrusts/Pages/Index.cshtml.cs
+++ DfE.FindInformationAcademiesTrusts/Pages/Index.cshtml.cs
@@ -1,15 +1,11 @@
 ﻿using DfE.FindInformationAcademiesTrusts.Pages.Shared;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DfE.FindInformationAcademiesTrusts.Pages;
 
-public class IndexModel : LayoutModel, ISearchFormModel
+public class IndexModel : PageModel, ISearchFormModel
 {
-    public IndexModel()
-    {
-        UsePageWidthContainer = false;
-    }
-
     [BindProperty(SupportsGet = true)] public string KeyWords { get; set; } = string.Empty;
     public string InputId => "home-search";
 }
