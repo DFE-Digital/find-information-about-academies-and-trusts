3b6aceebe2f5885962e822f3a0af469adbb72f0a
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2023-09-25 17:52:34 +0100

    Update error page to show different content if 404 result
    - Add check for 404 result status code
    - Display different title and page content if 404 result
    - Store 'report a problem' link as view constant

--- DfE.FindInformationAcademiesTrusts/Pages/Error.cshtml.cs
+++ DfE.FindInformationAcademiesTrusts/Pages/Error.cshtml.cs
@@ -4,4 +4,13 @@ namespace DfE.FindInformationAcademiesTrusts.Pages;
 
 public class ErrorModel : PageModel
 {
+    public bool Is404Result { get; set; }
+
+    public void OnGet(string statusCode)
+    {
+        if (statusCode == "404")
+        {
+            Is404Result = true;
+        }
+    }
 }
