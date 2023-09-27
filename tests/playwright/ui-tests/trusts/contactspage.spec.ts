f70be8532461db362b68b0e34ac8f84824c9d53a
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2023-09-27 11:29:37 +0100

    Add UI tests for not found trusts
    These test check that if a user tries to visit any page in the trust section without a trust id in the url, or if they try to visit the page with a trust id which doesn't exist - then they should be shown the 'Page not found' screen.

--- tests/playwright/ui-tests/trusts/contactspage.spec.ts
+++ tests/playwright/ui-tests/trusts/contactspage.spec.ts
@@ -1,8 +1,11 @@
 import { test } from '@playwright/test'
 import { ContactsPage } from '../../page-object-model/trust/contacts-page'
+import { NotFoundPage } from '../../page-object-model/not-found-page'
+import { MockTrustsProvider } from '../../mocks/mock-trusts-provider'
 
 test.describe('Details page', () => {
   let contactsPage: ContactsPage
+  let notFoundPage: NotFoundPage
 
   test.beforeEach(async ({ page }) => {
     contactsPage = new ContactsPage(page)
@@ -11,5 +14,19 @@ test.describe('Details page', () => {
 
   test('user should see trust name and type', async () => {
     await contactsPage.expect.toSeeCorrectTrustNameAndTypeInHeader()
+  })
+
+  test.describe('given a user tries to visit the url without an existing trust', () => {
+    test.beforeEach(({ page }) => {
+      notFoundPage = new NotFoundPage(page)
+    })
+
+    test('then they should see a not found message', async () => {
+      await contactsPage.goTo('')
+      await notFoundPage.expect.toBeShownNotFoundMessage()
+
+      await contactsPage.goTo(MockTrustsProvider.nonExistingTrustUkprn)
+      await notFoundPage.expect.toBeShownNotFoundMessage()
+    })
   })
 })
