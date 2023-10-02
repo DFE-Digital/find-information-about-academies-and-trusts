4b7cf94342244e418f2634622f771ee00486ffc8
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2023-10-02 15:35:20 +0100

    Add phase banner
    We need a phase banner on our website to let users know that the product is still in development, and provide a place for users to provide feedback.
    
    - Add GOV.UK phase banner component
    - Add background colour to match application light blue banner
    - Update wording to include the term 'product' rather than 'service'.
    - Let the user know that the link opens in a new tab
    - add feedback form link to view constants

--- DfE.FindInformationAcademiesTrusts/Pages/ViewConstants.cs
+++ DfE.FindInformationAcademiesTrusts/Pages/ViewConstants.cs
@@ -7,4 +7,7 @@ public static class ViewConstants
 
     public const string ReportAProblemLink =
         "https://forms.office.com.mcas.ms/Pages/ResponsePage.aspx?id=yXfS-grGoU2187O4s0qC-VCFjdIWb7BPh79bON_K__tUNzdaMUNUR0ozOVpDRFBGMTVTRlNETUlOWi4u";
+
+    public const string FeedbackFormLink =
+        "https://forms.office.com.mcas.ms/pages/responsepage.aspx?id=yXfS-grGoU2187O4s0qC-VCFjdIWb7BPh79bON_K__tUOTdJMlZZUzBMMFhHV1lHNERFM041U0dUUy4u";
 }
