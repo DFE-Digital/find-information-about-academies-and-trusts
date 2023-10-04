302e41fc111a666c17467c95b8c79c792412a019
Nick Warms <Nicholas.WARMS@education.gov.uk>
2023-10-04 12:03:43 +0100

    Create tests for health endpoint

---
+++ tests/playwright/page-object-model/health-page.ts
@@ -0,0 +1,26 @@
+import { Locator, Page, expect } from '@playwright/test'
+import { SearchFormComponent } from './shared/search-form-component'
+
+export class HealthPage {
+  readonly expect: HealthPageAssertions
+  readonly searchForm: SearchFormComponent
+  readonly body: Locator
+  readonly _searchButtonLocator: Locator
+
+  constructor (readonly page: Page) {
+    this.expect = new HealthPageAssertions(this)
+    this.body = page.locator('body')
+  }
+
+  async goTo(): Promise<void> {
+    await this.page.goto('/health')
+  }
+}
+
+class HealthPageAssertions {
+  constructor (readonly healthPage: HealthPage) { }
+
+  async toHaveStateMatching (state: string): Promise<void> {
+    await expect(this.healthPage.body).toHaveText(state)
+  }
+}
