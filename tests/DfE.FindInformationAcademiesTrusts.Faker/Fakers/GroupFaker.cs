889ca21e5e1d1e80255bb1f656d8c5fbf9027998
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2023-10-11 17:15:28 +0100

    Create GroupFaker
    Create `GroupFaker` class based on the Group table model from the `DfE.FindAcademiesTrusts.AcademiesDb` project.
    
    This Faker only generates data for the fields we are currently using in the application.
    
    Emitting the result of the generated data to a json file for now.
    
    This project uses the Fake Data Generator developed for the TRAMS-alpha prototype as a basis, and have taken the relevant example data and helpers from here.
    
    https://github.com/dynamictulip/FakeDataGenerator

---
+++ tests/DfE.FindInformationAcademiesTrusts.Faker/Fakers/GroupFaker.cs
@@ -0,0 +1,29 @@
+using Bogus;
+using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Models;
+using DfE.FindInformationAcademiesTrusts.Faker.Helpers;
+
+namespace DfE.FindInformationAcademiesTrusts.Faker.Fakers;
+
+public class GroupFaker
+{
+    private readonly Faker<Group> _groupFaker;
+
+    public GroupFaker(TrustToGenerate trustToGenerate)
+    {
+        _groupFaker = new Faker<Group>("en_GB")
+            .RuleFor(t => t.GroupName, trustToGenerate.Name)
+            .RuleFor(t => t.GroupUid, f => $"{f.Random.Int(1000, 9999)}")
+            .RuleFor(td => td.GroupId, f => $"TR{f.Random.Int(0, 9999)}")
+            .RuleFor(td => td.GroupType, trustToGenerate.TrustType)
+            .RuleFor(td => td.GroupContactStreet, f => $"{f.Address.BuildingNumber()} {f.Address.StreetName()}")
+            .RuleFor(td => td.GroupContactLocality, f => f.PickRandom(f.Address.StreetName(), string.Empty))
+            .RuleFor(td => td.GroupContactTown, f => f.PickRandom(f.Address.City(), string.Empty))
+            .RuleFor(td => td.GroupContactCounty, f => f.Address.County())
+            .RuleFor(td => td.GroupContactPostcode, f => f.Address.ZipCode());
+    }
+
+    public Group Generate()
+    {
+        return _groupFaker.Generate();
+    }
+}
--- tests/DfE.FindInformationAcademiesTrusts.Faker/Program.cs
+++ tests/DfE.FindInformationAcademiesTrusts.Faker/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System.Text.Json;
+using Bogus;
+using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Models;
+using DfE.FindInformationAcademiesTrusts.Faker.Fakers;
+using DfE.FindInformationAcademiesTrusts.Faker.Helpers;
+
+namespace DfE.FindInformationAcademiesTrusts.Faker;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        //The randomizer seed enables us to generate slightly repeatable data sets
+        Randomizer.Seed = new Random(28698);
+
+        var fakeGroups = Data.TrustsToGenerate
+            .Select(GenerateGroup)
+            .ToArray();
+
+        var serializeOptions = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+
+        File.WriteAllText("output.json", JsonSerializer.Serialize(fakeGroups, serializeOptions));
+    }
+
+    private static Group GenerateGroup(TrustToGenerate trustToGenerate)
+    {
+        var fakeGroup = new GroupFaker(trustToGenerate);
+        return fakeGroup.Generate();
+    }
+}
