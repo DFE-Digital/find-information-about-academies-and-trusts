c8945d6d7bd0cff9181c4f5e773e9633046cd90a
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2023-10-16 14:45:50 +0100

    Add UI tests for trust details page
    Add a UI test which checks that the correct trust information is displayed on the trust details page.
    
    As this change is purely UI work, using static data the page object model contains the same static data to use in the tests. Once the data is populated correctly in a later user story this test is expected to fail and the page object model will need to be updated to use the AcademiesDb faker to check the correct values are displaying for different trusts.

--- tests/playwright/page-object-model/trust/details-page.ts
+++ tests/playwright/page-object-model/trust/details-page.ts
@@ -8,12 +8,16 @@ export class DetailsPage {
   readonly trustHeading: TrustHeaderComponent
   readonly trustNavigation: TrustNavigationComponent
   readonly pageHeadingLocator: Locator
+  readonly trustDetailsCardLocator: Locator
+  readonly referenceNumbersCardLocator: Locator
 
   constructor (readonly page: Page) {
     this.expect = new DetailsPageAssertions(this)
     this.trustHeading = new TrustHeaderComponent(page)
     this.trustNavigation = new TrustNavigationComponent(page)
     this.pageHeadingLocator = page.locator('h1')
+    this.trustDetailsCardLocator = this.page.getByText('Trust details Address')
+    this.referenceNumbersCardLocator = this.page.getByText('Reference numbers UID')
   }
 
   async goTo (
@@ -39,5 +43,18 @@ class DetailsPageAssertions {
   async toSeeCorrectTrustNameAndTypeInHeader (): Promise<void> {
     const { name, type } = MockTrustsProvider.expectedFormattedTrustResult
     await this.detailsPage.trustHeading.expect.toSeeCorrectTrustNameAndType(name, type)
+  }
+
+  async toSeeCorrectTrustDetails (): Promise<void> {
+    await expect(this.detailsPage.trustDetailsCardLocator).toContainText('Address Dorthy Inlet, Kingston upon Hull, City of, JY36 9VC')
+    await expect(this.detailsPage.trustDetailsCardLocator).toContainText('Opened on 30 Dec 2013')
+    await expect(this.detailsPage.trustDetailsCardLocator).toContainText('Region and territory Yorkshire and the Humber')
+  }
+
+  async toSeeCorrectTrustReferenceNumbers (): Promise<void> {
+    await expect(this.detailsPage.referenceNumbersCardLocator).toContainText('UID (Unique group identifier) 2412')
+    await expect(this.detailsPage.referenceNumbersCardLocator).toContainText('Group ID (identifier) and TRN (trust reference number) TR3971')
+    await expect(this.detailsPage.referenceNumbersCardLocator).toContainText('UKPRN (UK provider reference number) 10013796')
+    await expect(this.detailsPage.referenceNumbersCardLocator).toContainText('Companies House number 03080547')
   }
 }
