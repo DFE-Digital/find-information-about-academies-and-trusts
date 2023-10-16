7a059f6a093263f28c5f512ef3f4f56c3451033a
Nick Warms <Nicholas.WARMS@education.gov.uk>
2023-10-16 10:31:20 +0100

    Change CSP policies to allow for App Insights tracking

--- DfE.FindInformationAcademiesTrusts/Program.cs
+++ DfE.FindInformationAcademiesTrusts/Program.cs
@@ -110,7 +110,12 @@ internal static class Program
                 cspBuilder.AddScriptSrc()
                     .Self()
                     .UnsafeInline()
-                    .WithNonce();
+                    .WithNonce()
+                    .From("https://js.monitor.azure.com/scripts/b/ai.2.min.js");
+                cspBuilder.AddConnectSrc()
+                    .Self()
+                    .WithNonce()
+                    .From("https://*.in.applicationinsights.azure.com//v2/track");
                 cspBuilder.AddObjectSrc().None();
                 cspBuilder.AddBlockAllMixedContent();
                 cspBuilder.AddImgSrc().Self();
