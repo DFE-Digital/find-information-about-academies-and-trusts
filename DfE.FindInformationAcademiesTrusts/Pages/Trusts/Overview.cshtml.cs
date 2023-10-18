937a3cc9eced30f468310a2cd3104ea784e053cd
Nick Warms <Nicholas.WARMS@education.gov.uk>
2023-10-18 12:31:02 +0100

    Create the inital overview page with temporary data

---
+++ DfE.FindInformationAcademiesTrusts/Pages/Trusts/Overview.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DfE.FindInformationAcademiesTrusts.Pages.Trusts;
+
+public class OverviewModel : PageModel, ITrustsAreaModel
+{
+    private readonly ITrustProvider _trustProvider;
+
+    public OverviewModel(ITrustProvider trustProvider)
+    {
+        _trustProvider = trustProvider;
+    }
+
+    [BindProperty(SupportsGet = true)] public string Ukprn { get; set; } = "";
+    public Trust Trust { get; set; } = default!;
+    public string PageName => "Overview";
+    public string Section => ViewConstants.AboutTheTrustSectionName;
+
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        var trust = await _trustProvider.GetTrustByUkprnAsync(Ukprn);
+
+        if (trust == null)
+        {
+            return new NotFoundResult();
+        }
+
+        Trust = trust;
+        return Page();
+    }
+}
