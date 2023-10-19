0738b1d2cae069fd1ea0aeebc62ea72e4a3a74e4
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2023-10-19 13:20:29 +0100

    Update program to create a number of sql scripts
    We are planning to run sql scripts inside our docker image creation. We will create these sql scripts using the entity models and fakeData generator.
    We will still output a json file containing the fake data, to use with our assertions in our playwright tests.

--- tests/DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Faker/Program.cs
+++ tests/DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Faker/Program.cs
@@ -1,33 +1,84 @@
-﻿using System.Text.Json;
+﻿using System.Reflection;
+using System.Text.Json;
 using Bogus;
 using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Faker.Fakers;
 using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Faker.Helpers;
 using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Faker;
 
-internal static class Program
+public static class Program
 {
-    public static void Main(string[] args)
+    public static void Main(params string[] args)
     {
-        //The randomizer seed enables us to generate slightly repeatable data sets
-        Randomizer.Seed = new Random(28698);
-
-        var fakeGroups = Data.TrustsToGenerate
-            .Select(GenerateGroup)
-            .ToArray();
-
-        var serializeOptions = new JsonSerializerOptions
+        try
         {
-            WriteIndented = true
-        };
+            var dbContextOptions = new DbContextOptionsBuilder<AcademiesDbContext>().UseSqlServer();
 
-        File.WriteAllText("output.json", JsonSerializer.Serialize(fakeGroups, serializeOptions));
+            //The randomizer seed enables us to generate slightly repeatable data sets
+            Randomizer.Seed = new Random(28698);
+
+            Directory.CreateDirectory("data");
+
+            var fakeGroups = Data.TrustsToGenerate
+                .Select(GenerateGroup).ToArray();
+
+            var serializeOptions = new JsonSerializerOptions { WriteIndented = true };
+            var jsonisisedTrusts = JsonSerializer.Serialize(fakeGroups, serializeOptions);
+            File.WriteAllText("data/output.json", jsonisisedTrusts);
+
+            var groupProperties = typeof(Group).GetProperties();
+            var valuesStrings = fakeGroups.Select(group => $"({GetEntityValues(group, groupProperties)})");
+
+            var insertScript = $"INSERT INTO [gias].[Group] VALUES {string.Join(',', valuesStrings)}";
+            File.WriteAllText("data/insertScript.sql", insertScript);
+
+            using var context = new AcademiesDbContext(dbContextOptions.Options);
+            var createScript = context.Database.GenerateCreateScript();
+            File.WriteAllText("data/createScript.sql", createScript);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"error {e}");
+        }
     }
 
     private static Group GenerateGroup(TrustToGenerate trustToGenerate)
     {
         var fakeGroup = new GroupFaker(trustToGenerate);
         return fakeGroup.Generate();
+    }
+
+    private static string GetEntityValues<T>(T obj, PropertyInfo[] entityProperties)
+    {
+        var list = entityProperties
+            .Select(pi => GetValueAsString(pi.GetValue(obj), pi.PropertyType));
+
+        return string.Join(", ", list);
+    }
+
+    private static string GetValueAsString(object? value, Type propertyType)
+    {
+        if (value is null)
+            return "NULL";
+        if (propertyType == typeof(string))
+        {
+            return TransformIntoSqlSafeString(value.ToString());
+        }
+
+        if (propertyType == typeof(int))
+        {
+            return value.ToString()!;
+        }
+
+        throw new Exception("unknown type");
+    }
+
+    private static string TransformIntoSqlSafeString(string? unsantisedString)
+    {
+        if (string.IsNullOrEmpty(unsantisedString))
+            return "''";
+        return $"'{unsantisedString.Replace("'", @"''")}'";
     }
 }
