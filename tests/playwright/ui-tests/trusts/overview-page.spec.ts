73db7e1c3bf6e7aac60cdbaa0d63c001653e4fe1
Nick Warms <Nicholas.WARMS@education.gov.uk>
2023-10-25 10:58:29 +0100

    Add non js tests to check compatibility when running without JS

--- tests/playwright/ui-tests/trusts/overview-page.spec.ts
+++ tests/playwright/ui-tests/trusts/overview-page.spec.ts
@@ -2,6 +2,7 @@ import { test } from '@playwright/test'
 import { OverviewPage } from '../../page-object-model/trust/overview-page'
 import { NotFoundPage } from '../../page-object-model/not-found-page'
 import { MockTrustsProvider } from '../../mocks/mock-trusts-provider'
+import { javaScriptContexts } from '../../helpers'
 
 test.describe('Overview page', () => {
   let overviewPage: OverviewPage
@@ -12,27 +13,33 @@ test.describe('Overview page', () => {
     await overviewPage.goTo()
   })
 
-  test('user should see trust name and type', async () => {
-    await overviewPage.expect.toBeOnTheRightPage()
-    await overviewPage.expect.toSeeCorrectTrustNameAndTypeInHeader()
-  })
+  for (const javaScriptContext of javaScriptContexts) {
+    test.describe(`With JavaScript ${javaScriptContext.name}`, () => {
+      test.use({ javaScriptEnabled: javaScriptContext.isEnabled })
 
-  test('user should see the correct trust information', async () => {
-    await overviewPage.expect.toSeeCorrectTrustSummary()
-    await overviewPage.expect.toSeeCorrectOfstedRatings()
-  })
+      test('user should see trust name and type', async () => {
+        await overviewPage.expect.toBeOnTheRightPage()
+        await overviewPage.expect.toSeeCorrectTrustNameAndTypeInHeader()
+      })
 
-  test.describe('given a user tries to visit the url without an existing trust', () => {
-    test.beforeEach(({ page }) => {
-      notFoundPage = new NotFoundPage(page)
+      test('user should see the correct trust information', async () => {
+        await overviewPage.expect.toSeeCorrectTrustSummary()
+        await overviewPage.expect.toSeeCorrectOfstedRatings()
+      })
+
+      test.describe('given a user tries to visit the url without an existing trust', () => {
+        test.beforeEach(({ page }) => {
+          notFoundPage = new NotFoundPage(page)
+        })
+
+        test('then they should see a not found message', async () => {
+          await overviewPage.goTo('')
+          await notFoundPage.expect.toBeShownNotFoundMessage()
+
+          await overviewPage.goTo(MockTrustsProvider.nonExistingTrustUkprn)
+          await notFoundPage.expect.toBeShownNotFoundMessage()
+        })
+      })
     })
-
-    test('then they should see a not found message', async () => {
-      await overviewPage.goTo('')
-      await notFoundPage.expect.toBeShownNotFoundMessage()
-
-      await overviewPage.goTo(MockTrustsProvider.nonExistingTrustUkprn)
-      await notFoundPage.expect.toBeShownNotFoundMessage()
-    })
-  })
+  }
 })
