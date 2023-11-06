224cf2858f5f9a8e154332a56e4094d0e92b52ff
Nick Warms <Nicholas.WARMS@education.gov.uk>
2023-11-06 16:59:02 +0000

    Create Paginated List for pagination responses
    This can be used by the academies db connection to pass pagination infomation to the project

---
+++ DfE.FindInformationAcademiesTrusts.Data/IPaginatedList.cs
@@ -0,0 +1,10 @@
+﻿namespace DfE.FindInformationAcademiesTrusts.Data;
+
+public interface IPaginatedList<T> : IList<T>
+{
+    int PageIndex { get; }
+    int TotalPages { get; }
+    int TotalResults { get; }
+    bool HasPreviousPage { get; }
+    bool HasNextPage { get; }
+}
---
+++ DfE.FindInformationAcademiesTrusts.Data/PaginatedList.cs
@@ -0,0 +1,24 @@
+﻿namespace DfE.FindInformationAcademiesTrusts.Data;
+
+public class PaginatedList<T> : List<T>, IPaginatedList<T>
+{
+    public int PageIndex { get; }
+    public int TotalPages { get; }
+    public int TotalResults { get; }
+
+    public PaginatedList(IEnumerable<T> items, int count, int pageIndex, int pageSize)
+    {
+        PageIndex = pageIndex;
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        TotalResults = count;
+        AddRange(items);
+    }
+
+    public PaginatedList()
+    {
+    }
+
+    public bool HasPreviousPage => PageIndex > 1;
+
+    public bool HasNextPage => PageIndex < TotalPages;
+}
