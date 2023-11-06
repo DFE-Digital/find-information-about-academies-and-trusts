2860534ac301773f4452c5ade8d476419800c2d7
danielryannimble <danielryan@nimbleapproach.com>
2023-11-06 15:59:26 +0000

    Fixed Linting issues due to file location change
    file location change did not account for import location changes

--- tests/playwright/accessibility-tests/privacy-page.spec.ts
+++ tests/playwright/accessibility-tests/privacy-page.spec.ts
@@ -2,14 +2,14 @@ import { test } from './a11y-test'
 import { PrivacyPage } from '../page-object-model/privacy-page'
 
 test.describe('Page not found', () => {
-    let privacyPage: PrivacyPage
+  let privacyPage: PrivacyPage
 
-    test.beforeEach(async ({ page }) => {
-        privacyPage = new PrivacyPage(page)
-    })
+  test.beforeEach(async ({ page }) => {
+    privacyPage = new PrivacyPage(page)
+  })
 
-    test('Privacy page should have no automatically detectable accessibility issues', async ({ expectNoAccessibilityViolations }) => {
-        await privacyPage.goTo()
-        await expectNoAccessibilityViolations()
-    })
+  test('Privacy page should have no automatically detectable accessibility issues', async ({ expectNoAccessibilityViolations }) => {
+    await privacyPage.goTo()
+    await expectNoAccessibilityViolations()
+  })
 })
--- tests/playwright/page-object-model/privacy-page.ts
+++ tests/playwright/page-object-model/privacy-page.ts
@@ -1,22 +1,21 @@
 import { Locator, Page, expect } from '@playwright/test'
 export class PrivacyPage {
-    readonly expect: PrivacyPageAssertions
-    readonly body: Locator
+  readonly expect: PrivacyPageAssertions
+  readonly body: Locator
 
-    constructor (readonly page: Page) {
-        this.expect = new PrivacyPageAssertions(this)
-        this.body = page.locator('body')
-    }
+  constructor (readonly page: Page) {
+    this.expect = new PrivacyPageAssertions(this)
+    this.body = page.locator('body')
+  }
 
-    async goTo (): Promise<void> {
-        await this.page.goto('/privacy')
-    }
+  async goTo (): Promise<void> {
+    await this.page.goto('/privacy')
+  }
 }
 
 class PrivacyPageAssertions {
-    constructor (readonly privacyPage: PrivacyPage) { }
-    async toBeOnTheRightPage(): Promise<void> {
-        
-        await expect(this.privacyPage.body).toContainText('Privacy notice for Find information about academies and trusts')
-    }
+  constructor (readonly privacyPage: PrivacyPage) { }
+  async toBeOnTheRightPage (): Promise<void> {
+    await expect(this.privacyPage.body).toContainText('Privacy notice for Find information about academies and trusts')
+  }
 }
