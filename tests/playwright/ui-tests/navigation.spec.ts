61f20e1548c2895e23a619712ad5f7e52876586e
danielryannimble <danielryan@nimbleapproach.com>
2023-11-06 15:29:55 +0000

    Privacy pages added with footer and ui tests
    A basic footer has been added to access the privacy policy page. Ui tests have been added also to verify accessibility and navigation

--- tests/playwright/ui-tests/navigation.spec.ts
+++ tests/playwright/ui-tests/navigation.spec.ts
@@ -1,19 +1,28 @@
 import { test } from '@playwright/test'
+import { CurrentSearch } from '../../page-object-model/shared/search-form-component'
+import { HomePage } from '../../page-object-model/home-page'
 import { DetailsPage } from '../../page-object-model/trust/details-page'
 import { ContactsPage } from '../../page-object-model/trust/contacts-page'
 import { FakeTestData } from '../../fake-data/fake-test-data'
 import { OverviewPage } from '../../page-object-model/trust/overview-page'
+import { PrivacyPage } from '../../page-object-model/privacy-page'
 
 test.describe('Trust navigation', () => {
+  let homePage : HomePage
+  let currentSearch : CurrentSearch
   let detailsPage: DetailsPage
   let contactsPage: ContactsPage
   let overviewPage: OverviewPage
+  let privacyPage: PrivacyPage
 
   test.beforeEach(async ({ page }) => {
     const fakeTestData = new FakeTestData()
+    currentSearch = new CurrentSearch()
+    homePage = new HomePage(page, currentSearch)
     detailsPage = new DetailsPage(page, fakeTestData)
     contactsPage = new ContactsPage(page, fakeTestData)
     overviewPage = new OverviewPage(page, fakeTestData)
+    privacyPage = new PrivacyPage(page)
   })
 
   test('user should be able to navigate between different sections about a trust', async () => {
@@ -38,4 +47,12 @@ test.describe('Trust navigation', () => {
     await overviewPage.trustNavigation.clickOn('Details')
     await detailsPage.expect.toBeOnTheRightPage()
   })
+
+
+test('user should be able to navigate to the different links within the footer', async () => {
+  //Footer Via HomePage => Privacy
+  await homePage.goTo()
+  await homePage.footerNavigation.clickPrivacyPolicy()
+  await privacyPage.expect.toBeOnTheRightPage()
+})
 })
