1434b1c3d4e2e72f278975e6c007bf914288b2ca
danielryannimble <danielryan@nimbleapproach.com>
2023-11-07 14:36:47 +0000

    contact page added with UI tests
    contact page added with UI test update and accessibilty test added. trust navigation changes as per the heroku prototype

---
+++ tests/playwright/accessibility-tests/trusts/contact-page.spec.ts
@@ -0,0 +1,15 @@
+import { test } from '../a11y-test'
+import { ContactsPage } from '../../page-object-model/trust/contacts-page'
+import { FakeTestData } from '../../fake-data/fake-test-data'
+
+test.describe('Contacts page', () => {
+    test('should not have any automatically detectable accessibility issues', async ({ expectNoAccessibilityViolations, page }) => {
+        const contactsPage = new ContactsPage(page, new FakeTestData())
+        await contactsPage.goTo()
+        await contactsPage.expect.toSeeCorrectTrustNameAndTypeInHeader()
+        await contactsPage.trustNavigation.expect.toBeVisible()
+        await contactsPage.expect.toSeeCorrectDfeContacts()
+        await contactsPage.expect.toSeeCorrectTrustContacts()
+        await expectNoAccessibilityViolations()
+    })
+})
