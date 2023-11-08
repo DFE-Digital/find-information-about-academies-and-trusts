270a6810ee18259afbf1225fa3a7d87b5a94df75
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2023-11-08 15:01:42 +0000

    Add page title for academies pages
    Academies page (browser) titles are slightly different to the h1 shown on the page, so adding an optional property for Trust pages: `PageTitle`. If this is not set, the `PageName` property will be used to created the title.
    
    Also added summaries to the properties in `TrustAreaModel` to make it clearer what each property is used for.

--- DfE.FindInformationAcademiesTrusts/Pages/Trusts/Academies/OfstedRatings.cshtml.cs
+++ DfE.FindInformationAcademiesTrusts/Pages/Trusts/Academies/OfstedRatings.cshtml.cs
@@ -6,6 +6,7 @@ public class OfstedRatingsModel : TrustsAreaModel, IAcademiesAreaModel
 {
     public OfstedRatingsModel(ITrustProvider trustProvider) : base(trustProvider, "Academies in this trust")
     {
+        PageTitle = "Academies Ofsted ratings";
     }
 
     public string TabName => "Ofsted ratings";
