224b7fe331a8fd22d0c581ce02ce630ae2a33c6f
Nick Warms <Nicholas.WARMS@education.gov.uk>
2023-11-08 09:50:24 +0000

    Create a generic pagination partial and model
    This allows any page to use pagination and provides model for it to inherit

---
+++ DfE.FindInformationAcademiesTrusts/Pages/Shared/IPaginationModel.cs
@@ -0,0 +1,11 @@
+﻿using DfE.FindInformationAcademiesTrusts.Data;
+
+namespace DfE.FindInformationAcademiesTrusts.Pages.Shared;
+
+public interface IPaginationModel
+{
+    public string PageName { get; }
+    public IPagination PageStatus { get; }
+    public int PageNumber { get; }
+    public Dictionary<string, string> PaginationRouteData { get; }
+}
