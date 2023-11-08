d278356debfb87ef937fd5a9f46a41414311ec0e
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2023-11-08 14:30:23 +0000

    Add sub navigation to Academies layout
    - Add Academies layout with html template for MOJ sub nav
    - Add Model for Academies area
    - Add Ofsted ratings page to show navigation
    - populate 'aria-current' property if page is current. This property is also used to set current link styling
    - label table by subnav link

--- DfE.FindInformationAcademiesTrusts/Pages/Trusts/Academies/Details.cshtml.cs
+++ DfE.FindInformationAcademiesTrusts/Pages/Trusts/Academies/Details.cshtml.cs
@@ -2,9 +2,11 @@ using DfE.FindInformationAcademiesTrusts.Data;
 
 namespace DfE.FindInformationAcademiesTrusts.Pages.Trusts.Academies;
 
-public class AcademiesDetailsModel : TrustsAreaModel
+public class AcademiesDetailsModel : TrustsAreaModel, IAcademiesAreaModel
 {
     public AcademiesDetailsModel(ITrustProvider trustProvider) : base(trustProvider, "Academies in this trust")
     {
     }
+    
+    public string TabName => "Details";
 }
