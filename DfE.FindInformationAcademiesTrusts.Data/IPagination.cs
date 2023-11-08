8a6fbf39346b76daedd9e741d09492d8ace5df49
Nick Warms <Nicholas.WARMS@education.gov.uk>
2023-11-08 09:49:16 +0000

    Create the pagination class and interface
    This will be used to store pagination specific details, enabling generic front ends

---
+++ DfE.FindInformationAcademiesTrusts.Data/IPagination.cs
@@ -0,0 +1,10 @@
+﻿namespace DfE.FindInformationAcademiesTrusts.Data;
+
+public interface IPagination
+{
+    int PageIndex { get; }
+    int TotalPages { get; }
+    int TotalResults { get; }
+    bool HasPreviousPage { get; }
+    bool HasNextPage { get; }
+}
---
+++ DfE.FindInformationAcademiesTrusts.Data/Pagination.cs
@@ -0,0 +1,18 @@
+﻿namespace DfE.FindInformationAcademiesTrusts.Data;
+
+public class Pagination : IPagination
+{
+    public Pagination(int pageIndex, int totalPages, int totalResults)
+    {
+        PageIndex = pageIndex;
+        TotalPages = totalPages;
+        TotalResults = totalResults;
+    }
+
+    public int PageIndex { get; }
+    public int TotalPages { get; }
+    public int TotalResults { get; }
+    public bool HasPreviousPage => PageIndex > 1;
+
+    public bool HasNextPage => PageIndex < TotalPages;
+}
