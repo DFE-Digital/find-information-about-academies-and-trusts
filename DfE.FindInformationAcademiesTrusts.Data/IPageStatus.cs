18db04a439eabdfcc351f4cbe86ce39abf1fe50b
Nick Warms <Nicholas.WARMS@education.gov.uk>
2023-11-15 15:56:46 +0000

    Create PaginatedList, PageStatus and their interfaces
    These will be used to transfer paginated infomation from the database to the client. Paginated List keeps track of the items that are paginated, and page status lets us know where we are in relation to all other items in the list

---
+++ DfE.FindInformationAcademiesTrusts.Data/IPageStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace DfE.FindInformationAcademiesTrusts.Data;
+
+public interface IPageStatus
+{
+    int PageIndex { get; }
+    int TotalPages { get; }
+    int TotalResults { get; }
+    bool HasPreviousPage { get; }
+    bool HasNextPage { get; }
+}
---
+++ DfE.FindInformationAcademiesTrusts.Data/IPagintatedList.cs
@@ -0,0 +1,6 @@
+﻿namespace DfE.FindInformationAcademiesTrusts.Data;
+
+public interface IPaginatedList<T> : IList<T>
+{
+    public PageStatus PageStatus { get; }
+}
