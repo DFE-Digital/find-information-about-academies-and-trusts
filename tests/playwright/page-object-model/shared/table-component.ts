713f4a7d7a8e53028bc622d264491c88d42f806e
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2023-11-16 17:36:25 +0000

    Create Table and row object model components
    To easily locate and test content of table rows.

---
+++ tests/playwright/page-object-model/shared/table-component.ts
@@ -0,0 +1,29 @@
+import { Locator } from '@playwright/test'
+
+export class TableComponent {
+  readonly locator: Locator
+  readonly rowsLocator: Locator
+
+  constructor (locator: Locator) {
+    this.locator = locator
+    this.rowsLocator = this.locator.locator('tr')
+  }
+
+  getRowComponentAt (rowNumber: number): RowComponent {
+    return new RowComponent(this.rowsLocator.nth(rowNumber))
+  }
+}
+
+export class RowComponent {
+  readonly locator: Locator
+  readonly rowHeaderLocator: Locator
+
+  constructor (locator: Locator) {
+    this.locator = locator
+    this.rowHeaderLocator = this.locator.locator('th')
+  }
+
+  cellLocator (columnNumber: number): Locator {
+    return this.locator.locator('td').nth(columnNumber)
+  }
+}
--- tests/playwright/page-object-model/trust/academies/base-academies-page.ts
+++ tests/playwright/page-object-model/trust/academies/base-academies-page.ts
@@ -2,18 +2,19 @@ import { Locator, Page, expect } from '@playwright/test'
 import { FakeTestData } from '../../../fake-data/fake-test-data'
 import { BaseTrustPage, BaseTrustPageAssertions } from '../base-trust-page'
 import { NavigationComponent } from '../../shared/navigation-component'
+import { TableComponent } from '../../shared/table-component'
 
 export class BaseAcademiesPage extends BaseTrustPage {
   readonly expect: BaseAcademiesPageAssertions
-  readonly academiesTableLocator: Locator
+  readonly academiesTable: TableComponent
   readonly academiesRowLocator: Locator
   readonly subNavigation: NavigationComponent
 
   constructor (readonly page: Page, fakeTestData: FakeTestData, pageUrl: string) {
     super(page, fakeTestData, pageUrl)
     this.expect = new BaseAcademiesPageAssertions(this)
-    this.academiesTableLocator = this.page.getByRole('table')
-    this.academiesRowLocator = this.academiesTableLocator.getByTestId('academy-row')
+    this.academiesTable = new TableComponent(this.page.getByRole('table'))
+    this.academiesRowLocator = this.academiesTable.locator.getByTestId('academy-row')
     this.subNavigation = new NavigationComponent(page, 'Academies tabs')
   }
 }
