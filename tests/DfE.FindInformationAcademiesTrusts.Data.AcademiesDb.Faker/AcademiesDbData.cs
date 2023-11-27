ee0d0fb6c55af9a1a0e545f789e6e0a5d63ed5d0
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2023-11-27 14:05:13 +0000

    Use `TrustProvider` to transform fake data into json
    This is much simpler and more trustworthy than trying to recreate the links ourselves in the JsonGenerator

--- tests/DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Faker/AcademiesDbData.cs
+++ tests/DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Faker/AcademiesDbData.cs
@@ -1,6 +1,9 @@
 using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Models.Cdm;
 using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Models.Gias;
+using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Models.Mis;
 using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Models.Mstr;
+using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.UnitTests.Mocks;
+using Microsoft.EntityFrameworkCore;
 
 namespace DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Faker;
 
@@ -12,5 +15,48 @@ public class AcademiesDbData
     public List<GiasGovernance> GiasGovernances { get; } = new();
     public List<GiasGroupLink> GiasGroupLinks { get; } = new();
     public List<GiasGroup> GiasGroups { get; } = new();
+    public List<MisEstablishment> MisEstablishments { get; } = new();
     public List<MstrTrust> MstrTrusts { get; } = new();
+    public List<MstrTrustGovernance> MstrTrustGovernances { get; } = new();
+
+    public IAcademiesDbContext AsAcademiesDbContext()
+    {
+        return new AcademiesDbDataContext(GiasEstablishments, GiasGovernances, GiasGroupLinks, GiasGroups, MstrTrusts,
+            CdmAccounts, MisEstablishments, CdmSystemusers, MstrTrustGovernances);
+    }
+
+    private class AcademiesDbDataContext : IAcademiesDbContext
+    {
+        public DbSet<GiasEstablishment> GiasEstablishments { get; }
+        public DbSet<GiasGovernance> GiasGovernances { get; }
+        public DbSet<GiasGroupLink> GiasGroupLinks { get; }
+        public DbSet<GiasGroup> Groups { get; }
+        public DbSet<MstrTrust> MstrTrusts { get; }
+        public DbSet<CdmAccount> CdmAccounts { get; }
+        public DbSet<MisEstablishment> MisEstablishments { get; }
+        public DbSet<CdmSystemuser> CdmSystemusers { get; }
+        public DbSet<MstrTrustGovernance> MstrTrustGovernances { get; }
+
+        public AcademiesDbDataContext(
+            IEnumerable<GiasEstablishment> giasEstablishments,
+            IEnumerable<GiasGovernance> giasGovernances,
+            IEnumerable<GiasGroupLink> giasGroupLinks,
+            IEnumerable<GiasGroup> giasGroups,
+            IEnumerable<MstrTrust> mstrTrusts,
+            IEnumerable<CdmAccount> cdmAccounts,
+            IEnumerable<MisEstablishment> misEstablishments,
+            IEnumerable<CdmSystemuser> cdmSystemusers,
+            IEnumerable<MstrTrustGovernance> mstrTrustGovernances)
+        {
+            GiasEstablishments = new MockDbSet<GiasEstablishment>(giasEstablishments).Object;
+            GiasGovernances = new MockDbSet<GiasGovernance>(giasGovernances).Object;
+            GiasGroupLinks = new MockDbSet<GiasGroupLink>(giasGroupLinks).Object;
+            Groups = new MockDbSet<GiasGroup>(giasGroups).Object;
+            MstrTrusts = new MockDbSet<MstrTrust>(mstrTrusts).Object;
+            CdmAccounts = new MockDbSet<CdmAccount>(cdmAccounts).Object;
+            MisEstablishments = new MockDbSet<MisEstablishment>(misEstablishments).Object;
+            CdmSystemusers = new MockDbSet<CdmSystemuser>(cdmSystemusers).Object;
+            MstrTrustGovernances = new MockDbSet<MstrTrustGovernance>(mstrTrustGovernances).Object;
+        }
+    }
 }
