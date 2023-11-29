1a41a8367e0994a3fd3f861b94f767ef7f62716a
Nick Warms <Nicholas.WARMS@education.gov.uk>
2023-11-29 14:57:50 +0000

    Update banner locators and add in the confirmation message checks

--- tests/playwright/page-object-model/shared/cookie-banner-component.ts
+++ tests/playwright/page-object-model/shared/cookie-banner-component.ts
@@ -2,15 +2,21 @@ import { Locator, Page, expect } from '@playwright/test'
 
 export class CookieBannerComponent {
   readonly expect: CookieBannerNavigationComponentAssertions
+  readonly banner: Locator
   readonly acceptCookiesLocator: Locator
   readonly rejectCookiesLocator: Locator
   readonly cookiePageLinkLocator: Locator
+  readonly cookieAcceptedBanner: Locator
+  readonly cookieRejectedBanner: Locator
 
   constructor (readonly page: Page) {
     this.expect = new CookieBannerNavigationComponentAssertions(this)
-    this.acceptCookiesLocator = page.getByRole('button', { name: 'Accept analytics cookies' })
-    this.rejectCookiesLocator = page.getByRole('button', { name: 'Reject analytics cookies' })
-    this.cookiePageLinkLocator = page.getByRole('link', { name: 'set your cookie preferences' })
+    this.banner = page.getByLabel('Cookies on Find Infomation about Academies and Trusts')
+    this.acceptCookiesLocator = this.banner.getByRole('button', { name: 'Accept analytics cookies' })
+    this.rejectCookiesLocator = this.banner.getByRole('button', { name: 'Reject analytics cookies' })
+    this.cookiePageLinkLocator = this.banner.getByRole('link', { name: 'View cookies' })
+    this.cookieAcceptedBanner = this.banner.getByText('You\u2019ve accepted additional cookies')
+    this.cookieRejectedBanner = this.banner.getByText('You\u2019ve rejected additional cookies')
   }
 
   async clickAcceptCookies (): Promise<void> {
@@ -35,6 +41,14 @@ class CookieBannerNavigationComponentAssertions {
   }
 
   async isNotVisible (): Promise<void> {
-    await expect(this.cookieBannerNavigation.acceptCookiesLocator).toHaveCount(0)
+    await expect(this.cookieBannerNavigation.acceptCookiesLocator).not.toBeVisible()
+  }
+
+  async isAccepted (): Promise<void> {
+    await expect(this.cookieBannerNavigation.cookieAcceptedBanner).toBeVisible()
+  }
+
+  async isRejected (): Promise<void> {
+    await expect(this.cookieBannerNavigation.cookieRejectedBanner).toBeVisible()
   }
 }
