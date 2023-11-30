6d614e523ee253f75c0f46ff479f9c0cf4c9ee05
danielryannimble <danielryan@nimbleapproach.com>
2023-11-30 10:43:02 +0000

    contact data added to contact page
    the queries for the governors select by role then if the role end date is null or in the future.

--- DfE.FindInformationAcademiesTrusts/Pages/Trusts/Contacts.cshtml.cs
+++ DfE.FindInformationAcademiesTrusts/Pages/Trusts/Contacts.cshtml.cs
@@ -1,10 +1,36 @@
 using DfE.FindInformationAcademiesTrusts.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DfE.FindInformationAcademiesTrusts.Pages.Trusts;
 
 public class ContactsModel : TrustsAreaModel
 {
+    public Governor? ChairOfTrustees { get; set; }
+
+    public Governor? AccountingOfficer { get; set; }
+
+    public Governor? ChiefFinancialOfficer { get; set; }
+
+    public string ContactNameNotAvailableMessage = "No contact name available";
+
+    public string ContactEmailNotAvailableMessage = "No contact email available";
+
+    public string ContactInformationNotAvailableMessage = "No contact information available";
+
     public ContactsModel(ITrustProvider trustProvider) : base(trustProvider, "Contacts")
     {
+    }
+
+    public override async Task<IActionResult> OnGetAsync()
+    {
+        var pageResult = await base.OnGetAsync();
+        ChairOfTrustees = Trust.Governors.FirstOrDefault(x =>
+            x.Role == "Chair of Trustees" && (x.DateOfTermEnd == null || x.DateOfTermEnd >= DateTime.Now));
+        AccountingOfficer = Trust.Governors.FirstOrDefault(x =>
+            x.Role == "Accounting Officer" && (x.DateOfTermEnd == null || x.DateOfTermEnd >= DateTime.Now));
+        ChiefFinancialOfficer = Trust.Governors.FirstOrDefault(x =>
+            x.Role == "Chief Financial Officer" && (x.DateOfTermEnd == null || x.DateOfTermEnd >= DateTime.Now));
+
+        return pageResult;
     }
 }
