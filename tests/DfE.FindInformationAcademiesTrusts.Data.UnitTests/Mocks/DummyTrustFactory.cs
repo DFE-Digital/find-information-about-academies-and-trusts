12acc3906bc8fb11a308c04ab9dc67a0b89b5bfb
danielryannimble <danielryan@nimbleapproach.com>
2023-12-01 14:45:53 +0000

    Unit tests for Contacts page added
    Unit tests, both positive and negative addded
    Dummy trust factory updated to include governors and trust ContactsModelTests.cs

--- tests/DfE.FindInformationAcademiesTrusts.Data.UnitTests/Mocks/DummyTrustFactory.cs
+++ tests/DfE.FindInformationAcademiesTrusts.Data.UnitTests/Mocks/DummyTrustFactory.cs
@@ -24,8 +24,14 @@ public class DummyTrustFactory
         return GetDummyTrust(uid, "Single-academy trust", academies: academies);
     }
 
+    public static Trust GetDummyTrustWithGovernors()
+    {
+        return GetDummyTrust("1234", governors: ListOfGovernors());
+    }
+
+
     public static Trust GetDummyTrust(string uid, string type = "test", string companiesHouseNumber = "test",
-        Academy[]? academies = null, string status = OpenStatus)
+        Academy[]? academies = null, Governor[]? governors = null, string status = OpenStatus)
     {
         return new Trust(uid,
             $"Trust {uid}",
@@ -37,10 +43,31 @@ public class DummyTrustFactory
             companiesHouseNumber,
             "test",
             academies ?? Array.Empty<Academy>(),
-            Array.Empty<Governor>(),
-            null,
-            null,
+            governors ?? Array.Empty<Governor>(),
+            new Person("Present Trm", "trm@test.com"),
+            new Person("Present Sfsolead", "Sfsolead@test.com"),
             status
         );
+    }
+
+    private static Governor[] ListOfGovernors()
+    {
+        Governor[] listOfGovernors =
+        {
+            new("1", "1", "Past Chair", Email: "pastchair@test.com", Role: "Chair Of Trustees",
+                AppointingBody: null, DateOfAppointment: null, DateOfTermEnd: DateTime.Today.AddDays(-1)),
+            new("2", "2", "Present Chair", Email: "presentchair@test.com", Role: "Chair Of Trustees",
+                AppointingBody: null, DateOfAppointment: null, DateOfTermEnd: DateTime.Today),
+            new("3", "3", "Past Accountingofficer", Email: "pastao@test.com", Role: "Accounting Officer",
+                AppointingBody: null, DateOfAppointment: null, DateOfTermEnd: DateTime.Today.AddDays(-1)),
+            new("4", "4", "Present Accountingofficer", Email: "presentao@test.com", Role: "Accounting Officer",
+                AppointingBody: null, DateOfAppointment: null, DateOfTermEnd: DateTime.Today),
+            new("5", "5", "Past Chieffinancialofficer", Email: "pastcfo@test.com", Role: "Chief Financial Officer",
+                AppointingBody: null, DateOfAppointment: null, DateOfTermEnd: DateTime.Today.AddDays(-1)),
+            new("6", "6", "Present Chieffinancialofficer", Email: "presentcfo@test.com",
+                Role: "Chief Financial Officer", AppointingBody: null, DateOfAppointment: null, DateOfTermEnd: null)
+        };
+
+        return listOfGovernors;
     }
 }
--- tests/DfE.FindInformationAcademiesTrusts.UnitTests/Pages/Trusts/ContactsModelTests.cs
+++ tests/DfE.FindInformationAcademiesTrusts.UnitTests/Pages/Trusts/ContactsModelTests.cs
@@ -1,21 +1,64 @@
 using DfE.FindInformationAcademiesTrusts.Data;
+using DfE.FindInformationAcademiesTrusts.Data.UnitTests.Mocks;
 using DfE.FindInformationAcademiesTrusts.Pages.Trusts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DfE.FindInformationAcademiesTrusts.UnitTests.Pages.Trusts;
 
 public class ContactsModelTests
 {
     private readonly ContactsModel _sut;
+    private readonly Mock<ITrustProvider> _mockTrustProvider;
+    private readonly Trust _dummyTrustWithGovernors;
+    private readonly Trust _dummyTrustWithNoGovernors;
 
     public ContactsModelTests()
     {
-        Mock<ITrustProvider> mockTrustProvider = new();
-        _sut = new ContactsModel(mockTrustProvider.Object);
+        _dummyTrustWithGovernors = DummyTrustFactory.GetDummyTrustWithGovernors();
+        _dummyTrustWithNoGovernors = DummyTrustFactory.GetDummyTrust("1234");
+        _mockTrustProvider = new Mock<ITrustProvider>();
+        _mockTrustProvider.Setup(tp => tp.GetTrustByUidAsync("1234")).ReturnsAsync(_dummyTrustWithGovernors);
+        _sut = new ContactsModel(_mockTrustProvider.Object) { Uid = "1234" };
     }
 
     [Fact]
     public void PageName_should_be_Contacts()
     {
         _sut.PageName.Should().Be("Contacts");
+    }
+
+    [Fact]
+    public async Task OnGetAsync_Returns_Correct_Trust_Contacts()
+    {
+        await _sut.OnGetAsync();
+        _sut.ChairOfTrustees?.FullName.Should().Be("Present Chair");
+        _sut.AccountingOfficer?.FullName.Should<string>().Be("Present Accountingofficer");
+        _sut.ChiefFinancialOfficer?.FullName.Should<string>().Be("Present Chieffinancialofficer");
+    }
+
+    [Fact]
+    public async Task OnGetAsync_Returns_Correct_Dfe_Contacts()
+    {
+        await _sut.OnGetAsync();
+        _sut.Trust.TrustRelationshipManager?.FullName.Should().Be("Present Trm");
+        _sut.Trust.SfsoLead?.FullName.Should().Be("Present Sfsolead");
+    }
+
+    [Fact]
+    public async Task OnGetAsync_Returns_Null_Dfe_Contacts()
+    {
+        _mockTrustProvider.Setup(tp => tp.GetTrustByUidAsync("1234")).ReturnsAsync(_dummyTrustWithNoGovernors);
+        await _sut.OnGetAsync();
+        _sut.ChairOfTrustees?.Should().Be(null);
+        _sut.AccountingOfficer?.FullName.Should().Be(null);
+        _sut.ChiefFinancialOfficer?.FullName.Should().Be(null);
+    }
+
+    [Fact]
+    public async Task OnGetAsync_returns_NotFoundResult_if_Trust_is_null()
+    {
+        _mockTrustProvider.Setup(tp => tp.GetTrustByUidAsync("1234")).ReturnsAsync((Trust?)null);
+        var result = await _sut.OnGetAsync();
+        result.Should().BeOfType<NotFoundResult>();
     }
 }
