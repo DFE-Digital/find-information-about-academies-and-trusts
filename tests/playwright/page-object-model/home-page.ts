5459e58f62f753e9d270266fbafc58bf39443347
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2023-12-06 16:56:18 +0000

    Remove base page components from homepage

--- tests/playwright/page-object-model/home-page.ts
+++ tests/playwright/page-object-model/home-page.ts
@@ -1,21 +1,15 @@
 import { Locator, Page } from '@playwright/test'
 import { CurrentSearch, SearchFormComponent } from './shared/search-form-component'
-import { FooterNavigationComponent } from '../page-object-model/shared/footer-navigation-component'
-import { CookieBannerComponent } from '../page-object-model/shared/cookie-banner-component'
 import { BasePage } from './base-page'
 
 export class HomePage extends BasePage {
   readonly searchForm: SearchFormComponent
-  readonly footerNavigation: FooterNavigationComponent
-  readonly cookieBanner: CookieBannerComponent
   readonly _searchBoxLocator: Locator
   readonly _searchButtonLocator: Locator
 
   constructor (page: Page, currentSearch: CurrentSearch) {
     super(page, '/', 'Home page - Find information about academies and trusts', 'Find information about academies and trusts')
 
-    this.footerNavigation = new FooterNavigationComponent(page)
-    this.cookieBanner = new CookieBannerComponent(page)
     this.searchForm = new SearchFormComponent(
       page,
       'Find information about academies and trusts',
