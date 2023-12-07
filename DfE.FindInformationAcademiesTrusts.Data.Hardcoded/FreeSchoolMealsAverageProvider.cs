eabd475e76974102aaee4b749d28666410e33d77
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2023-12-07 09:56:01 +0000

    Add establishment type matcher
    Create a switch expression which matches the Gias establishment to the establishment name on Explore education statistics free school meals data.
    
    This will be used to get the appropriate average for an an academy.
    
    Change return type for provider methods to be non-nullable
    The percentage will always have a value

--- DfE.FindInformationAcademiesTrusts.Data.Hardcoded/FreeSchoolMealsAverageProvider.cs
+++ DfE.FindInformationAcademiesTrusts.Data.Hardcoded/FreeSchoolMealsAverageProvider.cs
@@ -31,7 +31,35 @@ public class FreeSchoolMealsAverageProvider : IFreeSchoolMealsAverageProvider
 
     public double GetNationalAverage(Academy academy)
     {
-        var key = PhaseTypeGroup.StateFundedApSchool;
+        var key = GetPhaseTypeKey(academy);
         return _fsmAverages2022To23[NationalKey].PercentOfPupilsByPhase[key];
+    }
+
+    public static PhaseTypeGroup GetPhaseTypeKey(Academy academy)
+    {
+        var key = (academy.PhaseOfEducation, academy.TypeOfEstablishment) switch
+        {
+            ("Primary" or "Middle Deemed Primary", "Community School" or "Voluntary Aided School" or "Foundation School"
+                or
+                "Voluntary Controlled School" or "Academy Sponsor Led" or "Academy Converter"
+                or "City Technology College" or
+                "Free Schools" or "University technical college" or "Studio schools") => PhaseTypeGroup
+                    .StateFundedPrimary,
+            ("Secondary" or "Middle Deemed Secondary" or "16 Plus" or "Not applicable" or "All Through" or "All-Through"
+                or
+                "All-through" or "all-through", "Community School" or "Voluntary Aided School" or "Foundation School" or
+                "Voluntary Controlled School" or "Academy 16-19 Converter" or "Academy Sponsor Led"
+                or "Academy Converter" or
+                "City Technology College" or
+                "Free Schools" or "Free schools 16 to 19" or "University technical college" or "Studio schools" or
+                "Academy 16 to 19 sponsor led") => PhaseTypeGroup.StateFundedSecondary,
+            (_, "Foundation Special School" or "Community Special School" or "Academy Special Converter"
+                or "Academy Special Sponsor Led" or "Free Schools Special") => PhaseTypeGroup.StateFundedSpecialSchool,
+            (_, "Pupil Referral Unit" or "Academy Alternative Provision Sponsor Led" or
+                "Free schools alternative provision" or "Academy Alternative Provision Converter") => PhaseTypeGroup
+                    .StateFundedApSchool,
+            _ => throw new ArgumentOutOfRangeException(nameof(academy))
+        };
+        return key;
     }
 }
