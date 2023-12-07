8851b2a7dfcef2bca16c550032068e4408464c64
Nick Warms <Nicholas.WARMS@education.gov.uk>
2023-12-07 16:44:33 +0000

    Add provider to model so that trust pages can access when the data sources were updated

--- DfE.FindInformationAcademiesTrusts/Pages/Trusts/TrustsAreaModel.cs
+++ DfE.FindInformationAcademiesTrusts/Pages/Trusts/TrustsAreaModel.cs
@@ -7,18 +7,26 @@ namespace DfE.FindInformationAcademiesTrusts.Pages.Trusts;
 public class TrustsAreaModel : PageModel, ITrustsAreaModel
 {
     private readonly ITrustProvider _trustProvider;
+    private readonly IDataSourceProvider _dataSourceProvider;
 
-    public TrustsAreaModel(ITrustProvider trustProvider, string pageName)
+    public TrustsAreaModel(ITrustProvider trustProvider, IDataSourceProvider dataSourceProvider, string pageName)
     {
         _trustProvider = trustProvider;
+        _dataSourceProvider = dataSourceProvider;
         PageName = pageName;
     }
 
     [BindProperty(SupportsGet = true)] public string Uid { get; set; } = "";
     public Trust Trust { get; set; } = default!;
+    public IEnumerable<DataSourceListEntry> DataSources { get; set; } = default!;
     public string PageName { get; init; }
     public string? PageTitle { get; init; }
     public string Section => ViewConstants.AboutTheTrustSectionName;
+
+    internal async Task<DataSource?> GetGiasDataUpdated()
+    {
+        return await _dataSourceProvider.GetGIASUpdated();
+    }
 
     public virtual async Task<IActionResult> OnGetAsync()
     {
