e687f54edf8204c5ca85f0b51a046ae887581b83
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2023-12-08 15:00:53 +0000

    Add `MisFurtherEducationEstablishment` EF core scaffolding

--- DfE.FindInformationAcademiesTrusts.Data.AcademiesDb/AcademiesDbContext.cs
+++ DfE.FindInformationAcademiesTrusts.Data.AcademiesDb/AcademiesDbContext.cs
@@ -16,6 +16,7 @@ public interface IAcademiesDbContext
     DbSet<MstrTrust> MstrTrusts { get; }
     DbSet<CdmAccount> CdmAccounts { get; }
     DbSet<MisEstablishment> MisEstablishments { get; }
+    DbSet<MisFurtherEducationEstablishment> MisFurtherEducationEstablishments { get; }
     DbSet<CdmSystemuser> CdmSystemusers { get; }
     DbSet<MstrTrustGovernance> MstrTrustGovernances { get; }
 }
@@ -39,6 +40,7 @@ public class AcademiesDbContext : DbContext, IAcademiesDbContext
     public DbSet<MstrTrust> MstrTrusts { get; set; }
     public DbSet<CdmAccount> CdmAccounts { get; set; }
     public DbSet<MisEstablishment> MisEstablishments { get; set; }
+    public DbSet<MisFurtherEducationEstablishment> MisFurtherEducationEstablishments { get; set; }
     public DbSet<CdmSystemuser> CdmSystemusers { get; set; }
     public DbSet<MstrTrustGovernance> MstrTrustGovernances { get; set; }
 
@@ -1289,7 +1291,57 @@ public class AcademiesDbContext : DbContext, IAcademiesDbContext
                 .HasColumnName("URN at time of the section 8 inspection");
             entity.Property(e => e.WebLink).HasColumnName("Web link");
         });
+        
+        modelBuilder.Entity<MisFurtherEducationEstablishment>(entity =>
+        {
+            entity.HasKey(e => e.ProviderUrn).HasName("PK_ManagementInformationFurtherEducationSchoolTableData");
 
+            entity.ToTable("FurtherEducationEstablishments", "mis");
+
+            entity.Property(e => e.ProviderUrn)
+                .ValueGeneratedNever()
+                .HasColumnName("Provider URN");
+            entity.Property(e => e.BehaviourAndAttitudes).HasColumnName("Behaviour and attitudes");
+            entity.Property(e => e.BehaviourAndAttitudesRaw).HasColumnName("Behaviour and attitudes RAW");
+            entity.Property(e => e.DateOfLatestShortInspection).HasColumnName("Date of latest short inspection");
+            entity.Property(e => e.DatePublished).HasColumnName("Date published");
+            entity.Property(e => e.EffectivenessOfLeadershipAndManagement).HasColumnName("Effectiveness of leadership and management");
+            entity.Property(e => e.EffectivenessOfLeadershipAndManagementRaw).HasColumnName("Effectiveness of leadership and management RAW");
+            entity.Property(e => e.FirstDayOfInspection).HasColumnName("First day of inspection");
+            entity.Property(e => e.ImprovedDeclinedStayedTheSame).HasColumnName("Improved/ declined/ stayed the same");
+            entity.Property(e => e.InspectionNumber).HasColumnName("Inspection number");
+            entity.Property(e => e.InspectionType).HasColumnName("Inspection type");
+            entity.Property(e => e.IsSafeguardingEffective).HasColumnName("Is safeguarding effective?");
+            entity.Property(e => e.LastDayOfInspection).HasColumnName("Last day of Inspection");
+            entity.Property(e => e.LocalAuthority).HasColumnName("Local authority");
+            entity.Property(e => e.NumberOfShortInspectionsSinceLastFullInspection).HasColumnName("Number of short inspections since last full inspection");
+            entity.Property(e => e.NumberOfShortInspectionsSinceLastFullInspectionRaw).HasColumnName("Number of short inspections since last full inspection RAW");
+            entity.Property(e => e.OfstedRegion).HasColumnName("Ofsted region");
+            entity.Property(e => e.OverallEffectiveness).HasColumnName("Overall effectiveness");
+            entity.Property(e => e.OverallEffectivenessRaw).HasColumnName("Overall effectiveness RAW");
+            entity.Property(e => e.PersonalDevelopment).HasColumnName("Personal development");
+            entity.Property(e => e.PersonalDevelopmentRaw).HasColumnName("Personal development RAW");
+            entity.Property(e => e.PreviousBehaviourAndAttitudes).HasColumnName("Previous behaviour and attitudes");
+            entity.Property(e => e.PreviousBehaviourAndAttitudesRaw).HasColumnName("Previous behaviour and attitudes RAW");
+            entity.Property(e => e.PreviousEffectivenessOfLeadershipAndManagement).HasColumnName("Previous effectiveness of leadership and management");
+            entity.Property(e => e.PreviousEffectivenessOfLeadershipAndManagementRaw).HasColumnName("Previous effectiveness of leadership and management RAW");
+            entity.Property(e => e.PreviousInspectionNumber).HasColumnName("Previous inspection number");
+            entity.Property(e => e.PreviousLastDayOfInspection).HasColumnName("Previous last day of inspection");
+            entity.Property(e => e.PreviousOverallEffectiveness).HasColumnName("Previous overall effectiveness");
+            entity.Property(e => e.PreviousOverallEffectivenessRaw).HasColumnName("Previous overall effectiveness RAW");
+            entity.Property(e => e.PreviousPersonalDevelopment).HasColumnName("Previous personal development");
+            entity.Property(e => e.PreviousPersonalDevelopmentRaw).HasColumnName("Previous personal development RAW");
+            entity.Property(e => e.PreviousQualityOfEducation).HasColumnName("Previous quality of education");
+            entity.Property(e => e.PreviousQualityOfEducationRaw).HasColumnName("Previous quality of education RAW");
+            entity.Property(e => e.PreviousSafeguarding).HasColumnName("Previous safeguarding");
+            entity.Property(e => e.ProviderGroup).HasColumnName("Provider group");
+            entity.Property(e => e.ProviderName).HasColumnName("Provider name");
+            entity.Property(e => e.ProviderType).HasColumnName("Provider type");
+            entity.Property(e => e.ProviderUkprn).HasColumnName("Provider UKPRN");
+            entity.Property(e => e.QualityOfEducation).HasColumnName("Quality of education");
+            entity.Property(e => e.QualityOfEducationRaw).HasColumnName("Quality of education RAW");
+        });
+                
         modelBuilder.Entity<CdmSystemuser>(entity =>
         {
             entity
