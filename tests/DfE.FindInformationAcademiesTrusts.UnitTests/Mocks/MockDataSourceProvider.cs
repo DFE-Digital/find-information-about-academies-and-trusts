4febbcd15a894fb330dccc444480bfc2d7a181c9
Nick Warms <Nicholas.WARMS@education.gov.uk>
2023-12-13 16:41:21 +0000

    Create a mock data source for use in tests

---
+++ tests/DfE.FindInformationAcademiesTrusts.UnitTests/Mocks/MockDataSourceProvider.cs
@@ -0,0 +1,21 @@
+﻿using DfE.FindInformationAcademiesTrusts.Data;
+
+namespace DfE.FindInformationAcademiesTrusts.UnitTests.Mocks;
+
+public class MockDataSourceProvider : Mock<IDataSourceProvider>
+{
+    public MockDataSourceProvider()
+    {
+        var staticTime = new DateTime(2023, 11, 9);
+        Setup(f => f.GetGiasUpdated())
+            .ReturnsAsync(new DataSource(Source.Gias, staticTime, UpdateFrequency.Daily));
+        Setup(f => f.GetMstrUpdated())
+            .ReturnsAsync(new DataSource(Source.Mstr, staticTime, UpdateFrequency.Daily));
+        Setup(f => f.GetCdmUpdated())
+            .ReturnsAsync(new DataSource(Source.Cdm, staticTime, UpdateFrequency.Daily));
+        Setup(f => f.GetMisEstablishmentsUpdated())
+            .ReturnsAsync(new DataSource(Source.Mis, staticTime, UpdateFrequency.Monthly));
+        Setup(f => f.GetMisFurtherEducationEstablishmentsUpdated())
+            .ReturnsAsync(new DataSource(Source.Mis, staticTime, UpdateFrequency.Monthly));
+    }
+}
