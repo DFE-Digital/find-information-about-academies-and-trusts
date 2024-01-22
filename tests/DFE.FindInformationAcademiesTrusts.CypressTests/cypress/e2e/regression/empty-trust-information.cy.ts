c8050495bb3c62435e7341c06065d03b01cac0c7
mikestock-nimble <mike.stock@nimbleapproach.com>
2024-01-22 15:06:48 +0000

    adding a test for checking an empty trust

--- tests/DFE.FindInformationAcademiesTrusts.CypressTests/cypress/e2e/regression/empty-trust-information.cy.ts
+++ tests/DFE.FindInformationAcademiesTrusts.CypressTests/cypress/e2e/regression/empty-trust-information.cy.ts
@@ -1,9 +1,20 @@
+import searchTrustPage from "cypress/pages/searchTrustPage";
+
 describe("Checking the details of a trust that does not have any information", () => {
     beforeEach(() => {
         cy.login();
     });
 
     it("Should be able to find the trust and verify the empty information", () => {
+        searchTrustPage
+            .enterSearchText("The empty trust")
+            .withOption("The empty trust")
+            .then((option) => {
+                option
+                    .hasName("The empty trust")
+                    .select();
+            });
 
-    })
+        searchTrustPage.search();
+    });
 });
