572961e78a55e668b096519ee79b17b1f3e10f83
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2024-07-16 09:54:22 +0100

    Add in memory cache to data source provider
    These are taxing on the db and are loaded onto most pages but are only a few values which change infrequently so store in memory

--- DfE.FindInformationAcademiesTrusts.Data.AcademiesDb/DataSourceProvider.cs
+++ DfE.FindInformationAcademiesTrusts.Data.AcademiesDb/DataSourceProvider.cs
@@ -1,24 +1,31 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Models.Ops;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 
 namespace DfE.FindInformationAcademiesTrusts.Data.AcademiesDb;
 
 public class DataSourceProvider : IDataSourceProvider
 {
+    private readonly IMemoryCache _memoryCache;
     private readonly IAcademiesDbContext _academiesDbContext;
     private readonly ILogger<DataSourceProvider> _logger;
 
     [ExcludeFromCodeCoverage]
-    public DataSourceProvider(AcademiesDbContext academiesDbContext, ILogger<DataSourceProvider> logger) : this(
-        (IAcademiesDbContext)academiesDbContext, logger)
+    public DataSourceProvider(AcademiesDbContext academiesDbContext, ILogger<DataSourceProvider> logger,
+        IMemoryCache memoryCache) : this(
+        (IAcademiesDbContext)academiesDbContext, logger, memoryCache)
     {
+        _memoryCache = memoryCache;
     }
 
-    public DataSourceProvider(IAcademiesDbContext academiesDbContext, ILogger<DataSourceProvider> logger)
+    public DataSourceProvider(IAcademiesDbContext academiesDbContext, ILogger<DataSourceProvider> logger,
+        IMemoryCache memoryCache)
     {
         _academiesDbContext = academiesDbContext;
         _logger = logger;
+        _memoryCache = memoryCache;
     }
 
     public Task<DataSource> GetGiasUpdated()
@@ -39,10 +46,33 @@ public class DataSourceProvider : IDataSourceProvider
     private async Task<DataSource> GetDataSourceFromApplicationEvents(string pipelineName, Source source,
         UpdateFrequency updateFrequency)
     {
-        var lastEntry = await _academiesDbContext.ApplicationEvents
-            .Where(e => e.Message == "Finished"
-                        && e.EventType != 'E'
-                        && e.Description == pipelineName).MaxAsync(e => e.DateTime);
+        if (!_memoryCache.TryGetValue(source, out DateTime? lastEntry) || lastEntry is null)
+        {
+            lastEntry = await _academiesDbContext.ApplicationEvents
+                .Where(e => e.Message == "Finished"
+                            && e.EventType != 'E'
+                            && e.Description == pipelineName).MaxAsync(e => e.DateTime);
+
+            TimeSpan cacheExpiration;
+            try
+            {
+                cacheExpiration = updateFrequency switch
+                {
+                    UpdateFrequency.Daily => TimeSpan.FromHours(1),
+                    UpdateFrequency.Monthly => TimeSpan.FromDays(1),
+                    UpdateFrequency.Annually => TimeSpan.FromDays(1),
+                    _ => throw new ArgumentOutOfRangeException(nameof(updateFrequency), updateFrequency, null)
+                };
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                _logger.LogError("Unknown update frequency: {updateFrequency}", updateFrequency);
+                cacheExpiration = TimeSpan.FromHours(1);
+            }
+
+            _memoryCache.Set(source, lastEntry, cacheExpiration);
+        }
+
         if (lastEntry is null)
         {
             _logger.LogError("Unable to find when {pipelineName} was last run", pipelineName);
@@ -54,9 +84,15 @@ public class DataSourceProvider : IDataSourceProvider
 
     public async Task<DataSource> GetMisEstablishmentsUpdated()
     {
-        var lastEntry = await _academiesDbContext.ApplicationSettings
-            .FirstOrDefaultAsync(e => e.Key == "ManagementInformationSchoolTableData CSV Filename");
-        if (lastEntry is null || lastEntry.Modified is null)
+        if (!_memoryCache.TryGetValue(Source.Mis, out ApplicationSetting? lastEntry) || lastEntry is null)
+        {
+            lastEntry = await _academiesDbContext.ApplicationSettings
+                .FirstOrDefaultAsync(e => e.Key == "ManagementInformationSchoolTableData CSV Filename");
+
+            _memoryCache.Set(Source.Mis, lastEntry, TimeSpan.FromDays(1));
+        }
+
+        if (lastEntry?.Modified is null)
         {
             _logger.LogError("Unable to find when ManagementInformationSchoolTableData was last modified");
             return new DataSource(Source.Mis, null, UpdateFrequency.Monthly);
