905144c4b002a5954a82495b0090bbfb8057e1e7
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2024-07-18 14:05:14 +0100

    Implement cache for trust
    Use more unique key incase strings are used by other parts of the app for cache keys

--- DfE.FindInformationAcademiesTrusts.Data.AcademiesDb/TrustProvider.cs
+++ DfE.FindInformationAcademiesTrusts.Data.AcademiesDb/TrustProvider.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics.CodeAnalysis;
 using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Factories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace DfE.FindInformationAcademiesTrusts.Data.AcademiesDb;
 
@@ -11,26 +12,43 @@ public class TrustProvider : ITrustProvider
     private readonly IAcademiesProvider _academiesProvider;
     private readonly IGovernorProvider _governorProvider;
     private readonly IPersonProvider _personProvider;
+    private readonly IMemoryCache _memoryCache;
 
     [ExcludeFromCodeCoverage]
     public TrustProvider(AcademiesDbContext academiesDbContext, ITrustFactory trustFactory,
         IGovernorProvider governorProvider, IAcademiesProvider academiesProvider,
-        IPersonProvider personProvider) : this(
-        (IAcademiesDbContext)academiesDbContext, trustFactory, governorProvider, academiesProvider, personProvider)
+        IPersonProvider personProvider, IMemoryCache memoryCache) : this(
+        (IAcademiesDbContext)academiesDbContext, trustFactory, governorProvider, academiesProvider, personProvider, memoryCache)
     {
     }
 
     public TrustProvider(IAcademiesDbContext academiesDbContext, ITrustFactory trustFactory,
-        IGovernorProvider governorProvider, IAcademiesProvider academiesProvider, IPersonProvider personProvider)
+        IGovernorProvider governorProvider, IAcademiesProvider academiesProvider, IPersonProvider personProvider, IMemoryCache memoryCache)
     {
         _academiesProvider = academiesProvider;
         _personProvider = personProvider;
+        _memoryCache = memoryCache;
         _academiesDbContext = academiesDbContext;
         _trustFactory = trustFactory;
         _governorProvider = governorProvider;
     }
 
     public async Task<Trust?> GetTrustByUidAsync(string uid)
+    {
+        var cacheKey = $"Trust UID: {uid}";
+        if (_memoryCache.TryGetValue(cacheKey, out Trust? trust))
+        {
+            return trust;
+        }
+        
+        trust = await GetTrustFromDb(uid);
+
+        _memoryCache.Set(cacheKey, trust, new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1), SlidingExpiration = TimeSpan.FromMinutes(10) });
+        
+        return trust;
+    }
+
+    private async Task<Trust?> GetTrustFromDb(string uid)
     {
         var giasGroup = await _academiesDbContext.Groups.SingleOrDefaultAsync(g => g.GroupUid == uid);
         if (giasGroup is null) return null;
@@ -41,8 +59,9 @@ public class TrustProvider : ITrustProvider
         var sfsoLead = await _personProvider.GetSfsoLeadLinkedTo(uid);
         var trustRelationshipManager = await _personProvider.GetTrustRelationshipManagerLinkedTo(uid);
 
-        return _trustFactory.CreateTrustFrom(giasGroup, mstrTrust, academies,
+        var myTrust = _trustFactory.CreateTrustFrom(giasGroup, mstrTrust, academies,
             governors, trustRelationshipManager,
             sfsoLead);
+        return myTrust;
     }
 }
