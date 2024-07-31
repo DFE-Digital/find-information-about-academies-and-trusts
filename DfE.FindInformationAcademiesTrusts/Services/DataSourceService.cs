196a313aa7f7ecdb94b1a76299150fd2de4a475b
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2024-07-31 17:35:44 +0100

    Add memory cache to DataSourceService

--- DfE.FindInformationAcademiesTrusts/Services/DataSourceService.cs
+++ DfE.FindInformationAcademiesTrusts/Services/DataSourceService.cs
@@ -1,7 +1,9 @@
+using System.Runtime.CompilerServices;
 using DfE.FindInformationAcademiesTrusts.Data;
 using DfE.FindInformationAcademiesTrusts.Data.Enums;
 using DfE.FindInformationAcademiesTrusts.Data.Repositories;
 using DfE.FindInformationAcademiesTrusts.ServiceModels;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace DfE.FindInformationAcademiesTrusts.Services;
 
@@ -12,10 +14,16 @@ public interface IDataSourceService
 
 public class DataSourceService(
     IAcademiesDbDataSourceRepository academiesDbDataSourceRepository,
-    IFreeSchoolMealsAverageProvider freeSchoolMealsAverageProvider) : IDataSourceService
+    IFreeSchoolMealsAverageProvider freeSchoolMealsAverageProvider,
+    IMemoryCache memoryCache) : IDataSourceService
 {
     public async Task<DataSourceServiceModel> GetAsync(Source source)
     {
+        if (memoryCache.TryGetValue(source, out DataSourceServiceModel? cachedDataSource))
+        {
+            return cachedDataSource!;
+        }
+
         var dataSource = source switch
         {
             Source.Gias => await academiesDbDataSourceRepository.GetGiasUpdatedAsync(),
@@ -26,6 +34,22 @@ public class DataSourceService(
             _ => throw new ArgumentOutOfRangeException(nameof(source), source, null)
         };
 
-        return new DataSourceServiceModel(dataSource.Source, dataSource.LastUpdated, dataSource.NextUpdated);
+        var dataSourceServiceModel =
+            new DataSourceServiceModel(dataSource.Source, dataSource.LastUpdated, dataSource.NextUpdated);
+
+        if (dataSourceServiceModel.LastUpdated is not null)
+        {
+            var cacheExpiration = dataSourceServiceModel.NextUpdated switch
+            {
+                UpdateFrequency.Daily => TimeSpan.FromHours(1),
+                UpdateFrequency.Monthly or
+                    UpdateFrequency.Annually => TimeSpan.FromDays(1),
+                _ => throw new SwitchExpressionException(dataSourceServiceModel.NextUpdated)
+            };
+
+            memoryCache.Set(source, dataSourceServiceModel, cacheExpiration);
+        }
+
+        return dataSourceServiceModel;
     }
 }
