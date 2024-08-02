1ca9a6709e8930f3bab80e0a545b243633360f9f
cmurrell148 <conor.murrell@nimbleapproach.com>
2024-08-02 10:38:03 +0100

    create new cypress suite

---
+++ tests/NewCypress/cypress.config.ts
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      config.baseUrl = config.env.url;
+
+      return config;
+    },
+  },
+});
