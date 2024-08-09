73f2fb05261a6d2f788b6fca9054b0bc4e35c6b9
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-08-09 09:26:55 +0100

    Add frontend code for DfE frontend package

--- DfE.FindInformationAcademiesTrusts/assets/index.js
+++ DfE.FindInformationAcademiesTrusts/assets/index.js
@@ -1,25 +1,35 @@
-import { initAll as govukFrontendInitAll } from 'govuk-frontend'
-import { Autocomplete } from './javascripts/autocomplete'
-import SideNavigation from '@scottish-government/design-system/src/components/side-navigation/side-navigation'
-import $ from 'jquery'
-import { SortableTable, nodeListForEach } from '@ministryofjustice/frontend'
+import { initAll as govukFrontendInitAll } from "govuk-frontend";
+import { Autocomplete } from "./javascripts/autocomplete";
+import SideNavigation from "@scottish-government/design-system/src/components/side-navigation/side-navigation";
+import $ from "jquery";
+import { SortableTable, nodeListForEach } from "@ministryofjustice/frontend";
+import Header from "dfe-frontend/packages/components/header/header";
 
-window.$ = $
+window.$ = $;
 
-govukFrontendInitAll()
+govukFrontendInitAll();
 
 // Sortable table initialisation from the MOJ Frontend library
-const sortableTables = document.querySelectorAll('[data-module="moj-sortable-table"]')
-nodeListForEach(sortableTables, table =>
-  new SortableTable({
-    table
-  })
-)
+const sortableTables = document.querySelectorAll(
+  '[data-module="moj-sortable-table"]',
+);
+nodeListForEach(
+  sortableTables,
+  (table) =>
+    new SortableTable({
+      table,
+    }),
+);
 
-const sideNavs = [].slice.call(document.querySelectorAll('[data-module="ds-side-navigation"]'))
-sideNavs.forEach(accordion =>
-  new SideNavigation(accordion).init()
-)
+const sideNavs = [].slice.call(
+  document.querySelectorAll('[data-module="ds-side-navigation"]'),
+);
+sideNavs.forEach((accordion) => new SideNavigation(accordion).init());
 
 // Add Autocomplete namespace so that exposed methods can be invoked from Razor Partial
-window.Autocomplete = new Autocomplete()
+window.Autocomplete = new Autocomplete();
+
+// Initialize DfE frontend components
+document.addEventListener("DOMContentLoaded", () => {
+  Header();
+});
