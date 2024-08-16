931bfbbc5df20e236fb35afc9c0ea0bd1940171c
cmurrell148 <conor.murrell@nimbleapproach.com>
2024-08-16 10:20:36 +0100

    update search tests

---
+++ tests/NewCypress/cypress/e2e/regression/search-results-page.cy.ts
@@ -0,0 +1,40 @@
+import paginationPage from "../../pages/paginationPage";
+import searchPage from "../../pages/searchPage";
+import homePage from "../../pages/homePage";
+
+describe("Testing the components of the search results page", () => {
+
+    beforeEach(() => {
+        cy.login();
+    });
+
+    it("Should check that the search page returns result no found when something does not exist", () => {
+
+        homePage
+            .enterMainSearchText("KnowWhere")
+            .clickMainSearchButton()
+
+        searchPage
+            .checkNoSearchResultsFound()
+    });
+
+
+    it("Checks that the user can edit their search and search for a new trust from the search page", () => {
+
+        homePage
+            .enterMainSearchText("West")
+            .clickMainSearchButton()
+
+        searchPage
+            .checkSearchResultsReturned("West")
+
+        searchPage
+            .enterSearchResultsSearchText("East")
+            .clicSearchPageSearchButton()
+
+        searchPage
+            .checkSearchResultsReturned("East")
+    });
+
+
+})
