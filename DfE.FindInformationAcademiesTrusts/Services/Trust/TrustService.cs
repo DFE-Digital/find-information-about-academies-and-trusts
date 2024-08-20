c0f8114e3b47c4f5756744f7eeddbe2534503521
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-08-20 13:48:52 +0100

    Add governance to Trust Service Create Trust Governance Service Model

--- DfE.FindInformationAcademiesTrusts/Services/Trust/TrustService.cs
+++ DfE.FindInformationAcademiesTrusts/Services/Trust/TrustService.cs
@@ -8,6 +8,7 @@ public interface ITrustService
 {
     Task<TrustSummaryServiceModel?> GetTrustSummaryAsync(string uid);
     Task<TrustDetailsServiceModel> GetTrustDetailsAsync(string uid);
+    Task<TrustGovernanceServiceModel> GetTrustGoverenaceAsync(string uid);
 }
 
 public class TrustService(
@@ -61,5 +62,12 @@ public class TrustService(
         );
 
         return trustDetailsDto;
+    }
+
+    public async Task<TrustGovernanceServiceModel> GetTrustGoverenaceAsync(string uid)
+    {
+        var (trustLeadership, members, trustees, historicMembers) = await trustRepository.GetTrustGovernanceAsync(uid);
+        var governanceDto = new TrustGovernanceServiceModel(trustLeadership, members, trustees, historicMembers);
+        return governanceDto;
     }
 }
