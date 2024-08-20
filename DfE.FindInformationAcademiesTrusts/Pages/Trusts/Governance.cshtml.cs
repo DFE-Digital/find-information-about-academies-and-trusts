6248c94aa2b439733147288a2a18d29651c56681
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-08-20 13:49:06 +0100

    Create Governance page

---
+++ DfE.FindInformationAcademiesTrusts/Pages/Trusts/Governance.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using DfE.FindInformationAcademiesTrusts.Data;
+using DfE.FindInformationAcademiesTrusts.Data.Enums;
+using DfE.FindInformationAcademiesTrusts.ServiceModels;
+using DfE.FindInformationAcademiesTrusts.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DfE.FindInformationAcademiesTrusts.Pages.Trusts;
+
+public class GovernanceModel(
+    ITrustProvider trustProvider,
+    IDataSourceService dataSourceService,
+    ILogger<GovernanceModel> logger,
+    ITrustService trustService)
+    : TrustsAreaModel(trustProvider, dataSourceService, trustService, logger, "Governance")
+{
+    public TrustGovernanceServiceModel TrustGovernance { get; set; } = default!;
+
+    public override async Task<IActionResult> OnGetAsync()
+    {
+        var pageResult = await base.OnGetAsync();
+
+        if (pageResult.GetType() == typeof(NotFoundResult)) return pageResult;
+
+        TrustGovernance = await TrustService.GetTrustGoverenaceAsync(Uid);
+
+        DataSources.Add(new DataSourceListEntry(await DataSourceService.GetAsync(Source.Gias),
+            new List<string> { "Governance" }));
+
+        return pageResult;
+    }
+}
