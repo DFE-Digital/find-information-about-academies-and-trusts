e6d5357730362618ef33dc446a02163c95523b75
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2024-08-23 11:06:14 +0100

    Implement AcademyRepository `GetAcademiesInTrustOfstedAsync`

--- DfE.FindInformationAcademiesTrusts.Data.AcademiesDb/Repositories/AcademyRepository.cs
+++ DfE.FindInformationAcademiesTrusts.Data.AcademiesDb/Repositories/AcademyRepository.cs
@@ -1,10 +1,14 @@
+using System.Globalization;
 using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Contexts;
+using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Extensions;
 using DfE.FindInformationAcademiesTrusts.Data.Repositories.Academy;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Repositories;
 
-public class AcademyRepository(IAcademiesDbContext academiesDbContext) : IAcademyRepository
+public class AcademyRepository(IAcademiesDbContext academiesDbContext, ILogger<AcademyRepository> logger)
+    : IAcademyRepository
 {
     public async Task<AcademyDetails[]> GetAcademiesInTrustDetailsAsync(string uid)
     {
@@ -21,9 +25,94 @@ public class AcademyRepository(IAcademiesDbContext academiesDbContext) : IAcadem
             .ToArrayAsync();
     }
 
-    public Task<AcademyOfsted[]> GetAcademiesInTrustOfstedAsync(string uid)
+    public async Task<AcademyOfsted[]> GetAcademiesInTrustOfstedAsync(string uid)
     {
-        throw new NotImplementedException();
+        var giasGroupLinkData = await academiesDbContext.GiasGroupLinks
+            .Where(gl => gl.GroupUid == uid)
+            .Select(gl => new
+            {
+                Urn = gl.Urn!,
+                gl.EstablishmentName,
+                gl.JoinedDate
+            })
+            .ToListAsync();
+
+        var ofstedRatings = await GetOfstedRatings(giasGroupLinkData.Select(gl => int.Parse(gl.Urn)).ToArray());
+
+        var academyOfsteds = giasGroupLinkData.Select(gl =>
+                new AcademyOfsted(gl.Urn,
+                    gl.EstablishmentName,
+                    DateTime.ParseExact(gl.JoinedDate!, "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    ofstedRatings[gl.Urn].Previous,
+                    ofstedRatings[gl.Urn].Current
+                ))
+            .ToArray();
+
+        return academyOfsteds;
+    }
+
+    private async Task<Dictionary<string, (OfstedRating Current, OfstedRating Previous)>> GetOfstedRatings(int[] urns)
+    {
+        // Ofsted data is held in MisEstablishments for most academies
+        var ofstedRatings =
+            await academiesDbContext.MisEstablishments
+                .Where(me => urns.Contains(me.Urn!.Value))
+                .Select(me => new
+                {
+                    Urn = me.Urn!.Value,
+                    Current = me.OverallEffectiveness == null
+                        ? OfstedRating.None
+                        : new OfstedRating(
+                            (OfstedRatingScore)me.OverallEffectiveness.Value,
+                            me.InspectionEndDate.ParseAsNullableDate()
+                        ),
+                    Previous = me.PreviousFullInspectionOverallEffectiveness == null
+                        ? OfstedRating.None
+                        : new OfstedRating(
+                            (OfstedRatingScore)int.Parse(me.PreviousFullInspectionOverallEffectiveness),
+                            me.PreviousInspectionEndDate.ParseAsNullableDate()
+                        )
+                })
+                .ToListAsync();
+
+        // Look in MisFurtherEducationEstablishments for academies not found in MisEstablishments
+        // Note: if an entry is in MisEstablishments then it will not be in MisFurtherEducationEstablishments, even if it has no ofsted data
+        var urnsNotInMisEstablishments = urns.Except(ofstedRatings.Select(a => a.Urn)).ToArray();
+        if (urnsNotInMisEstablishments.Length > 0)
+        {
+            ofstedRatings.AddRange(
+                await academiesDbContext.MisFurtherEducationEstablishments
+                    .Where(mfe => urnsNotInMisEstablishments.Contains(mfe.ProviderUrn))
+                    .Select(mfe =>
+                        new
+                        {
+                            Urn = mfe.ProviderUrn,
+                            Current = mfe.OverallEffectiveness == null
+                                ? OfstedRating.None
+                                : new OfstedRating(
+                                    (OfstedRatingScore)mfe.OverallEffectiveness.Value,
+                                    mfe.LastDayOfInspection.ParseAsNullableDate()
+                                ),
+                            Previous = mfe.PreviousOverallEffectiveness == null
+                                ? OfstedRating.None
+                                : new OfstedRating(
+                                    (OfstedRatingScore)mfe.PreviousOverallEffectiveness.Value,
+                                    mfe.PreviousLastDayOfInspection.ParseAsNullableDate()
+                                )
+                        }
+                    ).ToArrayAsync()
+            );
+        }
+
+        foreach (var urn in urns.Except(ofstedRatings.Select(a => a.Urn)))
+        {
+            logger.LogError(
+                "URN {Urn} was not found in Mis.Establishments or Mis.FurtherEducationEstablishments. This indicates a data integrity issue with the Ofsted data in Academies Db.",
+                urn);
+            ofstedRatings.Add(new { Urn = urn, Current = OfstedRating.None, Previous = OfstedRating.None });
+        }
+
+        return ofstedRatings.ToDictionary(o => o.Urn.ToString(), o => (o.Current, o.Previous));
     }
 
     public async Task<int> GetNumberOfAcademiesInTrustAsync(string uid)
