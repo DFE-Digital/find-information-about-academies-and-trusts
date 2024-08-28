27c9ada88ae77eedbd2e7a6cf8e317f95454e144
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-08-28 13:11:31 +0100

    Add DateTime extension for replacing date with text if null

---
+++ DfE.FindInformationAcademiesTrusts/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using DfE.FindInformationAcademiesTrusts.Pages;
+
+namespace DfE.FindInformationAcademiesTrusts.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static string ShowDateStringOrReplaceWithText(this DateTime? date, string replacementText = "No Data",
+        string format = StringFormatConstants.ViewDate)
+    {
+        if (date.HasValue)
+        {
+            return date.Value.ToString(format);
+        }
+
+        return replacementText;
+    }
+}
