404a65acdc46ea3d52f8538745e2ed80ab95d9ca
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-08-28 13:12:47 +0100

    Add DateTime extension tests

---
+++ tests/DfE.FindInformationAcademiesTrusts.UnitTests/Extensions/DateTimeExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using DfE.FindInformationAcademiesTrusts.Extensions;
+using DfE.FindInformationAcademiesTrusts.Pages;
+
+namespace DfE.FindInformationAcademiesTrusts.UnitTests.Extensions;
+
+public class DateTimeExtensionsTests
+{
+    [Fact]
+    public void ShowDateStringOrReplaceWithText_ReturnsFormattedDate_WhenNotNull()
+    {
+        DateTime? testTime = DateTime.Today;
+        var result = testTime.ShowDateStringOrReplaceWithText();
+        result.Should().BeEquivalentTo(DateTime.Today.ToString(StringFormatConstants.ViewDate));
+    }
+
+    [Fact]
+    public void ShowDateStringOrReplaceWithText_returns_NoData_WhenNull()
+    {
+        DateTime? testTime = null;
+        var result = testTime.ShowDateStringOrReplaceWithText();
+        result.Should().BeEquivalentTo("No Data");
+    }
+
+    [Fact]
+    public void ShowDateStringOrReplaceWithText_ReturnsSpecifiedFormattedDate_WhenNotNull()
+    {
+        DateTime? testTime = DateTime.Today;
+        var result = testTime.ShowDateStringOrReplaceWithText(format: StringFormatConstants.SortDate);
+        result.Should().BeEquivalentTo(DateTime.Today.ToString(StringFormatConstants.SortDate));
+    }
+
+    [Fact]
+    public void ShowDateStringOrReplaceWithText_returns_SpecifiedText_WhenNull()
+    {
+        DateTime? testTime = null;
+        var result = testTime.ShowDateStringOrReplaceWithText("Test String");
+        result.Should().BeEquivalentTo("Test String");
+    }
+}
