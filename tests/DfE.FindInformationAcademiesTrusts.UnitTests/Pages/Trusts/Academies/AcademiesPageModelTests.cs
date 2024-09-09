042cb092e766afbaddc42dc1e630fc8325ff1f23
Dominic NEED <Dominic.NEED@EDUCATION.GOV.UK>
2024-09-09 15:58:42 +0100

    Added assertion for testing expected bytes on export

--- tests/DfE.FindInformationAcademiesTrusts.UnitTests/Pages/Trusts/Academies/AcademiesPageModelTests.cs
+++ tests/DfE.FindInformationAcademiesTrusts.UnitTests/Pages/Trusts/Academies/AcademiesPageModelTests.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 
 namespace DfE.FindInformationAcademiesTrusts.UnitTests.Pages.Trusts.Academies
 {
-    public class ExportableAcademiesPageModelTests
+    public class AcademiesPageModelTests
     {
         private readonly Mock<ITrustProvider> _mockTrustProvider = new();
         private readonly Mock<ITrustService> _mockTrustService = new();
@@ -19,15 +19,15 @@ namespace DfE.FindInformationAcademiesTrusts.UnitTests.Pages.Trusts.Academies
         private readonly Mock<IOtherServicesLinkBuilder> _mockOtherServicesLinkBuilder = new();
         private readonly Mock<IDataSourceService> _mockDataSourceService = new();
         private readonly Mock<ILogger<AcademiesDetailsModel>> _mockLogger = new();
-        private readonly ExportableAcademiesPageModel _sut;
+        private readonly AcademiesPageModel _sut;
 
-        public ExportableAcademiesPageModelTests()
+        public AcademiesPageModelTests()
         {
             _sut = new AcademiesDetailsModel(_mockTrustProvider.Object, _mockDataSourceService.Object, _mockOtherServicesLinkBuilder.Object, _mockLogger.Object, _mockTrustService.Object, _mockAcademyService.Object, _mockExportService.Object);
         }
 
         [Fact]
-        public async Task OnPostExportAsync_ShouldReturnFileResult_WhenUidIsValid()
+        public async Task OnGetExportAsync_ShouldReturnFileResult_WhenUidIsValid()
         {
             // Arrange
             string uid = "1234";
@@ -46,10 +46,11 @@ namespace DfE.FindInformationAcademiesTrusts.UnitTests.Pages.Trusts.Academies
                                   null,
                                   "Open");
             var trustSummary = new TrustSummaryServiceModel(uid, "Sample Trust", "Multi-academy trust", 0);
+            var expectedBytes = new byte[] { 1, 2, 3 };
 
             _mockTrustProvider.Setup(x => x.GetTrustByUidAsync(uid)).ReturnsAsync(trust);
             _mockTrustService.Setup(x => x.GetTrustSummaryAsync(uid)).ReturnsAsync(trustSummary);
-            _mockExportService.Setup(x => x.ExportAcademiesToSpreadsheetUsingProvider(trust, trustSummary)).Returns([]);
+            _mockExportService.Setup(x => x.ExportAcademiesToSpreadsheetUsingProvider(trust, trustSummary)).Returns(expectedBytes);
 
             // Act
             var result = await _sut.OnGetExportAsync(uid);
@@ -58,6 +59,7 @@ namespace DfE.FindInformationAcademiesTrusts.UnitTests.Pages.Trusts.Academies
             result.Should().BeOfType<FileContentResult>();
             var fileResult = result as FileContentResult;
             fileResult?.ContentType.Should().Be("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+            fileResult?.FileContents.Should().BeEquivalentTo(expectedBytes);
         }
 
     }
