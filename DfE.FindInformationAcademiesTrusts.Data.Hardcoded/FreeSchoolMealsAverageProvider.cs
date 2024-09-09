4cce79e081c85b8fa7b6b0dbd6ae03e78e774fae
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2024-09-09 14:15:12 +0100

    Add methods to `FreeSchoolMealsAverageProvider` to get averages without giving full academy objects
    Also refactor `GetPhaseTypeKey` to be a little easier to read

--- DfE.FindInformationAcademiesTrusts.Data.Hardcoded/FreeSchoolMealsAverageProvider.cs
+++ DfE.FindInformationAcademiesTrusts.Data.Hardcoded/FreeSchoolMealsAverageProvider.cs
@@ -9,16 +9,26 @@ public class FreeSchoolMealsAverageProvider : IFreeSchoolMealsAverageProvider
 {
     private const int NationalKey = -1;
 
+    public double GetLaAverage(int localAuthorityCode, string? phaseOfEducation, string? typeOfEstablishment)
+    {
+        var key = GetPhaseTypeKey(phaseOfEducation, typeOfEstablishment);
+        return FreeSchoolMealsData.Averages2023To24[localAuthorityCode].PercentOfPupilsByPhase[key];
+    }
+
     public double GetLaAverage(Academy academy)
     {
-        var key = GetPhaseTypeKey(academy);
-        return FreeSchoolMealsData.Averages2023To24[academy.OldLaCode].PercentOfPupilsByPhase[key];
+        return GetLaAverage(academy.OldLaCode, academy.PhaseOfEducation, academy.TypeOfEstablishment);
+    }
+
+    public double GetNationalAverage(string? phaseOfEducation, string? typeOfEstablishment)
+    {
+        var key = GetPhaseTypeKey(phaseOfEducation, typeOfEstablishment);
+        return FreeSchoolMealsData.Averages2023To24[NationalKey].PercentOfPupilsByPhase[key];
     }
 
     public double GetNationalAverage(Academy academy)
     {
-        var key = GetPhaseTypeKey(academy);
-        return FreeSchoolMealsData.Averages2023To24[NationalKey].PercentOfPupilsByPhase[key];
+        return GetNationalAverage(academy.PhaseOfEducation, academy.TypeOfEstablishment);
     }
 
     public DataSource GetFreeSchoolMealsUpdated()
@@ -27,32 +37,40 @@ public class FreeSchoolMealsAverageProvider : IFreeSchoolMealsAverageProvider
             UpdateFrequency.Annually);
     }
 
-    public static ExploreEducationStatisticsPhaseType GetPhaseTypeKey(Academy academy)
+    public static ExploreEducationStatisticsPhaseType GetPhaseTypeKey(string? phaseOfEducation,
+        string? typeOfEstablishment)
     {
-        var key = (academy.PhaseOfEducation?.ToLower(), academy.TypeOfEstablishment?.ToLower()) switch
-        {
-            ("primary" or "middle deemed primary",
-                "community school" or "voluntary aided school" or "foundation school" or "voluntary controlled school"
-                or "academy sponsor led" or "academy converter" or "city technology college" or "free schools"
-                or "university technical college" or "studio schools")
-                => ExploreEducationStatisticsPhaseType.StateFundedPrimary,
-            ("secondary" or "middle deemed secondary" or "16 plus" or "not applicable" or "all-through",
-                "community school" or "voluntary aided school" or "foundation school" or "voluntary controlled school"
-                or "academy 16-19 converter" or "academy sponsor led" or "academy converter"
-                or "city technology college" or "free schools" or "free schools 16 to 19"
-                or "university technical college" or "studio schools" or "academy 16 to 19 sponsor led")
-                => ExploreEducationStatisticsPhaseType.StateFundedSecondary,
-            (_,
-                "foundation special school" or "community special school" or "academy special converter"
-                or "academy special sponsor led" or "free schools special")
-                => ExploreEducationStatisticsPhaseType.StateFundedSpecialSchool,
-            (_,
-                "pupil referral unit" or "academy alternative provision sponsor led" or
-                "free schools alternative provision" or "academy alternative provision converter")
-                => ExploreEducationStatisticsPhaseType.StateFundedApSchool,
-            _ => throw new ArgumentOutOfRangeException(nameof(academy),
-                $"Can't get ExploreEducationStatisticsPhaseType for [{nameof(academy.PhaseOfEducation)}:{academy.PhaseOfEducation}, {nameof(academy.TypeOfEstablishment)}:{academy.TypeOfEstablishment}]")
-        };
-        return key;
+        return (phaseOfEducation: phaseOfEducation?.ToLower(),
+                typeOfEstablishment: typeOfEstablishment?.ToLower()) switch
+            {
+                {
+                    phaseOfEducation: "primary" or "middle deemed primary",
+                    typeOfEstablishment: "community school" or "voluntary aided school" or "foundation school"
+                    or "voluntary controlled school" or "academy sponsor led" or "academy converter"
+                    or "city technology college" or "free schools" or "university technical college" or "studio schools"
+                } => ExploreEducationStatisticsPhaseType.StateFundedPrimary,
+
+                {
+                    phaseOfEducation: "secondary" or "middle deemed secondary" or "16 plus" or "not applicable"
+                    or "all-through",
+                    typeOfEstablishment: "community school" or "voluntary aided school" or "foundation school"
+                    or "voluntary controlled school" or "academy 16-19 converter" or "academy sponsor led"
+                    or "academy converter" or "city technology college" or "free schools" or "free schools 16 to 19"
+                    or "university technical college" or "studio schools" or "academy 16 to 19 sponsor led"
+                } => ExploreEducationStatisticsPhaseType.StateFundedSecondary,
+
+                {
+                    typeOfEstablishment: "foundation special school" or "community special school"
+                    or "academy special converter" or "academy special sponsor led" or "free schools special"
+                } => ExploreEducationStatisticsPhaseType.StateFundedSpecialSchool,
+
+                {
+                    typeOfEstablishment: "pupil referral unit" or "academy alternative provision sponsor led"
+                    or "free schools alternative provision" or "academy alternative provision converter"
+                } => ExploreEducationStatisticsPhaseType.StateFundedApSchool,
+
+                _ => throw new ArgumentOutOfRangeException(nameof(phaseOfEducation),
+                    $"Can't get ExploreEducationStatisticsPhaseType for [{nameof(phaseOfEducation)}:{phaseOfEducation}, {nameof(typeOfEstablishment)}:{typeOfEstablishment}]")
+            };
     }
 }
