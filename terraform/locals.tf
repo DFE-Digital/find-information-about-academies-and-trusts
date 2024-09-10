ce631aabf6985dbf0395c1ae37e273ea725c03a1
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-09-10 15:55:07 +0100

    Set permitted network access rules to Storage Account (#483)

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -67,4 +67,5 @@ locals {
   statuscake_contact_group_integrations           = var.statuscake_contact_group_integrations
   statuscake_contact_group_email_addresses        = var.statuscake_contact_group_email_addresses
   enable_container_app_file_share                 = var.enable_container_app_file_share
+  storage_account_ipv4_allow_list                 = var.storage_account_ipv4_allow_list
 }
