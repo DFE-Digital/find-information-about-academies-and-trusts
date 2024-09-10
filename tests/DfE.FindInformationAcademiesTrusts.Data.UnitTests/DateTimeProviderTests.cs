837e39f938c905174054192feab45be35fba33f4
Dominic NEED <Dominic.NEED@EDUCATION.GOV.UK>
2024-09-10 12:27:20 +0100

    Added rudimentary test for DateTimeProvider

---
+++ tests/DfE.FindInformationAcademiesTrusts.Data.UnitTests/DateTimeProviderTests.cs
@@ -0,0 +1,20 @@
+﻿namespace DfE.FindInformationAcademiesTrusts.Data.UnitTests;
+
+public class DateTimeProviderTests
+{
+    [Fact]
+    public void Now_ShouldReturnCurrentDateTime()
+    {
+        // Arrange
+        IDateTimeProvider dateTimeProvider = new DateTimeProvider();
+        DateTime beforeNow = DateTime.Now;
+
+        // Act
+        DateTime result = dateTimeProvider.Now;
+        DateTime afterNow = DateTime.Now;
+
+        // Assert
+        result.Should().BeOnOrAfter(beforeNow);
+        result.Should().BeOnOrBefore(afterNow);
+    }
+}
