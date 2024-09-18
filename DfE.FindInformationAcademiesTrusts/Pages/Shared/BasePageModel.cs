5e02c6d82f228e4d250493777029f4ed1fc27743
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-09-18 16:55:28 +0100

    Make keywords nullable so it doesn't interfere with form validation on edit contacts form

--- DfE.FindInformationAcademiesTrusts/Pages/Shared/BasePageModel.cs
+++ DfE.FindInformationAcademiesTrusts/Pages/Shared/BasePageModel.cs
@@ -6,5 +6,5 @@ namespace DfE.FindInformationAcademiesTrusts.Pages.Shared;
 public abstract class BasePageModel : PageModel
 {
     public bool ShowHeaderSearch { get; init; } = true;
-    [BindProperty(SupportsGet = true)] public string KeyWords { get; set; } = string.Empty;
+    [BindProperty(SupportsGet = true)] public string? KeyWords { get; set; } = string.Empty;
 }
