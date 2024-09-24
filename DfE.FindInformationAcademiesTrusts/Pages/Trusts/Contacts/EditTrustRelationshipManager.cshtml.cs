831ef9e6d3ca66513ca2ee072e92b7d3d5f37831
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-09-24 15:46:27 +0100

    Refactor EditTrustRelationshipManager page model to remove OnGetAsync and change page name to ContactRole

--- DfE.FindInformationAcademiesTrusts/Pages/Trusts/Contacts/EditTrustRelationshipManager.cshtml.cs
+++ DfE.FindInformationAcademiesTrusts/Pages/Trusts/Contacts/EditTrustRelationshipManager.cshtml.cs
@@ -1,8 +1,8 @@
 using DfE.FindInformationAcademiesTrusts.Configuration;
 using DfE.FindInformationAcademiesTrusts.Data;
+using DfE.FindInformationAcademiesTrusts.Data.Enums;
 using DfE.FindInformationAcademiesTrusts.Services.DataSource;
 using DfE.FindInformationAcademiesTrusts.Services.Trust;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.FeatureManagement.Mvc;
 
 namespace DfE.FindInformationAcademiesTrusts.Pages.Trusts.Contacts;
@@ -14,19 +14,4 @@ public class EditTrustRelationshipManagerModel(
     ILogger<EditTrustRelationshipManagerModel> logger,
     ITrustService trustService)
     : EditContactModel(trustProvider, dataSourceService, trustService,
-        logger, "Trust relationship manager")
-{
-    public override async Task<IActionResult> OnGetAsync()
-    {
-        var pageResult = await base.OnGetAsync();
-
-        if (pageResult.GetType() == typeof(NotFoundResult)) return pageResult;
-
-        var contacts = await TrustService.GetTrustContactsAsync(Uid);
-
-        Email = contacts.TrustRelationshipManager?.Email;
-        Name = contacts.TrustRelationshipManager?.FullName;
-
-        return pageResult;
-    }
-}
+        logger, ContactRole.TrustRelationshipManager);
