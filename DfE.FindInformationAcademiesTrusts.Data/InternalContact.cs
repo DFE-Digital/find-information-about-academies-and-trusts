de082420a7bc9051abc83767b33bb8aaa13bc09f
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-09-24 15:24:49 +0100

    Create InternalContact record

---
+++ DfE.FindInformationAcademiesTrusts.Data/InternalContact.cs
@@ -0,0 +1,8 @@
+﻿namespace DfE.FindInformationAcademiesTrusts.Data;
+
+public record InternalContact(
+    string FullName,
+    string Email,
+    DateTime LastModifiedAtTime,
+    string LastModifiedByEmail
+) : Person(FullName, Email);
