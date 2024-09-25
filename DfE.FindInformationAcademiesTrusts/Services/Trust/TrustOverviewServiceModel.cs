79b3ff4773d5550294c48839c66ebfd8f4c311bd
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2024-09-25 12:18:14 +0100

    Change TrustOverviewServiceModel from class to record
    This follows the pattern of the other service models, reducing code and allowing features such as deconstruction and the `with` keyword

--- DfE.FindInformationAcademiesTrusts/Services/Trust/TrustOverviewServiceModel.cs
+++ DfE.FindInformationAcademiesTrusts/Services/Trust/TrustOverviewServiceModel.cs
@@ -1,21 +1,14 @@
 ﻿using DfE.FindInformationAcademiesTrusts.Data;
 
-namespace DfE.FindInformationAcademiesTrusts.Services.Trust
+namespace DfE.FindInformationAcademiesTrusts.Services.Trust;
+
+public record TrustOverviewServiceModel(
+    string Uid,
+    int TotalAcademies,
+    IReadOnlyDictionary<string, int> AcademiesByLocalAuthority,
+    int TotalPupilNumbers,
+    int TotalCapacity,
+    IReadOnlyDictionary<OfstedRatingScore, int> OfstedRatings)
 {
-    public class TrustOverviewServiceModel(
-        string uid,
-        int totalAcademies,
-        IReadOnlyDictionary<string, int> academiesByLocalAuthority,
-        int totalPupilNumbers,
-        int totalCapacity,
-        IReadOnlyDictionary<OfstedRatingScore, int> ofstedRatings)
-    {
-        public string Uid { get; } = uid;
-        public int TotalAcademies { get; } = totalAcademies;
-        public IReadOnlyDictionary<string, int> AcademiesByLocalAuthority { get; } = academiesByLocalAuthority;
-        public int TotalPupilNumbers { get; } = totalPupilNumbers;
-        public int TotalCapacity { get; } = totalCapacity;
-        public double PercentageFull => TotalCapacity > 0 ? (double)TotalPupilNumbers / TotalCapacity * 100 : 0;
-        public IReadOnlyDictionary<OfstedRatingScore, int> OfstedRatings { get; } = ofstedRatings;
-    }
+    public double PercentageFull => TotalCapacity > 0 ? (double)TotalPupilNumbers / TotalCapacity * 100 : 0;
 }
