b06d13693e67a0db44af9ab0e88fbc0d48dba172
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-09-25 15:33:45 +0100

    Create ContactRoleExtensions to map contact role to display string

---
+++ DfE.FindInformationAcademiesTrusts/Extensions/ContactRoleExtensions.cs
@@ -0,0 +1,16 @@
+﻿using DfE.FindInformationAcademiesTrusts.Data.Enums;
+
+namespace DfE.FindInformationAcademiesTrusts.Extensions;
+
+public static class ContactRoleExtensions
+{
+    public static string MapRoleToViewString(this ContactRole role)
+    {
+        return role switch
+        {
+            ContactRole.TrustRelationshipManager => "Trust relationship manager",
+            ContactRole.SfsoLead => "SFSO (Schools financial support and oversight) lead",
+            _ => throw new ArgumentOutOfRangeException(nameof(role))
+        };
+    }
+}
