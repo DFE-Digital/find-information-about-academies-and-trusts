a66afc75b8dccd5dad03fee43dcbad1300d91207
cmurrell148 <conor.murrell@nimbleapproach.com>
2024-09-27 14:05:19 +0100

    Create new pipeline hookin, readd mocha reporter, moved out old cypress folder

--- tests/OldCypressTests/cypress/auth/authenticationInterceptor.ts
+++ tests/OldCypressTests/cypress/auth/authenticationInterceptor.ts
@@ -1,17 +1,18 @@
+import { EnvUrl, EnvAuthKey } from "../constants/cypressConstants";
 
 export class AuthenticationInterceptor {
 
     register(params?: AuthenticationInterceptorParams) {
         cy.intercept(
             {
-                url: Cypress.env("url") + "/**",
+                url: Cypress.env(EnvUrl) + "/**",
                 middleware: true,
             },
             (req) => {
                 // Set an auth header on every request made by the browser
                 req.headers = {
                     ...req.headers,
-                    'Authorization': `Bearer ${Cypress.env("authKey")}`
+                    'Authorization': `Bearer ${Cypress.env(EnvAuthKey)}`
                 };
             }
         ).as("AuthInterceptor");
---
+++ tests/OldCypressTests/cypress/support/e2e.ts
@@ -0,0 +1,30 @@
+// ***********************************************************
+// This example support/index.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import { AuthenticationInterceptorParams } from 'cypress/auth/authenticationInterceptor';
+import './commands'
+
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            getByTestId(id: string): Chainable<Element>;
+            containsByTestId(id: string): Chainable<Element>;
+            getById(id: string): Chainable<Element>;
+            login(params?: AuthenticationInterceptorParams): Chainable<Element>;
+            excuteAccessibilityTests(): Chainable<Element>;
+        }
+    }
+}
