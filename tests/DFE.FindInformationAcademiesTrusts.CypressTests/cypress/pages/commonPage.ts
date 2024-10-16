764a068bb450caae0b9d0088eeed8f8e5cc86992
CMurrell148 <conor.murrell@nimbleapproach.com>
2024-10-16 17:23:02 +0100

    Resolve Nicks PR comments

--- tests/DFE.FindInformationAcademiesTrusts.CypressTests/cypress/pages/commonPage.ts
+++ tests/DFE.FindInformationAcademiesTrusts.CypressTests/cypress/pages/commonPage.ts
@@ -11,10 +11,6 @@ class CommonPage {
             Message: () => this.elements.ErrorPopup.Section().find('[data-testid="error-summary"]')
         },
 
-        Datasource: {
-            Section: () => cy.get('.govuk-details'),
-            LatestUpdatedBy: () => cy.get('[data-testid="data-source-fiatdb-trust-relationship-manager"] > :nth-child(3)')
-        }
     };
 
     public checkSuccessPopup(expectedMessage: string): this {
@@ -31,12 +27,7 @@ class CommonPage {
         return this;
     }
 
-    public checkLatestDatasourceUser(expectedMessage: string): this {
-        const { Datasource } = this.elements;
-        Datasource.Section().should('be.visible');
-        Datasource.LatestUpdatedBy().should('contain.text', expectedMessage);
-        return this;
-    }
+
 }
 
 const commonPage = new CommonPage();
