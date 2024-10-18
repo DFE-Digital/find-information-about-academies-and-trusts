55da68a34b5f6ad374372cf5fcc56fa085e5bbd0
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2024-10-18 14:06:38 +0100

    Default automated tests user to have the AuthorisedFiatUser role
    Also evaluate once for all requirements rather than once per requirement
    so we don't have to create the test user multiple times and so we don't
    have to update this every time we add a new requirement

--- DfE.FindInformationAcademiesTrusts/Authorization/AutomationAuthorizationHandler.cs
+++ DfE.FindInformationAcademiesTrusts/Authorization/AutomationAuthorizationHandler.cs
@@ -1,9 +1,9 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Security.Claims;
+using DfE.FindInformationAcademiesTrusts.Configuration;
 using DfE.FindInformationAcademiesTrusts.Extensions;
 using DfE.FindInformationAcademiesTrusts.Options;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.Extensions.Options;
 using Microsoft.Net.Http.Headers;
 
@@ -13,8 +13,7 @@ public class AutomationAuthorizationHandler(
     IWebHostEnvironment environment,
     IHttpContextAccessor httpContextAccessor,
     IOptions<TestOverrideOptions> testOverrideOptions)
-    : AuthorizationHandler<DenyAnonymousAuthorizationRequirement>,
-        IAuthorizationRequirement
+    : AuthorizationHandler<IAuthorizationRequirement>
 {
     private readonly string? _cypressTestSecret = testOverrideOptions.Value.CypressTestSecret;
     private readonly bool _isLiveEnvironment = environment.IsLiveEnvironment();
@@ -38,23 +37,31 @@ public class AutomationAuthorizationHandler(
         var identity = new ClaimsIdentity(new List<Claim>
         {
             new("name", "Automation User - name"),
-            new("preferred_username", "Automation User - email")
+            new("preferred_username", "Automation User - email"),
+            new(ClaimTypes.Role, UserRoles.AuthorisedFiatUser)
         });
         var user = new ClaimsPrincipal(identity);
 
         httpContextAccessor.HttpContext!.User = user;
     }
 
-    [ExcludeFromCodeCoverage] // This method is difficult to test, everything that can be tested has been extracted to IsClientSecretHeaderValid
-    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
-        DenyAnonymousAuthorizationRequirement requirement)
+    [ExcludeFromCodeCoverage] // This method is difficult to test, everything that can be tested has been extracted to other public methods
+    public override Task HandleAsync(AuthorizationHandlerContext context)
     {
         if (IsClientSecretHeaderValid())
         {
             SetupAutomationUser();
-            context.Succeed(requirement);
+            foreach (var requirement in context.Requirements)
+                context.Succeed(requirement);
         }
 
         return Task.CompletedTask;
+    }
+
+    [ExcludeFromCodeCoverage] // This method is difficult to test because it is protected and not invoked
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+        IAuthorizationRequirement requirement)
+    {
+        throw new NotImplementedException();
     }
 }
