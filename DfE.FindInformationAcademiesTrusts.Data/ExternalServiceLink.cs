13c3038648a8701c51e00aa94d74219b0d1202bc
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-10-24 14:06:16 +0100

    Exclude code from unneccesary coverage checks

--- DfE.FindInformationAcademiesTrusts.Data/ExternalServiceLink.cs
+++ DfE.FindInformationAcademiesTrusts.Data/ExternalServiceLink.cs
@@ -1,3 +1,6 @@
-﻿namespace DfE.FindInformationAcademiesTrusts.Data;
+﻿using System.Diagnostics.CodeAnalysis;
 
+namespace DfE.FindInformationAcademiesTrusts.Data;
+
+[ExcludeFromCodeCoverage]
 public record ExternalServiceLink(string ServiceName, string ServiceDescription, string Url);
