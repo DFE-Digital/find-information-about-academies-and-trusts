39b03b6afc6ed69b751a533b85c86fcdb96b5f0e
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-10-24 11:20:05 +0100

    Add ExternalServiceLinks to view constants

--- DfE.FindInformationAcademiesTrusts/Pages/ViewConstants.cs
+++ DfE.FindInformationAcademiesTrusts/Pages/ViewConstants.cs
@@ -1,3 +1,5 @@
+using DfE.FindInformationAcademiesTrusts.Data;
+
 namespace DfE.FindInformationAcademiesTrusts.Pages;
 
 public static class ViewConstants
@@ -21,4 +23,33 @@ public static class ViewConstants
         "https://forms.office.com/Pages/ResponsePage.aspx?id=yXfS-grGoU2187O4s0qC-fkHK2JGo_BIpVChpLMaBFpUNUFDSzhQN0FHVklTV0JWTzFZTjNKWTNJUi4u";
 
     public const string NoDataText = "No Data";
+
+    public static readonly List<ExternalServiceLink> ExternalServiceLinks =
+    [
+        new ExternalServiceLink("Prepare conversions and transfers",
+            "Create a transfer or conversion project document for an advisory board.",
+            "https://educationgovuk.sharepoint.com/sites/lvewp00299/SitePages/Prepare-Conversions-and-Transfers.aspx"),
+
+        new ExternalServiceLink("Complete conversions, transfers and changes",
+            "Manage a conversion or transfer project after it has been to an advisory board.",
+            "https://educationgovuk.sharepoint.com/sites/lvewp00299/SitePages/complete-conversions-transfers-and-changes.aspx"),
+
+        new ExternalServiceLink("Record concerns or support for trusts",
+            "Add cases or interactions, record risks and log support and concerns for trusts.",
+            "https://educationgovuk.sharepoint.com/sites/lvewp00299/SitePages/Record-concerns-and-support-for-trusts.aspx"),
+
+        new ExternalServiceLink("Manage free school projects",
+            "Manage presumption projects in the pre-opening phase.",
+            "https://educationgovuk.sharepoint.com/sites/lvewp00299/SitePages/Manage-free-school-projects.aspx?web=1"),
+
+        new ExternalServiceLink("Reporting and data tools",
+            "Tools to help you gather educational data and create reports.",
+            "https://educationgovuk.sharepoint.com/sites/lvewp00299/SitePages/reportinganddata.aspx"),
+
+        new ExternalServiceLink("High quality trust framework",
+            "Process guidance and tools for making trust-related project decisions.",
+            "https://educationgovuk.sharepoint.com/sites/lvewp00299/SitePages/RG%20high%20quality%20trust%20framework.aspx")
+    ];
 }
+
+
