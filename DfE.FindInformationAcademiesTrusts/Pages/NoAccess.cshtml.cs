b8e2142260497d6efd5b5245c9f42ebc062d0969
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2024-10-28 17:21:33 +0000

    Add redirect to no access page
    This is to enable a log in retry for users who may have stale role claims in their auth cookie. ADR to follow

--- DfE.FindInformationAcademiesTrusts/Pages/NoAccess.cshtml.cs
+++ DfE.FindInformationAcademiesTrusts/Pages/NoAccess.cshtml.cs
@@ -1,7 +1,58 @@
+using DfE.FindInformationAcademiesTrusts.Configuration;
+using DfE.FindInformationAcademiesTrusts.Extensions;
 using DfE.FindInformationAcademiesTrusts.Pages.Shared;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DfE.FindInformationAcademiesTrusts.Pages;
 
 [AllowAnonymous]
-public class NoAccessModel : ContentPageModel;
+public class NoAccessModel : ContentPageModel
+{
+    private const string RetryingLogin = "RetryingLogin";
+
+    public ActionResult OnGet(string? returnUrl)
+    {
+        if (User.HasAccessToFiat())
+        {
+            return Url.IsLocalUrl(returnUrl) ? LocalRedirect(returnUrl) : Page();
+        }
+
+        // Users may be redirected to Access Denied because the login cookie stored in their browser contains
+        // stale information about their roles
+        // We can force an update of their role claims by removing this login cookie and redirecting them 
+        // back to their intended destination
+        if (NotRetriedLoginYet())
+        {
+            RemoveLoginCookie();
+
+            // We use temp data to ensure we don't end up in a redirect loop for genuine unauthorised users
+            TempData.Add(RetryingLogin, "true");
+
+            return Url.IsLocalUrl(returnUrl) ? LocalRedirect(returnUrl) : Page();
+        }
+
+        TempData.Remove(RetryingLogin);
+        return Page();
+    }
+
+    private bool NotRetriedLoginYet()
+    {
+        return !TempData.ContainsKey(RetryingLogin);
+    }
+
+    private void RemoveLoginCookie()
+    {
+        if (Request.Cookies.ContainsKey(FiatCookies.Login))
+        {
+            Response.Cookies.Delete(FiatCookies.Login,
+                new CookieOptions
+                {
+                    HttpOnly = true,
+                    IsEssential = true,
+                    SameSite = SameSiteMode.Strict,
+                    Secure = true
+                });
+        }
+    }
+}
---
+++ tests/DfE.FindInformationAcademiesTrusts.UnitTests/Pages/NoAccessModelTests.cs
@@ -0,0 +1,117 @@
+using DfE.FindInformationAcademiesTrusts.Configuration;
+using DfE.FindInformationAcademiesTrusts.Pages;
+using DfE.FindInformationAcademiesTrusts.UnitTests.Mocks;
+using FluentAssertions.Execution;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace DfE.FindInformationAcademiesTrusts.UnitTests.Pages;
+
+public class NoAccessModelTests
+{
+    private readonly MockHttpContext _mockHttpContext = new();
+    private readonly NoAccessModel _sut;
+    private readonly TempDataDictionary _tempDataDictionary;
+
+    private const string LocalUrl = "/search";
+    private const string ExternalUrl = "https://google.com";
+
+    public NoAccessModelTests()
+    {
+        var mockUrlHelper = new Mock<IUrlHelper>();
+        mockUrlHelper.Setup(u => u.IsLocalUrl(LocalUrl)).Returns(true);
+        mockUrlHelper.Setup(u => u.IsLocalUrl(ExternalUrl)).Returns(false);
+
+        _tempDataDictionary = new TempDataDictionary(_mockHttpContext.Object, Mock.Of<ITempDataProvider>());
+        _sut = new NoAccessModel
+        {
+            PageContext = new PageContext
+            {
+                HttpContext = _mockHttpContext.Object
+            },
+            TempData = _tempDataDictionary,
+            Url = mockUrlHelper.Object
+        };
+    }
+
+    [Fact]
+    public void OnGet_should_redirect_to_local_return_url_when_user_is_authorised()
+    {
+        var result = _sut.OnGet(LocalUrl);
+
+        result.Should().BeOfType<LocalRedirectResult>();
+        result.As<LocalRedirectResult>().Url.Should().Be(LocalUrl);
+    }
+
+    [Theory]
+    [InlineData(MockHttpContext.UserAuthState.Authorised)]
+    [InlineData(MockHttpContext.UserAuthState.Unauthenticated)]
+    [InlineData(MockHttpContext.UserAuthState.Unauthorised)]
+    public void OnGet_should_render_page_when_no_return_url_specified(MockHttpContext.UserAuthState authState)
+    {
+        _mockHttpContext.SetUserTo(authState);
+
+        var result = _sut.OnGet(null);
+
+        result.Should().BeOfType<PageResult>();
+    }
+
+    [Theory]
+    [InlineData(MockHttpContext.UserAuthState.Authorised)]
+    [InlineData(MockHttpContext.UserAuthState.Unauthenticated)]
+    [InlineData(MockHttpContext.UserAuthState.Unauthorised)]
+    public void OnGet_should_never_redirect_to_external_return_url(MockHttpContext.UserAuthState authState)
+    {
+        _mockHttpContext.SetUserTo(authState);
+
+        var result = _sut.OnGet(ExternalUrl);
+
+        result.Should().BeOfType<PageResult>();
+        result.Should().NotBeOfType<LocalRedirectResult>();
+    }
+
+    [Fact]
+    public void OnGet_should_prompt_login_when_user_is_unauthenticated()
+    {
+        _mockHttpContext.SetUserTo(MockHttpContext.UserAuthState.Unauthenticated);
+
+        var result = _sut.OnGet(LocalUrl);
+
+        using var scope = new AssertionScope();
+
+        result.Should().BeOfType<LocalRedirectResult>();
+        result.As<LocalRedirectResult>().Url.Should().Be(LocalUrl);
+        _tempDataDictionary.Should().ContainKey("RetryingLogin").WhoseValue.Should().Be("true");
+        _mockHttpContext.MockResponseCookies.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public void OnGet_should_retry_login_when_user_is_unauthorised_and_not_retried_login()
+    {
+        _mockHttpContext.SetUserTo(MockHttpContext.UserAuthState.Unauthorised);
+
+        var result = _sut.OnGet(LocalUrl);
+
+        using var scope = new AssertionScope();
+
+        result.Should().BeOfType<LocalRedirectResult>();
+        result.As<LocalRedirectResult>().Url.Should().Be(LocalUrl);
+        _tempDataDictionary.Should().ContainKey("RetryingLogin").WhoseValue.Should().Be("true");
+        _mockHttpContext.MockResponseCookies.VerifyCookieDeleted(FiatCookies.Login,
+            new CookieOptions { HttpOnly = true, IsEssential = true, SameSite = SameSiteMode.Strict, Secure = true });
+    }
+
+    [Fact]
+    public void OnGet_should_return_no_access_page_when_user_is_unauthorised_and_has_retried_login()
+    {
+        _mockHttpContext.SetUserTo(MockHttpContext.UserAuthState.Unauthorised);
+        _tempDataDictionary.Add("RetryingLogin", "true");
+
+        var result = _sut.OnGet(LocalUrl);
+
+        result.Should().BeOfType<PageResult>();
+        _tempDataDictionary.Should().NotContainKey("RetryingLogin");
+    }
+}
