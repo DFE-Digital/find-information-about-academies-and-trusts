0eaa92f625ecd859e7454503cccd0379968ec164
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2024-10-30 11:54:22 +0000

    Share an anonymous page model between always accessible pages
    Reduces code, makes it easier to see which pages have anonymous access and aggregates the behavior into one place

--- DfE.FindInformationAcademiesTrusts/Pages/Accessibility.cshtml.cs
+++
@@ -1,7 +0,0 @@
-using DfE.FindInformationAcademiesTrusts.Pages.Shared;
-using Microsoft.AspNetCore.Authorization;
-
-namespace DfE.FindInformationAcademiesTrusts.Pages;
-
-[AllowAnonymous]
-public class AccessibilityModel : ContentPageModel;
