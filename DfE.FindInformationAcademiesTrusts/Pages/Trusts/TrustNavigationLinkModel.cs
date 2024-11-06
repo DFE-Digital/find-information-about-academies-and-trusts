9c3701f99301e9ba1315828b1868247631a0bc62
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-11-06 11:17:11 +0000

    CReate TrustNavigationLinkModel

---
+++ DfE.FindInformationAcademiesTrusts/Pages/Trusts/TrustNavigationLinkModel.cs
@@ -0,0 +1,3 @@
+﻿namespace DfE.FindInformationAcademiesTrusts.Pages.Trusts;
+
+public record TrustNavigationLinkModel(string LinkText, string Page, string Uid, bool LinkIsActive, string DataTestId);
