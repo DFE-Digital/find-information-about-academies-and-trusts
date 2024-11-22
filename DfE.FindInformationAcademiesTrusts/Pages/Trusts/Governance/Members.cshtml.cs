f53ed3cdc1bbb126de659fa4dd32a36876d1e7b7
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-11-22 16:01:27 +0000

    Create members page

---
+++ DfE.FindInformationAcademiesTrusts/Pages/Trusts/Governance/Members.cshtml.cs
@@ -0,0 +1,10 @@
+﻿using DfE.FindInformationAcademiesTrusts.Services.DataSource;
+using DfE.FindInformationAcademiesTrusts.Services.Trust;
+
+namespace DfE.FindInformationAcademiesTrusts.Pages.Trusts.Governance;
+
+public class MembersModel(
+    IDataSourceService dataSourceService,
+    ILogger<MembersModel> logger,
+    ITrustService trustService)
+    : GovernanceAreaModel(dataSourceService, trustService, logger, item => item.Page == "./Members");
