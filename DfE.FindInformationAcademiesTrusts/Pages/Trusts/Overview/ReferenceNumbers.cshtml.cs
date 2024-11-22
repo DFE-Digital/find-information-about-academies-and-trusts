136aa0261d902eaab71364bc92eb149571a18872
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-11-22 16:00:12 +0000

    Create Trust Summary, Trust Details and reference Numbers page

---
+++ DfE.FindInformationAcademiesTrusts/Pages/Trusts/Overview/ReferenceNumbers.cshtml.cs
@@ -0,0 +1,10 @@
+﻿using DfE.FindInformationAcademiesTrusts.Services.DataSource;
+using DfE.FindInformationAcademiesTrusts.Services.Trust;
+
+namespace DfE.FindInformationAcademiesTrusts.Pages.Trusts.Overview;
+
+public class ReferenceNumbersModel(
+    IDataSourceService dataSourceService,
+    ILogger<ReferenceNumbersModel> logger,
+    ITrustService trustService)
+    : OverviewAreaModel(dataSourceService, trustService, logger);
