49e37828ad7aeb957530bbec2ba96dcf91c06f98
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-11-22 16:02:30 +0000

    Create in Dfe page

---
+++ DfE.FindInformationAcademiesTrusts/Pages/Trusts/Contacts/InDfe.cshtml.cs
@@ -0,0 +1,10 @@
+using DfE.FindInformationAcademiesTrusts.Services.DataSource;
+using DfE.FindInformationAcademiesTrusts.Services.Trust;
+
+namespace DfE.FindInformationAcademiesTrusts.Pages.Trusts.Contacts;
+
+public class InDfeModel(
+    IDataSourceService dataSourceService,
+    ITrustService trustService,
+    ILogger<InDfeModel> logger)
+    : ContactAreaModel(dataSourceService, trustService, logger, item => item.Page == "./InDfE");
