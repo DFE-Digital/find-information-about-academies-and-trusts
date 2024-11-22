9b5c187378c2d01457ffe6003fe9a7b724b40a1d
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-11-22 16:00:30 +0000

    Create Trust Details page

---
+++ DfE.FindInformationAcademiesTrusts/Pages/Trusts/Overview/TrustDetails.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using DfE.FindInformationAcademiesTrusts.Services.DataSource;
+using DfE.FindInformationAcademiesTrusts.Services.Trust;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DfE.FindInformationAcademiesTrusts.Pages.Trusts.Overview;
+
+public class TrustDetailsModel(
+    IDataSourceService dataSourceService,
+    ILogger<TrustDetailsModel> logger,
+    ITrustService trustService,
+    IOtherServicesLinkBuilder otherServicesLinkBuilder)
+    : OverviewAreaModel(dataSourceService, trustService, logger, item => item.Page == "./TrustDetails")
+{
+    public string? CompaniesHouseLink { get; set; }
+    public string? GetInformationAboutSchoolsLink { get; set; }
+    public string? SchoolsFinancialBenchmarkingLink { get; set; }
+    public string? FindSchoolPerformanceLink { get; set; }
+
+    public override async Task<IActionResult> OnGetAsync()
+    {
+        var pageResult = await base.OnGetAsync();
+        if (pageResult is NotFoundResult) return pageResult;
+
+        // Setup external links
+        CompaniesHouseLink = otherServicesLinkBuilder.CompaniesHouseListingLink(TrustOverview.CompaniesHouseNumber);
+        GetInformationAboutSchoolsLink =
+            otherServicesLinkBuilder.GetInformationAboutSchoolsListingLinkForTrust(TrustOverview.Uid);
+        SchoolsFinancialBenchmarkingLink =
+            otherServicesLinkBuilder.SchoolFinancialBenchmarkingServiceListingLink(TrustOverview.Type,
+                TrustOverview.SingleAcademyTrustAcademyUrn, TrustOverview.CompaniesHouseNumber);
+        FindSchoolPerformanceLink =
+            otherServicesLinkBuilder.FindSchoolPerformanceDataListingLink(TrustOverview.Uid, TrustOverview.Type,
+                TrustOverview.SingleAcademyTrustAcademyUrn);
+
+        return Page();
+    }
+}
