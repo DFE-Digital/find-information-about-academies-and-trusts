7ec5fa2f65e6a8b2d40332517ff7e8aad8de7e95
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-11-22 16:01:05 +0000

    Create Trust Leadership, trustees, members and historic members page

---
+++ DfE.FindInformationAcademiesTrusts/Pages/Trusts/Governance/Trustees.cshtml.cs
@@ -0,0 +1,10 @@
+﻿using DfE.FindInformationAcademiesTrusts.Services.DataSource;
+using DfE.FindInformationAcademiesTrusts.Services.Trust;
+
+namespace DfE.FindInformationAcademiesTrusts.Pages.Trusts.Governance;
+
+public class TrusteesModel(
+    IDataSourceService dataSourceService,
+    ILogger<TrusteesModel> logger,
+    ITrustService trustService)
+    : GovernanceAreaModel(dataSourceService, trustService, logger);
