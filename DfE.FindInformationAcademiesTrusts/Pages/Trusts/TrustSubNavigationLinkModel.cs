b7ab911f5a335a568934377facf6caf60c27c599
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-11-22 15:57:58 +0000

    Create sub navigation partials

---
+++ DfE.FindInformationAcademiesTrusts/Pages/Trusts/TrustSubNavigationLinkModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DfE.FindInformationAcademiesTrusts.Pages.Trusts;
+
+public record TrustSubNavigationLinkModel(
+    string LinkText,
+    string Page,
+    string Uid,
+    string ServiceName,
+    bool LinkIsActive)
+{
+    public string TestId =>
+        $"{ServiceName.ToLowerInvariant().Trim().Replace(" ", "-")}-{LinkText.ToLowerInvariant().Trim().Replace(" ", "-")}-subnav";
+}
