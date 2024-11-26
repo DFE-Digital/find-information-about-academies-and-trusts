d8d5945aaa60504a282b7866112272a7a3a8786e
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-11-26 16:47:37 +0000

    Create SafeguardingAndConcernsExtensions

---
+++ DfE.FindInformationAcademiesTrusts/Extensions/SafeguardingScoreExtensions.cs
@@ -0,0 +1,23 @@
+﻿using DfE.FindInformationAcademiesTrusts.Data;
+
+namespace DfE.FindInformationAcademiesTrusts.Extensions;
+
+public static class SafeguardingScoreExtensions
+{
+    public static string ToDataSortValue(this SafeguardingScore rating)
+    {
+        return rating.ToDisplayString().ToLowerInvariant().Trim();
+    }
+
+    public static string ToDisplayString(this SafeguardingScore score)
+    {
+        return score switch
+        {
+            SafeguardingScore.None => "None",
+            SafeguardingScore.Yes => "Yes",
+            SafeguardingScore.No => "No",
+            SafeguardingScore.NotRecorded => "Not Recorded",
+            _ => "Unknown"
+        };
+    }
+}
