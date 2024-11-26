819a1e38c2c6fd82bb4dd43d9e440da5c702fb6c
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-11-26 16:46:53 +0000

    Create CategoriesOfConcernExtensions

---
+++ DfE.FindInformationAcademiesTrusts/Extensions/CategoriesOfConcernExtensions.cs
@@ -0,0 +1,23 @@
+﻿using DfE.FindInformationAcademiesTrusts.Data;
+
+namespace DfE.FindInformationAcademiesTrusts.Extensions;
+
+public static class CategoriesOfConcernExtensions
+{
+    public static string ToDataSortValue(this CategoriesOfConcern rating)
+    {
+        return rating.ToDisplayString().ToLowerInvariant().Trim();
+    }
+
+    public static string ToDisplayString(this CategoriesOfConcern rating)
+    {
+        return rating switch
+        {
+            CategoriesOfConcern.None => "None",
+            CategoriesOfConcern.SpecialMeasures => "Special Measures",
+            CategoriesOfConcern.SeriousWeakness => "Serious Weakness",
+            CategoriesOfConcern.NoticeToImprove => "Notice To Improve",
+            _ => "Unknown"
+        };
+    }
+}
