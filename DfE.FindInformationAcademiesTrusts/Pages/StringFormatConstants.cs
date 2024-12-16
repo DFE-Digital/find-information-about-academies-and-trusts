7487c32c32339d435b795572e8de9c5de27d28c7
dneed <113213942+dneed-nimble@users.noreply.github.com>
2024-12-16 10:00:25 +0000

    Update DfE.FindInformationAcademiesTrusts/Pages/StringFormatConstants.cs
    Co-authored-by: Nick Warms <128540970+nwarms@users.noreply.github.com>

--- DfE.FindInformationAcademiesTrusts/Pages/StringFormatConstants.cs
+++ DfE.FindInformationAcademiesTrusts/Pages/StringFormatConstants.cs
@@ -2,6 +2,6 @@ namespace DfE.FindInformationAcademiesTrusts.Pages;
 
 public static class StringFormatConstants
 {
-    public const string FullDateFormat = "dd MMM yyyy";
+    public const string DisplayDateFormat = "dd MMM yyyy";
     public const string SortableDateFormat = "yyyyMMdd";
 }
