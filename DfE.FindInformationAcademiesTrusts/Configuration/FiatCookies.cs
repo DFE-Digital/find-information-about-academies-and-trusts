588436b1a8ca2a8985f643d84b31bb7a2dfa5520
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2024-12-19 12:08:37 +0000

    Make consent cookie name consistent with other names

--- DfE.FindInformationAcademiesTrusts/Configuration/FiatCookies.cs
+++ DfE.FindInformationAcademiesTrusts/Configuration/FiatCookies.cs
@@ -3,6 +3,6 @@ namespace DfE.FindInformationAcademiesTrusts.Configuration;
 public static class FiatCookies
 {
     public const string Antiforgery = ".FindInformationAcademiesTrusts.Antiforgery";
-    public const string CookieConsent = ".FindInformationAcademiesTrust.CookieConsent";
+    public const string CookieConsent = ".FindInformationAcademiesTrusts.CookieConsent";
     public const string Login = ".FindInformationAcademiesTrusts.Login";
 }
