5721bd2058b0aa057d9dadc31361fbcb41dbfe8b
Dominic NEED <Dominic.NEED@EDUCATION.GOV.UK>
2025-01-27 09:58:11 +0000

    Adding Pipeline FreeSchools, Pre and Post Advisory to the DbContext

---
+++ DfE.FindInformationAcademiesTrusts.Data.AcademiesDb/Contexts/AcademiesDbContext.MstrFreeSchoolProjects.cs
@@ -0,0 +1,61 @@
+﻿using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Models.Mstr;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Contexts;
+public partial class AcademiesDbContext
+{
+    public DbSet<MstrFreeSchoolProject> MstrFreeSchoolProjects { get; set; }
+
+    [ExcludeFromCodeCoverage]
+    protected static void OnModelCreatingMstrFreeSchoolProjects(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<MstrFreeSchoolProject>(entity =>
+        {
+            entity
+                .HasKey(e => e.SK);
+
+            entity.ToTable("FreeSchoolProjects", "mstr");
+
+            entity.Property(e => e.ProjectID)
+                .HasColumnName("Project ID");
+
+            entity.Property(e => e.ProjectName)
+                .HasColumnName("Project Name");
+
+            entity.Property(e => e.ProjectApplicationType)
+                .HasColumnName("Project Application Type");
+
+            entity.Property(e => e.LocalAuthority)
+                .HasColumnName("Local Authority");
+
+            entity.Property(e => e.RouteOfProject)
+                .HasColumnName("Route of Project");
+
+            entity.Property(e => e.StatutoryLowestAge)
+                .HasColumnName("Statutory Lowest Age");
+
+            entity.Property(e => e.StatutoryHighestAge)
+                .HasColumnName("Statutory Highest Age");
+
+            entity.Property(e => e.EstablishmentName)
+                .HasColumnName("Establishment Name");
+
+            entity.Property(e => e.ActualDateOpened)
+                .HasColumnName("Actual Date Opened");
+
+            entity.Property(e => e.TrustID)
+                .HasColumnName("Trust ID");
+
+            entity.Property(e => e.NewURN)
+                .HasColumnName("New URN");
+
+            entity.Property(e => e.DateSource)
+                .HasColumnName("Datasource");
+
+            entity.Property(e => e.LastDataRefresh)
+                .HasColumnName("Last Data Refresh");
+        });
+
+    }
+}
