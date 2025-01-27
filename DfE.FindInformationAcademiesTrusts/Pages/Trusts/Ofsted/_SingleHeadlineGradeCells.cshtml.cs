63a8ba596e3252528f98fd2e4ccece8850ceddef
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2025-01-27 16:04:35 +0000

    Add single headline grade display logic
    Moved logic into the cs model where possible so it can be unit tested

--- DfE.FindInformationAcademiesTrusts/Pages/Trusts/Ofsted/_SingleHeadlineGradeCells.cshtml.cs
+++ DfE.FindInformationAcademiesTrusts/Pages/Trusts/Ofsted/_SingleHeadlineGradeCells.cshtml.cs
@@ -1,5 +1,6 @@
 using DfE.FindInformationAcademiesTrusts.Data;
 using DfE.FindInformationAcademiesTrusts.Data.Enums;
+using DfE.FindInformationAcademiesTrusts.Extensions;
 
 namespace DfE.FindInformationAcademiesTrusts.Pages.Trusts.Ofsted;
 
@@ -8,5 +9,34 @@ public record SingleHeadlineGradeCellsModel(
     BeforeOrAfterJoining BeforeOrAfterJoining,
     bool IsCurrent)
 {
-    public BeforeOrAfterTagModel BeforeOrAfterTagModel { get; } = new(BeforeOrAfterJoining, IsCurrent);
+    public bool HasInspection => SingleHeadlineGradeRating.OverallEffectiveness != OfstedRatingScore.NotInspected;
+
+    public string TagLabel => BeforeOrAfterJoining switch
+    {
+        BeforeOrAfterJoining.Before => "Before joining",
+        BeforeOrAfterJoining.After => "After joining",
+        _ => "Unknown"
+    };
+
+    public bool IsBeforeJoining => BeforeOrAfterJoining == BeforeOrAfterJoining.Before;
+
+    public bool RatingShouldBeBold => SingleHeadlineGradeRating.OverallEffectiveness switch
+    {
+        OfstedRatingScore.Outstanding
+            or OfstedRatingScore.Good
+            or OfstedRatingScore.RequiresImprovement
+            or OfstedRatingScore.Inadequate => true,
+        _ => false
+    };
+
+    public string InspectionDateText => HasInspection
+        ? SingleHeadlineGradeRating.InspectionDate.ShowDateStringOrReplaceWithText()
+        : OfstedRatingScore.NotInspected.ToDisplayString(IsCurrent);
+
+    public string InspectionDateSort =>
+        SingleHeadlineGradeRating.InspectionDate?.ToString(StringFormatConstants.SortableDateFormat)
+        ?? "0";
+
+    public string SingleHeadlineGradeText => SingleHeadlineGradeRating.OverallEffectiveness.ToDisplayString(IsCurrent);
+    public int SingleHeadlineGradeSort => SingleHeadlineGradeRating.OverallEffectiveness.ToDataSortValue();
 }
