f01f1353090fc32aef619ec7d8512ef75e172f1a
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2025-03-03 15:07:04 +0000

    Add financial docs pages

---
+++ DfE.FindInformationAcademiesTrusts/Pages/Trusts/FinancialDocuments/FinancialDocumentsAreaModel.cs
@@ -0,0 +1,37 @@
+using DfE.FindInformationAcademiesTrusts.Services.DataSource;
+using DfE.FindInformationAcademiesTrusts.Services.Trust;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DfE.FindInformationAcademiesTrusts.Pages.Trusts.FinancialDocuments;
+
+public class FinancialDocumentsAreaModel(
+    IDataSourceService dataSourceService,
+    ITrustService trustService,
+    ILogger<FinancialDocumentsAreaModel> logger)
+    : TrustsAreaModel(dataSourceService, trustService, logger)
+{
+    public override TrustPageMetadata TrustPageMetadata =>
+        base.TrustPageMetadata with { PageName = ViewConstants.FinancialDocumentsPageName };
+
+    public string[] FinancialDocuments { get; set; } = ["Doc 1", "Doc 2", "Doc 3"];
+
+    public override async Task<IActionResult> OnGetAsync()
+    {
+        var pageResult = await base.OnGetAsync();
+        if (pageResult is NotFoundResult) return pageResult;
+
+        SubNavigationLinks =
+        [
+            GetSubPageLink<FinancialStatementsModel>
+                (ViewConstants.FinancialDocumentsFinancialStatementsSubPageName, "./FinancialStatements"),
+            GetSubPageLink<ManagementLettersModel>
+                (ViewConstants.FinancialDocumentsManagementLettersSubPageName, "./ManagementLetters"),
+            GetSubPageLink<InternalScrutinyReportsModel>
+                (ViewConstants.FinancialDocumentsInternalScrutinyReportsSubPageName, "./InternalScrutinyReports"),
+            GetSubPageLink<SelfAssessmentChecklistsModel>
+                (ViewConstants.FinancialDocumentsSelfAssessmentChecklistsSubPageName, "./SelfAssessmentChecklists")
+        ];
+
+        return Page();
+    }
+}
---
+++ DfE.FindInformationAcademiesTrusts/Pages/Trusts/FinancialDocuments/InternalScrutinyReports.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using DfE.FindInformationAcademiesTrusts.Services.DataSource;
+using DfE.FindInformationAcademiesTrusts.Services.Trust;
+
+namespace DfE.FindInformationAcademiesTrusts.Pages.Trusts.FinancialDocuments;
+
+public class InternalScrutinyReportsModel(
+    IDataSourceService dataSourceService,
+    ILogger<InternalScrutinyReportsModel> logger,
+    ITrustService trustService)
+    : FinancialDocumentsAreaModel(dataSourceService, trustService, logger)
+{
+    public override TrustPageMetadata TrustPageMetadata => base.TrustPageMetadata with
+    {
+        SubPageName = ViewConstants.FinancialDocumentsInternalScrutinyReportsSubPageName
+    };
+}
