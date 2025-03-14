6aa82f9efd8f501d1c0ec76d817dfd8347e365c1
Linzi CARLIN <Linzi.CARLIN@EDUCATION.GOV.UK>
2025-03-14 13:59:36 +0000

    revert accidental change

--- DfE.FindInformationAcademiesTrusts.Data/TrustSearchEntry.cs
+++ DfE.FindInformationAcademiesTrusts.Data/TrustSearchEntry.cs
@@ -1,11 +1,3 @@
-using System.Collections;
-
 namespace DfE.FindInformationAcademiesTrusts.Data;
 
-public record TrustSearchEntry(string Name, string Address, string Uid, string GroupId) : IEnumerable
-{
-    public IEnumerator GetEnumerator()
-    {
-        throw new NotImplementedException();
-    }
-}
+public record TrustSearchEntry(string Name, string Address, string Uid, string GroupId);
