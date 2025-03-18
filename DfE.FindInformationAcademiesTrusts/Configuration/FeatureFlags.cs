7222cda0bd8eeb3cd4be110d530a57e6205cee8b
Linzi CARLIN <Linzi.CARLIN@EDUCATION.GOV.UK>
2025-03-18 14:33:08 +0000

    add feature flag to hide governance turnover calculation

--- DfE.FindInformationAcademiesTrusts/Configuration/FeatureFlags.cs
+++ DfE.FindInformationAcademiesTrusts/Configuration/FeatureFlags.cs
@@ -4,4 +4,5 @@ public static class FeatureFlags
 {
     public const string TestFlag = "TestFlag";
     public const string UpdatedFooterHelpLink = "UpdatedFooterHelpLink";
+    public const string GovernanceTurnover = "GovernanceTurnover";
 }
