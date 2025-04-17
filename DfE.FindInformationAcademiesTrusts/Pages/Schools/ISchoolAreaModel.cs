fcb54f08dfb4febdad8a9e488d84acee348e6895
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2025-04-17 10:26:08 +0100

    Add school area model
    For now this contains dummy data so we can see mimic viewing a school or academy

---
+++ DfE.FindInformationAcademiesTrusts/Pages/Schools/ISchoolAreaModel.cs
@@ -0,0 +1,15 @@
+using DfE.FindInformationAcademiesTrusts.Data.Enums;
+using DfE.FindInformationAcademiesTrusts.Pages.Shared;
+using DfE.FindInformationAcademiesTrusts.Pages.Shared.DataSource;
+
+namespace DfE.FindInformationAcademiesTrusts.Pages.Schools;
+
+public interface ISchoolAreaModel
+{
+    string Urn { get; }
+    SchoolCategory SchoolCategory { get; }
+    List<DataSourcePageListEntry> DataSourcesPerPage { get; }
+    PageMetadata PageMetadata { get; }
+    string SchoolName { get; }
+    string SchoolType { get; }
+}
