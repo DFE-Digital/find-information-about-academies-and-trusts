e8ac8c1f116a4b27767d06b470679b6c8247eba9
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2025-04-17 10:31:12 +0100

    Add school layout and first page

---
+++ tests/DfE.FindInformationAcademiesTrusts.UnitTests/Pages/Schools/Overview/BaseOverviewAreaModelTests.cs
@@ -0,0 +1,20 @@
+﻿using DfE.FindInformationAcademiesTrusts.Pages.Schools.Overview;
+
+namespace DfE.FindInformationAcademiesTrusts.UnitTests.Pages.Schools.Overview;
+
+public abstract class BaseOverviewAreaModelTests<T> : BaseSchoolPageTests<T> where T : OverviewAreaModel
+{
+    [Fact]
+    public override Task OnGetAsync_should_configure_PageMetadata_PageName()
+    {
+        Sut.PageMetadata.PageName.Should().Be("Overview");
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    public override Task OnGetAsync_sets_correct_data_source_list()
+    {
+        Sut.DataSourcesPerPage.Should().BeEmpty();
+        return Task.CompletedTask;
+    }
+}
