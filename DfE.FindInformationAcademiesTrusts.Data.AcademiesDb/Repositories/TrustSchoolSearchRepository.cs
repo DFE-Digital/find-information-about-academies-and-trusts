30a4aa46d51530e73e6eb5f90a4898f05a44bf17
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2025-05-19 10:52:49 +0100

    Minimise number of db queries executed and don't select when only count is needed

--- DfE.FindInformationAcademiesTrusts.Data.AcademiesDb/Repositories/TrustSchoolSearchRepository.cs
+++ DfE.FindInformationAcademiesTrusts.Data.AcademiesDb/Repositories/TrustSchoolSearchRepository.cs
@@ -1,97 +1,112 @@
 ﻿using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Contexts;
 using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Extensions;
+using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Models.Gias;
 using DfE.FindInformationAcademiesTrusts.Data.Repositories.Search;
 using Microsoft.EntityFrameworkCore;
 
-namespace DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Repositories
+namespace DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Repositories;
+
+public class TrustSchoolSearchRepository(IAcademiesDbContext academiesDbContext) : ITrustSchoolSearchRepository
 {
-    public class TrustSchoolSearchRepository(IAcademiesDbContext academiesDbContext) : ITrustSchoolSearchRepository
+    public async Task<(SearchResult[] Results, SearchResultCount NumberOfResults)> GetSearchResultsAsync(string? text,
+        int pageSize, int page = 1)
     {
-        public async Task<(SearchResult[] Results, SearchResultCount NumberOfResults)> GetSearchResultsAsync(string? text, int pageSize, int page = 1)
+        if (string.IsNullOrWhiteSpace(text))
         {
-            if (string.IsNullOrWhiteSpace(text))
+            return ([], new SearchResultCount(0, 0, 0));
+        }
+
+        //Count is done without the select for speed efficiency
+        var numberOfTrusts = await CreateTrustSearchQuery(text).CountAsync();
+        var numberOfSchools = await CreateSchoolSearchQuery(text).CountAsync();
+        var totalCount = numberOfTrusts + numberOfSchools;
+
+        //If there's no results then don't go back to the db
+        if (totalCount == 0)
+            return ([], new SearchResultCount(0, 0, 0));
+
+        //Now get all the results
+        var searchQuery = BuildSearchQueryWithSelect(text);
+        var results = await searchQuery.OrderBy(g => g.Name)
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToArrayAsync();
+
+        return (results, new SearchResultCount(totalCount, numberOfTrusts, numberOfSchools));
+    }
+
+    public async Task<SearchResult[]> GetAutoCompleteSearchResultsAsync(string? text)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            return [];
+        }
+
+        var searchQuery = BuildSearchQueryWithSelect(text);
+
+        var results = await searchQuery.OrderBy(g => g.Name)
+            .Take(5)
+            .ToArrayAsync();
+
+        return results;
+    }
+
+    private IQueryable<GiasGroup> CreateTrustSearchQuery(string searchTerm)
+    {
+        return academiesDbContext.Groups.Trusts()
+            .Where(g =>
+                    g.GroupId!.Contains(searchTerm) // GroupId cannot be null for a trust
+                    || g.GroupName!.Contains(searchTerm) // Enforced by global EF query filter
+            );
+    }
+
+    private IQueryable<GiasEstablishment> CreateSchoolSearchQuery(string searchTerm)
+    {
+        return academiesDbContext.GiasEstablishments
+            .Where(x =>
+                x.EstablishmentName!.Contains(searchTerm) // Enforced by global EF query filter
+                || x.Urn.ToString().Contains(searchTerm));
+    }
+
+    private IQueryable<SearchResult> BuildSearchQueryWithSelect(string text)
+    {
+        return SelectTrusts(CreateTrustSearchQuery(text))
+            .Union(SelectSchools(CreateSchoolSearchQuery(text)));
+    }
+
+    private static IQueryable<SearchResult> SelectTrusts(IQueryable<GiasGroup> trustsBaseQuery)
+    {
+        var query = trustsBaseQuery
+            .Select(g => new SearchResult
             {
-                return ([], new SearchResultCount(0, 0, 0));
-            }
+                Street = g.GroupContactStreet,
+                Locality = g.GroupContactLocality,
+                Town = g.GroupContactTown,
+                PostCode = g.GroupContactPostcode,
+                Name = g.GroupName!,
+                Id = g.GroupUid!.ToString(),
+                TrustGroupId = g.GroupId!.ToString(),
+                Type = g.GroupType!,
+                IsTrust = true
+            });
+        return query;
+    }
 
-            var searchQuery = BuildSearchQuery(text);
-
-            var totalCount = await searchQuery.CountAsync();
-            var numberOfTrusts = await searchQuery.CountAsync(x => x.IsTrust);
-            var numberOfSchools = await searchQuery.CountAsync(x => !x.IsTrust);
-
-            var results = await searchQuery.OrderBy(g => g.Name)
-                .Skip((page - 1) * pageSize)
-                .Take(pageSize)
-                .ToArrayAsync();
-
-            return (results, new SearchResultCount(totalCount, numberOfTrusts, numberOfSchools));
-        }
-
-        public async Task<SearchResult[]> GetAutoCompleteSearchResultsAsync(string? text)
-        {
-            if (string.IsNullOrWhiteSpace(text))
+    private static IQueryable<SearchResult> SelectSchools(IQueryable<GiasEstablishment> schoolsBaseQuery)
+    {
+        var query = schoolsBaseQuery
+            .Select(e => new SearchResult
             {
-                return [];
-            }
-
-            var searchQuery = BuildSearchQuery(text);
-
-           var results = await searchQuery.OrderBy(g => g.Name)
-                .Take(5)
-                .ToArrayAsync();
-
-            return results;
-        }
-
-        private IQueryable<SearchResult> BuildSearchQuery(string text)
-        {
-            return CreateTrustSearchQuery(text).Union(CreateSchoolSearchQuery(text));
-        }
-
-        private IQueryable<SearchResult> CreateTrustSearchQuery(string searchTerm)
-        {
-
-            var query = academiesDbContext.Groups.Trusts()
-                .Where(g =>
-                        g.GroupId!.Contains(searchTerm) // GroupId cannot be null for a trust
-                        || g.GroupName!.Contains(searchTerm) // Enforced by global EF query filter
-                )
-                .Select(g => new SearchResult
-                {
-                    Street = g.GroupContactStreet,
-                    Locality = g.GroupContactLocality,
-                    Town = g.GroupContactTown,
-                    PostCode = g.GroupContactPostcode,
-                    Name = g.GroupName!,
-                    Id = g.GroupUid!.ToString(),
-                    TrustGroupId = g.GroupId!.ToString(),
-                    Type = g.GroupType!,
-                    IsTrust = true
-                });
-            return query;
-        }
-
-        private IQueryable<SearchResult> CreateSchoolSearchQuery(string searchTerm)
-        {
-            var query = academiesDbContext.GiasEstablishments
-                .Where(x => 
-                    x.EstablishmentName!.Contains(searchTerm)
-                    || x.Urn.ToString().Contains(searchTerm))
-                .Select(e => new SearchResult
-                {
-                    Street = e.Street,
-                    Locality = e.Locality,
-                    Town = e.Town,
-                    PostCode = e.Postcode,
-                    TrustGroupId = null,
-                    Name = e.EstablishmentName!,
-                    Id = e.Urn.ToString(),
-                    Type = e.TypeOfEstablishmentName!,
-                    IsTrust = false
-                });
-            return query;
-
-        }
+                Street = e.Street,
+                Locality = e.Locality,
+                Town = e.Town,
+                PostCode = e.Postcode,
+                TrustGroupId = null,
+                Name = e.EstablishmentName!,
+                Id = e.Urn.ToString(),
+                Type = e.TypeOfEstablishmentName!,
+                IsTrust = false
+            });
+        return query;
     }
 }
