d1809a0e77625ec72cfe5d1c6f3888e185fcfa06
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2025-05-30 09:49:07 +0100

    Update `TadHeadTeacherContacts` with schema generated by ef tools

--- DfE.FindInformationAcademiesTrusts.Data.AcademiesDb/Contexts/AcademiesDbContext.TadHeadTeacherContacts.cs
+++ DfE.FindInformationAcademiesTrusts.Data.AcademiesDb/Contexts/AcademiesDbContext.TadHeadTeacherContacts.cs
@@ -1,29 +1,38 @@
+using System.Diagnostics.CodeAnalysis;
 using DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Models.Tad;
 using Microsoft.EntityFrameworkCore;
-using System.Diagnostics.CodeAnalysis;
 
 namespace DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Contexts;
 
 public partial class AcademiesDbContext
 {
-    public DbSet<TadHeadTeacherContactsFiat> TadHeadTeacherContacts { get; set; }
+    public DbSet<TadHeadTeacherContact> TadHeadTeacherContacts { get; set; }
 
     [ExcludeFromCodeCoverage]
     protected static void OnModelCreatingTadHeadTeacherContacts(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<TadHeadTeacherContactsFiat>(entity =>
+        modelBuilder.Entity<TadHeadTeacherContact>(entity =>
         {
-            entity.HasNoKey().ToTable("HeadTeacherContacts_FIAT", "tad");
+            entity.HasKey(e => e.Urn).HasName("PK__HeadTeac__DD778414D59087FB");
+
+            entity.ToTable("HeadTeacherContacts_FIAT", "tad");
 
             entity.Property(e => e.Urn)
-                .IsRequired()
+                .ValueGeneratedNever()
                 .HasColumnName("urn");
+            entity.Property(e => e.DateImported).HasColumnName("date_imported");
+            entity.Property(e => e.FileName)
+                .HasMaxLength(255)
+                .HasColumnName("file_name");
+            entity.Property(e => e.HeadEmail)
+                .HasMaxLength(255)
+                .HasColumnName("head_email");
             entity.Property(e => e.HeadFirstName)
+                .HasMaxLength(255)
                 .HasColumnName("head_first_name");
             entity.Property(e => e.HeadLastName)
+                .HasMaxLength(255)
                 .HasColumnName("head_last_name");
-            entity.Property(e => e.HeadEmail)
-                .HasColumnName("head_email");
         });
     }
 }
