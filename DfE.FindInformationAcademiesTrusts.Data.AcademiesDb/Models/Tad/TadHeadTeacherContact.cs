94ed2abf7a8d04ae2efe1423f4679d243e427f2c
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2025-05-30 09:49:07 +0100

    Update `TadHeadTeacherContacts` with schema generated by ef tools

--- DfE.FindInformationAcademiesTrusts.Data.AcademiesDb/Models/Tad/TadHeadTeacherContact.cs
+++ DfE.FindInformationAcademiesTrusts.Data.AcademiesDb/Models/Tad/TadHeadTeacherContact.cs
@@ -3,14 +3,17 @@ using System.Diagnostics.CodeAnalysis;
 namespace DfE.FindInformationAcademiesTrusts.Data.AcademiesDb.Models.Tad;
 
 [ExcludeFromCodeCoverage] // Database model POCO
-
-public class TadHeadTeacherContactsFiat
+public class TadHeadTeacherContact
 {
     public int Urn { get; set; }
-    public string? HeadFirstName { get; set; }
-    
-    public string? HeadLastName { get; set; }
-    
-    public string? HeadEmail { get; set; }
-}
 
+    public string? HeadFirstName { get; set; }
+
+    public string? HeadLastName { get; set; }
+
+    public string? HeadEmail { get; set; }
+
+    public string? FileName { get; set; }
+
+    public DateTime? DateImported { get; set; }
+}
