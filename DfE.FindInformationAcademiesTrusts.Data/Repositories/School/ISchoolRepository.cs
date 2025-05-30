b367fb02a166aef21c3faf47598b21975454cccf
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2025-05-30 15:05:41 +0100

    Make `SchoolContact` singular

--- DfE.FindInformationAcademiesTrusts.Data/Repositories/School/ISchoolRepository.cs
+++ DfE.FindInformationAcademiesTrusts.Data/Repositories/School/ISchoolRepository.cs
@@ -8,5 +8,5 @@ public interface ISchoolRepository
 
     Task<DateOnly> GetDateJoinedTrustAsync(int urn);
     
-    Task<SchoolContacts> GetSchoolContactsAsync(int urn);
+    Task<SchoolContact> GetSchoolContactsAsync(int urn);
 }
