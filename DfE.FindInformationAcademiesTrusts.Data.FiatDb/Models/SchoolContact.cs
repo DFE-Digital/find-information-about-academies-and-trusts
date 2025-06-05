126593858d2ab4b8a768e31c34e29ca1ca5a6f43
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2025-06-05 16:58:32 +0100

    Add new SchoolContact entity and SchoolContactRole enum

---
+++ DfE.FindInformationAcademiesTrusts.Data.FiatDb/Models/SchoolContact.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DfE.FindInformationAcademiesTrusts.Data.Enums;
+
+namespace DfE.FindInformationAcademiesTrusts.Data.FiatDb.Models;
+
+public class SchoolContact : BaseEntity
+{
+    public int Id { get; set; }
+    public required int Urn { get; set; }
+    public required SchoolContactRole Role { get; set; }
+    [MaxLength(500)] public required string Name { get; set; }
+    [MaxLength(320)] public required string Email { get; set; }
+}
